117daba4c7c5074f95c8c819a41f21fd7c7464a2
Kyle Banker <kylebanker@gmail.com>
2011-08-08 13:26:20 -0400

    Ensure finished BSON object on bson_iterator_code_scope.

--- src/bson.c
+++ src/bson.c
@@ -515,6 +515,8 @@ void bson_iterator_code_scope( const bson_iterator *i, bson *scope ) {
         int code_len;
         bson_little_endian32( &code_len, bson_iterator_value( i )+4 );
         bson_init_data( scope, ( void * )( bson_iterator_value( i )+8+code_len ) );
+        _bson_reset( scope );
+        scope->finished = 1;
     } else {
         bson_empty( scope );
     }
