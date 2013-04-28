fc4d935153a856bbc943e9d6683281747411ca09
Christian Hergert <christian.hergert@10gen.com>
2013-04-28 16:54:49 -0400

    tests: add unit test for mongoc_uri_t.

---
+++ tests/mongoc-tests.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2013 10gen Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef MONGOC_TESTS_H
+#define MONGOC_TESTS_H
+
+
+#include <assert.h>
+#include <bson.h>
+#include <stdio.h>
+#include <time.h>
+
+
+BSON_BEGIN_DECLS
+
+
+#define assert_cmpstr(a, b)                                             \
+   do {                                                                 \
+      if (((a) != (b)) && !!strcmp((a), (b))) {                         \
+         fprintf(stderr, "FAIL\n\nAssert Failure: \"%s\" != \"%s\"\n",  \
+                         a, b);                                         \
+         abort();                                                       \
+      }                                                                 \
+   } while (0)
+
+
+#define assert_cmpint(a, eq, b)                                         \
+   do {                                                                 \
+      if (!((a) eq (b))) {                                              \
+         fprintf(stderr, "FAIL\n\nAssert Failure: "                     \
+                         #a " " #eq " " #b "\n");                       \
+         abort();                                                       \
+      }                                                                 \
+   } while (0)
+
+
+static void
+run_test (const char *name,
+          void (*func) (void))
+{
+   struct timeval begin;
+   struct timeval end;
+   struct timeval diff;
+   bson_int64_t usec;
+   double format;
+
+   fprintf(stdout, "%-42s : ", name);
+   fflush(stdout);
+   gettimeofday(&begin, NULL);
+   func();
+   gettimeofday(&end, NULL);
+   fprintf(stdout, "PASS");
+
+   diff.tv_sec = end.tv_sec - begin.tv_sec;
+   diff.tv_usec = usec = end.tv_usec - begin.tv_usec;
+   if (usec < 0) {
+      diff.tv_sec -= 1;
+      diff.tv_usec = usec + 1000000;
+   }
+
+   format = diff.tv_sec + (diff.tv_usec / 1000000.0);
+   fprintf(stdout, " : %lf\n", format);
+}
+
+
+BSON_END_DECLS
+
+#endif /* MONGOC_TESTS_H */
---
+++ tests/test-mongoc-uri.c
@@ -0,0 +1,129 @@
+#include <mongoc.h>
+
+#include "mongoc-tests.h"
+
+
+static void
+test_mongoc_uri_new (void)
+{
+   const mongoc_host_list_t *hosts;
+   const bson_t *options;
+   mongoc_uri_t *uri;
+   bson_iter_t iter;
+   bson_iter_t child;
+
+   assert(!mongoc_uri_new("mongodb://"));
+   assert(!mongoc_uri_new("mongodb://::"));
+   assert(!mongoc_uri_new("mongodb://localhost::27017"));
+   assert(!mongoc_uri_new("mongodb://localhost,localhost::"));
+   assert(!mongoc_uri_new("mongodb://local1,local2,local3/d?k"));
+   assert(!mongoc_uri_new(""));
+   assert(!mongoc_uri_new("mongo://localhost:27017"));
+   assert(!mongoc_uri_new("mongodb://localhost::27017"));
+   assert(!mongoc_uri_new("mongodb://localhost::27017/"));
+   assert(!mongoc_uri_new("mongodb://localhost::27017,abc"));
+
+   /*
+    * TODO: Support IPv6.
+    */
+   assert(!mongoc_uri_new("mongodb://[::1]/?ipv6=true&safe=true"));
+
+   uri = mongoc_uri_new("mongodb://localhost:27017/test?q=1");
+   assert(uri);
+   hosts = mongoc_uri_get_hosts(uri);
+   assert(hosts);
+   assert(!hosts->next);
+   assert_cmpstr(hosts->host, "localhost");
+   assert_cmpint(hosts->port, ==, 27017);
+   assert_cmpstr(hosts->host_and_port, "localhost:27017");
+   assert_cmpstr(mongoc_uri_get_database(uri), "test");
+   options = mongoc_uri_get_options(uri);
+   assert(options);
+   assert(bson_iter_init_find(&iter, options, "q"));
+   assert_cmpstr(bson_iter_utf8(&iter, NULL), "1");
+   mongoc_uri_destroy(uri);
+
+   uri = mongoc_uri_new("mongodb://local1,local2:999,local3?q=1");
+   assert(uri);
+   hosts = mongoc_uri_get_hosts(uri);
+   assert(hosts);
+   assert(hosts->next);
+   assert(hosts->next->next);
+   assert(!hosts->next->next->next);
+   assert_cmpstr(hosts->host, "local1");
+   assert_cmpint(hosts->port, ==, 27017);
+   assert_cmpstr(hosts->next->host, "local2");
+   assert_cmpint(hosts->next->port, ==, 999);
+   assert_cmpstr(hosts->next->next->host, "local3");
+   assert_cmpint(hosts->next->next->port, ==, 27017);
+   options = mongoc_uri_get_options(uri);
+   assert(options);
+   assert(bson_iter_init_find(&iter, options, "q"));
+   assert_cmpstr(bson_iter_utf8(&iter, NULL), "1");
+   mongoc_uri_destroy(uri);
+
+   uri = mongoc_uri_new("mongodb://localhost:27017/?readPreferenceTags=dc:ny&readPreferenceTags=");
+   assert(uri);
+   options = mongoc_uri_get_read_preferences(uri);
+   assert(options);
+   assert_cmpint(bson_count(options), ==, 2);
+   assert(bson_iter_init_find(&iter, options, "0"));
+   assert(BSON_ITER_HOLDS_DOCUMENT(&iter));
+   assert(bson_iter_recurse(&iter, &child));
+   assert(bson_iter_next(&child));
+   assert_cmpstr(bson_iter_key(&child), "dc");
+   assert_cmpstr(bson_iter_utf8(&child, NULL), "ny");
+   assert(!bson_iter_next(&child));
+   assert(bson_iter_next(&iter));
+   assert(BSON_ITER_HOLDS_DOCUMENT(&iter));
+   assert(bson_iter_recurse(&iter, &child));
+   assert(!bson_iter_next(&child));
+   assert(!bson_iter_next(&iter));
+   mongoc_uri_destroy(uri);
+
+   uri = mongoc_uri_new("mongodb://localhost/a?slaveok=true&ssl=false&journal=true");
+   options = mongoc_uri_get_options(uri);
+   assert(options);
+   assert_cmpint(bson_count(options), ==, 3);
+   assert(bson_iter_init(&iter, options));
+   assert(bson_iter_find_case(&iter, "slaveok"));
+   assert(BSON_ITER_HOLDS_BOOL(&iter));
+   assert(bson_iter_bool(&iter));
+   assert(bson_iter_find_case(&iter, "ssl"));
+   assert(BSON_ITER_HOLDS_BOOL(&iter));
+   assert(!bson_iter_bool(&iter));
+   assert(bson_iter_find_case(&iter, "journal"));
+   assert(BSON_ITER_HOLDS_BOOL(&iter));
+   assert(bson_iter_bool(&iter));
+   assert(!bson_iter_next(&iter));
+   mongoc_uri_destroy(uri);
+
+   uri = mongoc_uri_new("mongodb:///tmp/mongodb.sock/?ssl=false");
+   assert(uri);
+   assert_cmpstr(mongoc_uri_get_hosts(uri)->host, "/tmp/mongodb.sock");
+   mongoc_uri_destroy(uri);
+
+   uri = mongoc_uri_new("mongodb:///tmp/mongodb.sock,localhost:27017/?ssl=false");
+   assert(uri);
+   assert_cmpstr(mongoc_uri_get_hosts(uri)->host, "/tmp/mongodb.sock");
+   assert_cmpstr(mongoc_uri_get_hosts(uri)->next->host_and_port, "localhost:27017");
+   assert(!mongoc_uri_get_hosts(uri)->next->next);
+   mongoc_uri_destroy(uri);
+
+   uri = mongoc_uri_new("mongodb://localhost:27017,/tmp/mongodb.sock/?ssl=false");
+   assert(uri);
+   assert_cmpstr(mongoc_uri_get_hosts(uri)->host_and_port, "localhost:27017");
+   assert_cmpstr(mongoc_uri_get_hosts(uri)->next->host, "/tmp/mongodb.sock");
+   assert(!mongoc_uri_get_hosts(uri)->next->next);
+   mongoc_uri_destroy(uri);
+}
+
+
+int
+main (int   argc,
+      char *argv[])
+{
+   run_test("/mongoc/uri/new", test_mongoc_uri_new);
+
+   return 0;
+}
