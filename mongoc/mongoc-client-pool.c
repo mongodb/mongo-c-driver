491cce3a5df220c4d21eb850627e116059d464c9
Christian Hergert <christian.hergert@10gen.com>
2013-05-08 16:36:44 -0700

    pool: start on pool data structure.

---
+++ mongoc/mongoc-client-pool.c
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2013 10gen Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "mongoc-client-pool.h"
+#include "mongoc-queue-private.h"
+
+
+struct _mongoc_client_pool_t
+{
+   mongoc_queue_t  queue;
+   mongoc_uri_t   *uri;
+   bson_uint32_t   min_pool_size;
+   bson_uint32_t   max_pool_size;
+};
+
+
+mongoc_client_pool_t *
+mongoc_client_pool_new (const mongoc_uri_t *uri)
+{
+   mongoc_client_pool_t *pool;
+   const bson_t *b;
+   bson_iter_t iter;
+
+   bson_return_val_if_fail(uri, NULL);
+
+   pool = bson_malloc0(sizeof *pool);
+   mongoc_queue_init(&pool->queue);
+   pool->uri = mongoc_uri_copy(uri);
+   pool->min_pool_size = 0;
+   pool->max_pool_size = 100;
+
+   b = mongoc_uri_get_options(pool->uri);
+
+   if (bson_iter_init_find_case(&iter, b, "minpoolsize")) {
+      if (BSON_ITER_HOLDS_INT32(&iter)) {
+         pool->min_pool_size = bson_iter_int32(&iter);
+      }
+   }
+
+   if (bson_iter_init_find_case(&iter, b, "maxpoolsize")) {
+      if (BSON_ITER_HOLDS_INT32(&iter)) {
+         pool->max_pool_size = bson_iter_int32(&iter);
+      }
+   }
+
+   return pool;
+}
+
+
+void
+mongoc_client_pool_destroy (mongoc_client_pool_t *pool)
+{
+   mongoc_client_t *client;
+
+   bson_return_if_fail(pool);
+
+   while ((client = mongoc_queue_pop_head(&pool->queue))) {
+      mongoc_client_destroy(client);
+   }
+   mongoc_uri_destroy(pool->uri);
+   bson_free(pool);
+}
+
+
+mongoc_client_t *
+mongoc_client_pool_pop (mongoc_client_pool_t *pool)
+{
+   return NULL;
+}
+
+
+void
+mongoc_client_pool_push (mongoc_client_pool_t *pool,
+                         mongoc_client_t      *client)
+{
+}
---
+++ mongoc/mongoc-client-pool.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 10gen Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef MONGOC_CLIENT_POOL_H
+#define MONGOC_CLIENT_POOL_H
+
+
+#include <bson.h>
+
+#include "mongoc-client.h"
+#include "mongoc-uri.h"
+
+
+BSON_BEGIN_DECLS
+
+
+typedef struct _mongoc_client_pool_t mongoc_client_pool_t;
+
+
+mongoc_client_pool_t *mongoc_client_pool_new     (const mongoc_uri_t *uri);
+void                  mongoc_client_pool_destroy (mongoc_client_pool_t *pool);
+mongoc_client_t      *mongoc_client_pool_pop     (mongoc_client_pool_t *pool);
+void                  mongoc_client_pool_push    (mongoc_client_pool_t *pool,
+                                                  mongoc_client_t      *client);
+
+
+BSON_END_DECLS
+
+
+#endif /* MONGOC_CLIENT_POOL_H */
