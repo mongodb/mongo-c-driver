ba6f3c20c0ff6ad16e540684629fa2dc2bafd243
Christian Hergert <christian.hergert@10gen.com>
2013-06-18 15:08:02 -0700

    rpc: start prototyping an xmacro based rpc design.

---
+++ mongoc/mongoc-rpc-private.h
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2013 10gen Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef MONGOC_RPC_PRIVATE_H
+#define MONGOC_RPC_PRIVATE_H
+
+
+#include <bson.h>
+
+#include "mongoc-array-private.h"
+
+
+BSON_BEGIN_DECLS
+
+
+#define RPC(_name, _code)                typedef struct { _code } mongoc_rpc_##_name##_t;
+#define INT32_FIELD(_name)               bson_int32_t _name;
+#define INT64_FIELD(_name)               bson_int64_t _name;
+#define INT64_ARRAY_FIELD(_len, _name)   bson_int32_t _len; bson_int64_t *_name;
+#define CSTRING_FIELD(_name)             char _name[256];
+#define BSON_FIELD(_name)                const bson_t *_name;
+#define BSON_ARRAY_FIELD(_len, _name)    const bson_t * const *_name;
+#define RAW_BUFFER_FIELD(_name)          void *_name; bson_int32_t _name##_len;
+#define OPTIONAL(_check, _code)          _code
+
+
+#pragma pack(push, 1)
+
+#include "op-delete.def"
+#include "op-get-more.def"
+#include "op-header.def"
+#include "op-insert.def"
+#include "op-kill-cursors.def"
+#include "op-msg.def"
+#include "op-query.def"
+#include "op-reply.def"
+#include "op-update.def"
+
+typedef union
+{
+   mongoc_rpc_delete_t       delete;
+   mongoc_rpc_get_more_t     get_more;
+   mongoc_rpc_header_t       header;
+   mongoc_rpc_insert_t       insert;
+   mongoc_rpc_kill_cursors_t kill_cursors;
+   mongoc_rpc_msg_t          msg;
+   mongoc_rpc_query_t        query;
+   mongoc_rpc_reply_t        reply;
+   mongoc_rpc_update_t       update;
+} mongoc_rpc_t;
+
+#pragma pack(pop)
+
+
+#undef RPC
+#undef INT32_FIELD
+#undef INT64_FIELD
+#undef INT64_ARRAY_FIELD
+#undef CSTRING_FIELD
+#undef BSON_FIELD
+#undef BSON_ARRAY_FIELD
+#undef OPTIONAL
+#undef RAW_BUFFER_FIELD
+
+
+bson_bool_t mongoc_rpc_gather (mongoc_rpc_t   *rpc,
+                               mongoc_array_t *iovec);
+void        mongoc_rpc_swab   (mongoc_rpc_t   *rpc);
+
+
+BSON_END_DECLS
+
+
+#endif /* MONGOC_RPC_PRIVATE_H */
---
+++ mongoc/mongoc-rpc.c
@@ -0,0 +1,201 @@
+/*
+ * Copyright 2013 10gen Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <sys/uio.h>
+
+#include "mongoc-event-private.h"
+#include "mongoc-log.h"
+#include "mongoc-rpc-private.h"
+
+
+#define RPC(_name, _code) \
+   static BSON_INLINE bson_bool_t \
+   mongoc_rpc_gather_##_name (mongoc_rpc_##_name##_t *rpc, \
+                              mongoc_array_t *array) \
+   { \
+      struct iovec iov; \
+      BSON_ASSERT(rpc); \
+      BSON_ASSERT(array); \
+      _code \
+      return TRUE; \
+   }
+#define INT32_FIELD(_name) \
+   iov.iov_base = &rpc->_name; \
+   iov.iov_len = 4; \
+   mongoc_array_append_val(array, iov);
+#define INT64_FIELD(_name) \
+   iov.iov_base = &rpc->_name; \
+   iov.iov_len = 8; \
+   mongoc_array_append_val(array, iov);
+#define CSTRING_FIELD(_name) \
+   iov.iov_base = rpc->_name; \
+   iov.iov_len = strlen(rpc->_name) + 1; \
+   mongoc_array_append_val(array, iov);
+#define BSON_FIELD(_name) \
+   iov.iov_base = (void *)bson_get_data(rpc->_name); \
+   iov.iov_len = rpc->_name->len; \
+   mongoc_array_append_val(array, iov);
+#define OPTIONAL(_check, _code) \
+   if (rpc->_check) { _code }
+#define BSON_ARRAY_FIELD(_len, _name) \
+   do { \
+      typeof(rpc->_len) i; \
+      for (i = 0; i < rpc->_len; i++) { \
+         iov.iov_base = (void *)bson_get_data(rpc->_name[i]); \
+         iov.iov_len = rpc->_name[i]->len; \
+         mongoc_array_append_val(array, iov); \
+      } \
+   } while (0);
+#define RAW_BUFFER_FIELD(_name) \
+   iov.iov_base = rpc->_name; \
+   iov.iov_len = rpc->_name##_len; \
+   mongoc_array_append_val(array, iov);
+#define INT64_ARRAY_FIELD(_len, _name) \
+   iov.iov_base = rpc->_name; \
+   iov.iov_len = rpc->_len * 8; \
+   mongoc_array_append_val(array, iov);
+
+
+
+#include "op-delete.def"
+#include "op-get-more.def"
+#include "op-insert.def"
+#include "op-kill-cursors.def"
+#include "op-msg.def"
+#include "op-query.def"
+#include "op-reply.def"
+#include "op-update.def"
+
+
+#undef RPC
+#undef INT32_FIELD
+#undef INT64_FIELD
+#undef INT64_ARRAY_FIELD
+#undef CSTRING_FIELD
+#undef BSON_FIELD
+#undef BSON_ARRAY_FIELD
+#undef RAW_BUFFER_FIELD
+#undef OPTIONAL
+
+
+#define RPC(_name, _code) \
+   static BSON_INLINE void \
+   mongoc_rpc_swab_##_name (mongoc_rpc_##_name##_t *rpc) \
+   { \
+      BSON_ASSERT(rpc); \
+      _code \
+   }
+#define INT32_FIELD(_name) \
+   rpc->_name = BSON_UINT32_FROM_LE(rpc->_name);
+#define INT64_FIELD(_name) \
+   rpc->_name = BSON_UINT64_FROM_LE(rpc->_name);
+#define CSTRING_FIELD(_name)
+#define BSON_FIELD(_name)
+#define BSON_ARRAY_FIELD(_len, _name) \
+   rpc->_len = BSON_UINT32_FROM_LE(rpc->_len);
+#define OPTIONAL(_check, _code) \
+   if (rpc->_check) { _code }
+#define RAW_BUFFER_FIELD(_name)
+#define INT64_ARRAY_FIELD(_len, _name) \
+   do { \
+      typeof(rpc->_len) i; \
+      for (i = 0; i < rpc->_len; i++) { \
+         rpc->_name[i] = BSON_UINT64_FROM_LE(rpc->_name[i]); \
+      } \
+      rpc->_len = BSON_UINT32_FROM_LE(rpc->_len); \
+   } while (0);
+
+
+#include "op-delete.def"
+#include "op-get-more.def"
+#include "op-header.def"
+#include "op-insert.def"
+#include "op-kill-cursors.def"
+#include "op-msg.def"
+#include "op-query.def"
+#include "op-reply.def"
+#include "op-update.def"
+
+
+#undef RPC
+#undef INT32_FIELD
+#undef INT64_FIELD
+#undef INT64_ARRAY_FIELD
+#undef CSTRING_FIELD
+#undef BSON_FIELD
+#undef BSON_ARRAY_FIELD
+#undef OPTIONAL
+#undef RAW_BUFFER_FIELD
+
+
+bson_bool_t
+mongoc_rpc_gather (mongoc_rpc_t   *rpc,
+                   mongoc_array_t *array)
+{
+   bson_return_val_if_fail(rpc, FALSE);
+   bson_return_val_if_fail(array, FALSE);
+
+   switch ((mongoc_opcode_t)rpc->header.op_code) {
+   case MONGOC_OPCODE_REPLY:
+      return mongoc_rpc_gather_reply(&rpc->reply, array);
+   case MONGOC_OPCODE_MSG:
+      return mongoc_rpc_gather_msg(&rpc->msg, array);
+   case MONGOC_OPCODE_UPDATE:
+      return mongoc_rpc_gather_update(&rpc->update, array);
+   case MONGOC_OPCODE_INSERT:
+      return mongoc_rpc_gather_insert(&rpc->insert, array);
+   case MONGOC_OPCODE_QUERY:
+      return mongoc_rpc_gather_query(&rpc->query, array);
+   case MONGOC_OPCODE_GET_MORE:
+      return mongoc_rpc_gather_get_more(&rpc->get_more, array);
+   case MONGOC_OPCODE_DELETE:
+      return mongoc_rpc_gather_delete(&rpc->delete, array);
+   case MONGOC_OPCODE_KILL_CURSORS:
+      return mongoc_rpc_gather_kill_cursors(&rpc->kill_cursors, array);
+   default:
+      return FALSE;
+   }
+}
+
+
+void
+mongoc_rpc_swab (mongoc_rpc_t *rpc)
+{
+   bson_return_if_fail(rpc);
+
+   switch ((mongoc_opcode_t)rpc->header.op_code) {
+   case MONGOC_OPCODE_REPLY:
+      return mongoc_rpc_swab_reply(&rpc->reply);
+   case MONGOC_OPCODE_MSG:
+      return mongoc_rpc_swab_msg(&rpc->msg);
+   case MONGOC_OPCODE_UPDATE:
+      return mongoc_rpc_swab_update(&rpc->update);
+   case MONGOC_OPCODE_INSERT:
+      return mongoc_rpc_swab_insert(&rpc->insert);
+   case MONGOC_OPCODE_QUERY:
+      return mongoc_rpc_swab_query(&rpc->query);
+   case MONGOC_OPCODE_GET_MORE:
+      return mongoc_rpc_swab_get_more(&rpc->get_more);
+   case MONGOC_OPCODE_DELETE:
+      return mongoc_rpc_swab_delete(&rpc->delete);
+   case MONGOC_OPCODE_KILL_CURSORS:
+      return mongoc_rpc_swab_kill_cursors(&rpc->kill_cursors);
+   default:
+      MONGOC_WARNING("Unknown rpc type: 0x%08x", rpc->header.op_code);
+      break;
+   }
+}
