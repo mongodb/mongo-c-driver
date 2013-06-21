a52e77854393ba6d8edaf7be71dfc40f5b09938b
Christian Hergert <christian@hergert.me>
2013-06-20 18:51:40 -0700

    tests: update buffer test for new api.

--- tests/test-mongoc-buffer.c
+++ tests/test-mongoc-buffer.c
@@ -8,12 +8,11 @@
 static void
 test_mongoc_buffer_basic (void)
 {
-   bson_uint8_t lbuf[1024];
    mongoc_stream_t *stream;
    mongoc_buffer_t buf;
    bson_uint8_t *data = bson_malloc0(1024);
    bson_error_t error = { 0 };
-   struct iovec iov;
+   bson_bool_t r;
    int fd;
 
    fd = open("tests/binary/reply1.dat", O_RDONLY);
@@ -23,17 +22,11 @@ test_mongoc_buffer_basic (void)
    assert(stream);
 
    mongoc_buffer_init(&buf, data, 1024, bson_realloc);
-   assert(TRUE == mongoc_buffer_fill(&buf, stream, 536, &error));
+   r = mongoc_buffer_fill(&buf, stream, &error);
+   assert(r == TRUE);
+   assert_cmpint(buf.len, ==, 536);
 
-   iov.iov_base = lbuf;
-   iov.iov_len = 1024;
-   assert_cmpint(536, ==, mongoc_buffer_readv(&buf, &iov, 1));
-   assert_cmpint(0, ==, mongoc_buffer_readv(&buf, &iov, 1));
-   assert(FALSE == mongoc_buffer_fill(&buf, stream, 1, &error));
-   assert_cmpint(0, ==, mongoc_buffer_readv(&buf, &iov, 1));
-   mongoc_stream_close(stream);
-   assert(FALSE == mongoc_buffer_fill(&buf, stream, 1, &error));
-   assert_cmpint(0, ==, mongoc_buffer_readv(&buf, &iov, 1));
+   mongoc_buffer_destroy(&buf);
 }
 
 
