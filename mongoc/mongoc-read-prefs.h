943e5b37a930d371f88ee315510af82821b58005
Christian Hergert <christian.hergert@10gen.com>
2013-07-03 20:16:04 -0700

    read-prefs: stub out read-prefs structure.

---
+++ mongoc/mongoc-read-prefs.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2013 10gen Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef MONGOC_READ_PREFS_H
+#define MONGOC_READ_PREFS_H
+
+
+#include <bson.h>
+
+
+BSON_BEGIN_DECLS
+
+
+typedef struct _mongoc_read_prefs_t mongoc_read_prefs_t;
+
+
+mongoc_read_prefs_t *mongoc_read_prefs_new     (void);
+void                 mongoc_read_prefs_destroy (mongoc_read_prefs_t *read_prefs);
+
+
+BSON_END_DECLS
+
+
+#endif /* MONGOC_READ_PREFS_H */
