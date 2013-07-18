c6386cd22cb55b9a67f373f6528a71fa4ca95887
Christian Hergert <christian.hergert@10gen.com>
2013-07-17 17:59:36 -0700

    tests: add test for mongoc_collection_delete().

--- tests/test-mongoc-collection.c
+++ tests/test-mongoc-collection.c
@@ -117,6 +117,61 @@ test_update (void)
 
 
 static void
+test_delete (void)
+{
+   mongoc_collection_t *collection;
+   mongoc_client_t *client;
+   bson_context_t *context;
+   bson_error_t error;
+   bson_bool_t r;
+   bson_oid_t oid;
+   bson_t b;
+   bson_t result;
+   int i;
+
+   client = mongoc_client_new(TEST_HOST);
+   assert(client);
+
+   collection = mongoc_client_get_collection(client, "test", "test");
+   assert(collection);
+
+   context = bson_context_new(BSON_CONTEXT_NONE);
+   assert(context);
+
+   for (i = 0; i < 100; i++) {
+      bson_init(&b);
+      bson_oid_init(&oid, context);
+      bson_append_oid(&b, "_id", 3, &oid);
+      bson_append_utf8(&b, "hello", 5, "world", 5);
+      r = mongoc_collection_insert(collection, MONGOC_INSERT_NONE, &b, NULL,
+                                   &result, &error);
+      if (!r) {
+         MONGOC_WARNING("%s\n", error.message);
+         bson_error_destroy(&error);
+      }
+      assert(r);
+      bson_destroy(&b);
+      bson_destroy(&result);
+
+      bson_init(&b);
+      bson_append_oid(&b, "_id", 3, &oid);
+      r = mongoc_collection_delete(collection, MONGOC_DELETE_NONE, &b, NULL,
+                                   &error);
+      if (!r) {
+         MONGOC_WARNING("%s\n", error.message);
+         bson_error_destroy(&error);
+      }
+      assert(r);
+      bson_destroy(&b);
+   }
+
+   mongoc_collection_destroy(collection);
+   bson_context_destroy(context);
+   mongoc_client_destroy(client);
+}
+
+
+static void
 log_handler (mongoc_log_level_t  log_level,
              const char         *domain,
              const char         *message,
@@ -136,6 +191,7 @@ main (int   argc,
 
    run_test("/mongoc/collection/insert", test_insert);
    run_test("/mongoc/collection/update", test_update);
+   run_test("/mongoc/collection/delete", test_delete);
 
    return 0;
 }
