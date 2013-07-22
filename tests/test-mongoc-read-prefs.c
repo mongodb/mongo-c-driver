2e78a1e2c3e98035ad58d03b2a160311a02023bb
Christian Hergert <christian.hergert@10gen.com>
2013-07-22 15:14:25 -0700

    tests: more node scoring tests.

--- tests/test-mongoc-read-prefs.c
+++ tests/test-mongoc-read-prefs.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <mongoc.h>
 #include <mongoc-cluster-private.h>
 #include <mongoc-read-prefs-private.h>
@@ -28,6 +29,22 @@ test_mongoc_read_prefs_score (void)
    ASSERT_VALID(read_prefs);
    score = _mongoc_read_prefs_score(read_prefs, &node);
    assert_cmpint(score, ==, 1);
+
+   mongoc_read_prefs_set_mode(read_prefs, MONGOC_READ_SECONDARY_PREFERRED);
+   ASSERT_VALID(read_prefs);
+   score = _mongoc_read_prefs_score(read_prefs, &node);
+   assert_cmpint(score, ==, 1);
+
+   mongoc_read_prefs_set_mode(read_prefs, MONGOC_READ_SECONDARY);
+   ASSERT_VALID(read_prefs);
+   score = _mongoc_read_prefs_score(read_prefs, &node);
+   assert_cmpint(score, ==, 1);
+
+   node.primary = TRUE;
+   mongoc_read_prefs_set_mode(read_prefs, MONGOC_READ_PRIMARY);
+   ASSERT_VALID(read_prefs);
+   score = _mongoc_read_prefs_score(read_prefs, &node);
+   assert_cmpint(score, ==, INT_MAX);
 
    mongoc_read_prefs_destroy(read_prefs);
 
