a72771a355b3f8832709c81bfb3e9bc655bae7af
Christian Hergert <christian.hergert@10gen.com>
2013-07-25 14:09:45 -0700

    cluster: send getlasterror on try_sendv().

--- mongoc/mongoc-cluster.c
+++ mongoc/mongoc-cluster.c
@@ -592,6 +592,9 @@ mongoc_cluster_try_sendv (mongoc_cluster_t       *cluster,
 {
    mongoc_cluster_node_t *node;
    struct iovec *iov;
+   const bson_t *b;
+   mongoc_rpc_t gle;
+   bson_bool_t need_gle;
    size_t iovcnt;
    size_t i;
 
@@ -609,8 +612,39 @@ mongoc_cluster_try_sendv (mongoc_cluster_t       *cluster,
    mongoc_array_clear(&cluster->iov);
 
    for (i = 0; i < rpcs_len; i++) {
+      rpcs[i].header.request_id = ++cluster->request_id;
+      need_gle = mongoc_cluster_needs_gle(cluster, &rpcs[i], write_concern);
       mongoc_rpc_gather(&rpcs[i], &cluster->iov);
       mongoc_rpc_swab(&rpcs[i]);
+      if (need_gle) {
+         gle.query.msg_len = 0;
+         gle.query.request_id = ++cluster->request_id;
+         gle.query.response_to = -1;
+         gle.query.opcode = MONGOC_OPCODE_QUERY;
+         gle.query.flags = MONGOC_QUERY_NONE;
+         switch (rpcs[i].header.opcode) {
+         case MONGOC_OPCODE_INSERT:
+            gle.query.collection = rpcs[i].insert.collection;
+            break;
+         case MONGOC_OPCODE_DELETE:
+            gle.query.collection = rpcs[i].delete.collection;
+            break;
+         case MONGOC_OPCODE_UPDATE:
+            gle.query.collection = rpcs[i].update.collection;
+            break;
+         default:
+            BSON_ASSERT(FALSE);
+            gle.query.collection = "";
+            break;
+         }
+         gle.query.skip = 0;
+         gle.query.n_return = 1;
+         b = mongoc_write_concern_freeze(write_concern);
+         gle.query.query = bson_get_data(b);
+         gle.query.fields = NULL;
+         mongoc_rpc_gather(&gle, &cluster->iov);
+         mongoc_rpc_swab(&gle);
+      }
    }
 
    iov = cluster->iov.data;
