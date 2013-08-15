12d3acd49ee0cb0c489d71e5f98b5782889dd613
Christian Hergert <christian@hergert.me>
2013-08-14 17:58:26 -0700

    client: test authentication failure.

--- tests/test-mongoc-client.c
+++ tests/test-mongoc-client.c
@@ -3,8 +3,9 @@
 #include "mongoc-tests.h"
 
 
-#define MONGOC_TEST_URI               "mongodb://localhost:27017/"
-#define MONGOC_TEST_URI_WITH_PASSWORD "mongodb://testuser:testpass@localhost:27017/test"
+#define MONGOC_TEST_URI                   "mongodb://localhost:27017/"
+#define MONGOC_TEST_URI_WITH_PASSWORD     "mongodb://testuser:testpass@localhost:27017/test"
+#define MONGOC_TEST_URI_WITH_BAD_PASSWORD "mongodb://baduser:badpass@localhost:27017/test"
 
 
 static void
@@ -49,6 +50,38 @@ test_mongoc_client_authenticate (void)
 
 
 static void
+test_mongoc_client_authenticate_failure (void)
+{
+   mongoc_collection_t *collection;
+   mongoc_cursor_t *cursor;
+   mongoc_client_t *client;
+   const bson_t *doc;
+   bson_error_t error;
+   bson_bool_t r;
+   bson_t q;
+
+   /*
+    * Try authenticating with that user.
+    */
+   bson_init(&q);
+   client = mongoc_client_new(MONGOC_TEST_URI_WITH_BAD_PASSWORD);
+   collection = mongoc_client_get_collection(client, "test", "test");
+   cursor = mongoc_collection_find(collection, MONGOC_QUERY_NONE, 0, 1,
+                                   &q, NULL, NULL);
+   r = mongoc_cursor_next(cursor, &doc);
+   assert(!r);
+   r = mongoc_cursor_error(cursor, &error);
+   assert(r);
+   assert(error.domain == MONGOC_ERROR_CLIENT);
+   assert(error.code == MONGOC_ERROR_CLIENT_AUTHENTICATE);
+   bson_error_destroy(&error);
+   mongoc_cursor_destroy(cursor);
+   mongoc_collection_destroy(collection);
+   mongoc_client_destroy(client);
+}
+
+
+static void
 log_handler (mongoc_log_level_t  log_level,
              const char         *domain,
              const char         *message,
@@ -67,6 +100,7 @@ main (int   argc,
    }
 
    run_test("/mongoc/client/authenticate", test_mongoc_client_authenticate);
+   run_test("/mongoc/client/authenticate_failure", test_mongoc_client_authenticate_failure);
 
    return 0;
 }
