2e0c93ebcaa47d09ac5af8a0ede922bc996547db
Christian Hergert <christian.hergert@10gen.com>
2013-09-26 13:46:58 -0700

    tests: launch subprocess tester.

--- tests/ha/test1.py
+++ tests/ha/test1.py
@@ -18,6 +18,13 @@ class ReplicaSet:
         self.nodes = []
         self.port = random.randint(30000, 40000)
 
+    @property
+    def uri(self):
+        return ('mongodb://' +
+                ','.join(['127.0.0.1:%s' % node.port for node in self.nodes]) +
+                '/?replicaSet=' +
+                self.name)
+
     def _addNode(self, node):
         if node not in self.nodes:
             node.port, self.port = self.port, self.port + 1
@@ -27,10 +34,12 @@ class ReplicaSet:
     def addReplica(self, replica):
         assert isinstance(replica, Replica)
         self._addNode(replica)
+        return replica
 
     def addArbiter(self, arbiter):
         assert isinstance(arbiter, Arbiter)
         self._addNode(arbiter)
+        return arbiter
 
     def start(self):
         for node in self.nodes:
@@ -173,13 +182,20 @@ class Arbiter(Node):
     def setup(self):
         Node.setup(self)
 
+def runHelper(cmd, *args):
+    cmd = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        '..', '..', cmd)
+    argv = [cmd] + list(args)
+    return subprocess.Popen(argv, close_fds=True, shell=False)
+
 def test1():
     # Build and start our ReplicaSet
     replSet = ReplicaSet('test1')
-    replSet.addReplica(Replica('replica1', dbpath='test1/replica1'))
-    replSet.addReplica(Replica('replica2', dbpath='test1/replica2'))
-    replSet.addReplica(Replica('replica3', dbpath='test1/replica3'))
-    replSet.addArbiter(Arbiter('arbiter1', dbpath='test1/arbiter1'))
+    r1 = replSet.addReplica(Replica('replica1', dbpath='test1/replica1'))
+    r2 = replSet.addReplica(Replica('replica2', dbpath='test1/replica2'))
+    r3 = replSet.addReplica(Replica('replica3', dbpath='test1/replica3'))
+    a1 = replSet.addArbiter(Arbiter('arbiter1', dbpath='test1/arbiter1'))
 
     try:
         replSet.start()
@@ -188,8 +204,21 @@ def test1():
         print >> sys.stderr, 'Checking ReplicaSet state.'
         replSet.waitForHealthy()
         print >> sys.stderr, 'ReplicaSet is healthy.'
+
+        # Start running a test against the replica set.
+        p = runHelper('test-secondary', replSet.uri)
+        time.sleep(1)
+
+        # Kill a replicaSet node.
+        r2.kill()
+
+        # Tell test-secondary to run a few more tests then exit.
+        p.send_signal(signal.SIGUSR1)
     finally:
         replSet.kill()
 
 if __name__ == '__main__':
-    test1()
+    #test1()
+    p = runHelper('test-secondary', 'mongodb://localhost:27017/')
+    p.wait()
+    print p.returncode
