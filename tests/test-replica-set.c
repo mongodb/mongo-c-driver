d6748fa7954870f985c6c04bdaf6f6d1bf428d97
Christian Hergert <christian.hergert@10gen.com>
2013-09-27 14:03:17 -0700

    tests: cleanup.

--- tests/test-replica-set.c
+++ tests/test-replica-set.c
@@ -6,8 +6,8 @@
 static void
 test1 (void)
 {
+   __attribute__((unused)) ha_node_t *r1, *r2, *r3, *a1;
    ha_replica_set_t *replica_set;
-   ha_node_t *r1, *r2, *r3, *a1;
 
    replica_set = ha_replica_set_new("test1");
    r1 = ha_replica_set_add_replica(replica_set, "replica1");
@@ -16,15 +16,9 @@ test1 (void)
    a1 = ha_replica_set_add_arbiter(replica_set, "arbiter1");
 
    ha_replica_set_start(replica_set);
-
-   (void)r1;
-   (void)r2;
-   (void)r3;
-   (void)a1;
-
    ha_replica_set_wait_for_healthy(replica_set);
-
    ha_replica_set_shutdown(replica_set);
+   ha_replica_set_destroy(replica_set);
 }
 
 
