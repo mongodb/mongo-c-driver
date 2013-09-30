35b39ec98f1575e99156e8646e9a4ee80aa6b753
Christian Hergert <christian.hergert@10gen.com>
2013-09-30 16:29:19 -0700

    tests: add test using oversized buffer.

--- tests/test-mongoc-stream.c
+++ tests/test-mongoc-stream.c
@@ -5,6 +5,7 @@
 
 #include "mongoc-tests.h"
 
+
 static void
 test_buffered_basic (void)
 {
@@ -22,7 +23,37 @@ test_buffered_basic (void)
    stream = mongoc_stream_unix_new(fd);
 
    /* buffered assumes ownership of stream */
-   buffered = mongoc_stream_buffered_new(stream, 4096);
+   buffered = mongoc_stream_buffered_new(stream, 1024);
+
+   /* try to read large chunk larger than buffer. */
+   iov.iov_len = sizeof buf;
+   iov.iov_base = buf;
+   r = mongoc_stream_readv(buffered, &iov, 1, iov.iov_len, 0);
+   BSON_ASSERT(r == iov.iov_len);
+
+   /* cleanup */
+   mongoc_stream_destroy(buffered);
+}
+
+
+static void
+test_buffered_oversized (void)
+{
+   mongoc_stream_t *stream;
+   mongoc_stream_t *buffered;
+   ssize_t r;
+   struct iovec iov;
+   char buf[16236];
+   int fd;
+
+   fd = open("tests/binary/reply2.dat", O_RDONLY);
+   assert(fd != -1);
+
+   /* stream assumes ownership of fd */
+   stream = mongoc_stream_unix_new(fd);
+
+   /* buffered assumes ownership of stream */
+   buffered = mongoc_stream_buffered_new(stream, 20000);
 
    /* try to read large chunk larger than buffer. */
    iov.iov_len = sizeof buf;
@@ -39,6 +70,7 @@ main (int argc,
       char *argv[])
 {
    run_test("/mongoc/stream/buffered/basic", test_buffered_basic);
+   run_test("/mongoc/stream/buffered/oversized", test_buffered_oversized);
 
    return 0;
 }
