09e1e7b2d67d0f03ca36d1aee0b5d4090c5e2607
Christian Hergert <christian.hergert@10gen.com>
2013-10-11 12:22:04 -0700

    rpc: use BSON_ASSERT() macro instead of assert().

--- mongoc/mongoc-rpc.c
+++ mongoc/mongoc-rpc.c
@@ -36,20 +36,20 @@
 #define INT32_FIELD(_name) \
    iov.iov_base = (void *)&rpc->_name; \
    iov.iov_len = 4; \
-   assert(iov.iov_len); \
+   BSON_ASSERT(iov.iov_len); \
    rpc->msg_len += iov.iov_len; \
    mongoc_array_append_val(array, iov);
 #define INT64_FIELD(_name) \
    iov.iov_base = (void *)&rpc->_name; \
    iov.iov_len = 8; \
-   assert(iov.iov_len); \
+   BSON_ASSERT(iov.iov_len); \
    rpc->msg_len += iov.iov_len; \
    mongoc_array_append_val(array, iov);
 #define CSTRING_FIELD(_name) \
-   assert(rpc->_name); \
+   BSON_ASSERT(rpc->_name); \
    iov.iov_base = (void *)rpc->_name; \
    iov.iov_len = strlen(rpc->_name) + 1; \
-   assert(iov.iov_len); \
+   BSON_ASSERT(iov.iov_len); \
    rpc->msg_len += iov.iov_len; \
    mongoc_array_append_val(array, iov);
 #define BSON_FIELD(_name) \
@@ -59,7 +59,7 @@
       __l = BSON_UINT32_FROM_LE(__l); \
       iov.iov_base = (void *)rpc->_name; \
       iov.iov_len = __l; \
-      assert(iov.iov_len); \
+      BSON_ASSERT(iov.iov_len); \
       rpc->msg_len += iov.iov_len; \
       mongoc_array_append_val(array, iov); \
    } while (0);
@@ -68,24 +68,24 @@
 #define BSON_ARRAY_FIELD(_name) \
    iov.iov_base = (void *)rpc->_name; \
    iov.iov_len = rpc->_name##_len; \
-   assert(iov.iov_len); \
+   BSON_ASSERT(iov.iov_len); \
    rpc->msg_len += iov.iov_len; \
    mongoc_array_append_val(array, iov);
 #define RAW_BUFFER_FIELD(_name) \
    iov.iov_base = (void *)rpc->_name; \
    iov.iov_len = rpc->_name##_len; \
-   assert(iov.iov_len); \
+   BSON_ASSERT(iov.iov_len); \
    rpc->msg_len += iov.iov_len; \
    mongoc_array_append_val(array, iov);
 #define INT64_ARRAY_FIELD(_len, _name) \
    iov.iov_base = (void *)&rpc->_len; \
    iov.iov_len = 4; \
-   assert(iov.iov_len); \
+   BSON_ASSERT(iov.iov_len); \
    rpc->msg_len += iov.iov_len; \
    mongoc_array_append_val(array, iov); \
    iov.iov_base = (void *)rpc->_name; \
    iov.iov_len = rpc->_len * 8; \
-   assert(iov.iov_len); \
+   BSON_ASSERT(iov.iov_len); \
    rpc->msg_len += iov.iov_len; \
    mongoc_array_append_val(array, iov);
 
