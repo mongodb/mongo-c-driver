2e05df244bbbb535e885e4c31a2601f23b0c4de4
Christian Hergert <christian.hergert@10gen.com>
2013-11-22 14:42:04 -0800

    examples: add example of internal API to validate an RPC.

---
+++ examples/mongoc-rpc-validate.c
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2013 10gen Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/*
+ * This example can only be used internally to the library as it uses
+ * private features that are not exported in the public ABI. It does,
+ * however, illustrate some of the internals of the system.
+ */
+
+
+#include <fcntl.h>
+#include <mongoc.h>
+#include <mongoc-array-private.h>
+#include <mongoc-rpc-private.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+static void
+validate (const char *name,
+          int         fd)
+{
+   bson_uint8_t *buf;
+   mongoc_rpc_t rpc;
+   bson_int32_t len;
+   struct stat st;
+
+   if (fstat (fd, &st) != 0) {
+      fprintf (stderr, "%s: Failed to fstat.\n", name);
+      return;
+   }
+
+   if (st.st_size > (100 * 1024 * 1024)) {
+      fprintf (stderr, "%s: unreasonable message size\n", name);
+      return;
+   }
+
+   buf = malloc (st.st_size);
+   if (buf == NULL) {
+      fprintf (stderr, "%s: Failed to malloc %d bytes.\n",
+               name, (int)st.st_size);
+      return;
+   }
+
+   if (st.st_size != read (fd, buf, st.st_size)) {
+      fprintf (stderr, "%s: Failed to read %d bytes into buffer.\n",
+               name, (int)st.st_size);
+      goto cleanup;
+   }
+
+   memcpy (&len, buf, 4);
+   len = BSON_UINT32_FROM_LE (len);
+   if (len != st.st_size) {
+      fprintf (stderr, "%s is invalid. Invalid Length.\n", name);
+      goto cleanup;
+   }
+
+   if (!_mongoc_rpc_scatter (&rpc, buf, st.st_size)) {
+      fprintf (stderr, "%s is invalid. Invalid Format.\n", name);
+      goto cleanup;
+   }
+
+   fprintf (stdout, "%s is valid.\n", name);
+
+cleanup:
+   free (buf);
+}
+
+
+int
+main (int   argc,
+      char *argv[])
+{
+   int fd;
+   int i;
+
+   if (argc < 2) {
+      fprintf (stderr, "usage: %s FILE...\n", argv[0]);
+      return EXIT_FAILURE;
+   }
+
+   for (i = 1; i < argc; i++) {
+      fd = open (argv[i], O_RDONLY);
+      if (fd == -1) {
+         fprintf (stderr, "Failed to open \"%s\"\n", argv[i]);
+         continue;
+      }
+      validate (argv[i], fd);
+      close (fd);
+   }
+
+   return EXIT_SUCCESS;
+}
