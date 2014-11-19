8e3de2c37109e5a720eafd36b095b0fe9edc2235
Hannes Magnusson <bjori@mongodb.com>
2014-11-19 15:32:10 -0800

    CDRIVER-466: Add mongoc_cursor_get_id()
    These can be retrieved by calling currentOp, but its impossible to map
    them to specific cursor. We can just as well expose the id rather then
    having the user guessing
    
    Closes #122

--- src/mongoc/mongoc-cursor.c
+++ src/mongoc/mongoc-cursor.c
@@ -1055,3 +1055,10 @@ mongoc_cursor_get_hint (const mongoc_cursor_t *cursor)
    return cursor->hint;
 }
 
+uint64_t
+mongoc_cursor_get_id (const mongoc_cursor_t  *cursor)
+{
+   BSON_ASSERT(cursor);
+
+   return cursor->rpc.reply.cursor_id;
+}
