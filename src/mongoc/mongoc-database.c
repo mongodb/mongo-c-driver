9c450c34dbb8746bf9d321eefc7c4993c935c52d
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-05-11 14:18:45 -0400

    CDRIVER-642 detect auth error from usersInfo
    Starting with MongoDB 2.7.1 the scope of the localhost exception for
    authentication was dramatically narrowed. It is no longer possible to call the
    userInfo command unauthenticated, even when the localhost exception is in
    effect. A workaround for this issue is to catch the exception and call
    createUser if the error code is 13 (Unauthorized). Any exception from the
    createUser call should propagate to the user application.

--- src/mongoc/mongoc-database.c
+++ src/mongoc/mongoc-database.c
@@ -425,7 +425,8 @@ mongoc_database_add_user (mongoc_database_t *database,
 
    if (!ret && (lerror.code == MONGOC_ERROR_QUERY_COMMAND_NOT_FOUND)) {
       ret = mongoc_database_add_user_legacy (database, username, password, error);
-   } else if (ret) {
+   } else if (ret || (lerror.code == 13)) {
+      /* usersInfo succeeded or failed with auth err, we're on modern mongod */
       input = bson_strdup_printf ("%s:mongo:%s", username, password);
       hashed_password = _mongoc_hex_md5 (input);
       bson_free (input);
