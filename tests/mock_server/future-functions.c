1f0eada6071c29fcc31023478ad608b5ab80fe51
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-06-07 17:38:38 -0400

    generate mock_server2 functions with python, not macros
    I'd hacked up macros to generate functions for getting and setting
    values of various types for the future_t implementation, and for
    generating functions like future_cursor_next.
    
    The macro magic / hackery was very hard to understand, but it did (just
    barely) work in GCC and Clang. When it broke in MSVC, however, I saw I
    was in over my head. Generating the functions with Python is clear and
    maintainable.

--- tests/mock_server/future-functions.c
+++ tests/mock_server/future-functions.c
@@ -1,18 +1,3 @@
-/*
- * Copyright 2015 MongoDB, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 /*
  * Define two sets of functions. A function in the first set, like
@@ -27,131 +12,190 @@
 
 #include "future-functions.h"
 
-#undef TYPE_
-#undef TYPE
-#undef NAME_
-#undef NAME
 
-/* in future-functions.def we define params like "FUTURE_PARAM(type, name)",
- * these macros unpack type and name from the pair */
-#define TYPE_(X, Y) X
-#define TYPE(PAIR) TYPE_ PAIR
-#define NAME_(X, Y) Y
-#define NAME(PAIR) NAME_ PAIR
+static void *
+background_bulk_operation_execute (void *data)
+{
+   future_t *future = (future_t *) data;
 
-/* define functions like:
- *
- *    static void *
- *    background_mongoc_cursor_next (void *data)
- *    {
- *       future_t *future = (future_t *) data;
- *       future_t *copy = future_new_copy (future);
- *       future_value_t return_value;
- *
- *       future_value_set_bool (
- *           &return_value,
- *           mongoc_cursor_next (
- *              future_value_get_mongoc_cursor_ptr (&future->argv[0]),
- *              future_value_get_bson_ptr_ptr (&future->argv[2 - 1]));
- *
- *       future_destroy (copy);
- *       future_resolve (future, return_value);
- *
- *       return NULL;
- *    }
- *
- * The future is copied so we can unlock it while calling mongoc_cursor_next.
- */
+   /* copy the future so we can unlock it while calling
+    * bulk_operation_execute
+    */
+   future_t *copy = future_new_copy (future);
+   future_value_t return_value;
 
-#undef FUTURE_PARAM
-#undef FUTURE_GET
-#undef FUTURE_GET_LAST
-#undef FUTURE_FUNCTION
+   return_value.type = future_value_uint32_t_type;
+ 
+   future_value_set_uint32_t (
+      &return_value,
+      mongoc_bulk_operation_execute (
 
-#define FUTURE_PARAM(TYPE, NAME) (TYPE, NAME)
-#define FUTURE_GET_LAST(PARAM, i) XPASTE(future_value_get_, TYPE(PARAM)) (&future->argv[i])
-/* add comma */
-#define FUTURE_GET(PARAM, i) FUTURE_GET_LAST(PARAM, i),
+         future_value_get_mongoc_bulk_operation_ptr (future_get_param(copy, 0)),
+         future_value_get_bson_ptr (future_get_param(copy, 1)),
+         future_value_get_bson_error_ptr (future_get_param(copy, 2))
+   ));
+ 
+   future_destroy (copy);
+   future_resolve (future, return_value);
+ 
+   return NULL;
+}
 
-#define FUTURE_FUNCTION(RET_TYPE, FUTURE_FN, FN, ...) \
-   static void * \
-   background_ ## FN (void *data) \
-   { \
-      future_t *future = (future_t *) data; \
-      future_t *copy = future_new_copy (future); \
-      future_value_t return_value; \
-      \
-      future_value_set_ ## RET_TYPE ( \
-         &return_value, \
-         FN ( \
-            /* avoid trailing comma */ \
-            FOREACH_EXCEPT_LAST(FUTURE_GET, __VA_ARGS__) \
-            FUTURE_GET_LAST(LAST_ARG(__VA_ARGS__), ARGC(__VA_ARGS__) - 1) \
-      )); \
-      \
-      future_destroy (copy); \
-      future_resolve (future, return_value); \
-      \
-      return NULL; \
-   }
+static void *
+background_cursor_next (void *data)
+{
+   future_t *future = (future_t *) data;
 
+   /* copy the future so we can unlock it while calling
+    * cursor_next
+    */
+   future_t *copy = future_new_copy (future);
+   future_value_t return_value;
 
-#include "future-functions.def"
+   return_value.type = future_value_bool_type;
+ 
+   future_value_set_bool (
+      &return_value,
+      mongoc_cursor_next (
 
-#undef FUTURE_PARAM
-#undef FUTURE_GET
-#undef FUTURE_GET_LAST
-#undef FUTURE_FUNCTION
+         future_value_get_mongoc_cursor_ptr (future_get_param(copy, 0)),
+         future_value_get_const_bson_ptr_ptr (future_get_param(copy, 1))
+   ));
+ 
+   future_destroy (copy);
+   future_resolve (future, return_value);
+ 
+   return NULL;
+}
 
-#undef PARAM_DECL
-#undef LAST_PARAM_DECL
-#undef FUTURE_SET
-#undef FUTURE_FUNCTION
+static void *
+background_client_get_database_names (void *data)
+{
+   future_t *future = (future_t *) data;
 
-#define FUTURE_PARAM(TYPE, NAME) (TYPE, NAME)
-#define LAST_PARAM_DECL(PARAM) TYPE(PARAM) NAME(PARAM)
-/* add comma */
-#define PARAM_DECL(PARAM, i) LAST_PARAM_DECL(PARAM),
-#define FUTURE_SET(PARAM, i) XPASTE(future_value_set_, TYPE(PARAM)) (&future->argv[i], NAME(PARAM));
+   /* copy the future so we can unlock it while calling
+    * client_get_database_names
+    */
+   future_t *copy = future_new_copy (future);
+   future_value_t return_value;
 
-/* define functions like :
- *    future_t *
- *    future_cursor_next (mongoc_cursor_t *cursor,
- *                        bson_t **doc)
- *    {
- *       future_t *future = future_new (future_value_bool_type, 2);
- *       future_value_set_mongoc_cursor_ptr (&future->argv[0], cursor);
- *       future_value_set_bson_ptr_ptr (&future->argv[1], doc);
- *       future_start (future, background_mongoc_cursor_next);
- *       return future;
- *    }
- */
+   return_value.type = future_value_char_ptr_ptr_type;
+ 
+   future_value_set_char_ptr_ptr (
+      &return_value,
+      mongoc_client_get_database_names (
 
-#define FUTURE_FUNCTION(RET_TYPE, FUTURE_FN, FN, ...) \
-   future_t * \
-   FUTURE_FN ( \
-      /* avoid trailing comma */ \
-      FOREACH_EXCEPT_LAST(PARAM_DECL, __VA_ARGS__) \
-      LAST_PARAM_DECL(LAST_ARG(__VA_ARGS__)) \
-   ) \
-   { \
-      future_t *future = future_new (future_value_ ## RET_TYPE ## _type, \
-                                     ARGC(__VA_ARGS__)); \
-      FOREACH(FUTURE_SET, __VA_ARGS__) \
-      future_start (future, background_ ## FN); \
-      return future; \
-   }
+         future_value_get_mongoc_client_ptr (future_get_param(copy, 0)),
+         future_value_get_bson_error_ptr (future_get_param(copy, 1))
+   ));
+ 
+   future_destroy (copy);
+   future_resolve (future, return_value);
+ 
+   return NULL;
+}
+
+static void *
+background_database_get_collection_names (void *data)
+{
+   future_t *future = (future_t *) data;
+
+   /* copy the future so we can unlock it while calling
+    * database_get_collection_names
+    */
+   future_t *copy = future_new_copy (future);
+   future_value_t return_value;
+
+   return_value.type = future_value_char_ptr_ptr_type;
+ 
+   future_value_set_char_ptr_ptr (
+      &return_value,
+      mongoc_database_get_collection_names (
+
+         future_value_get_mongoc_database_ptr (future_get_param(copy, 0)),
+         future_value_get_bson_error_ptr (future_get_param(copy, 1))
+   ));
+ 
+   future_destroy (copy);
+   future_resolve (future, return_value);
+ 
+   return NULL;
+}
 
 
-#include "future-functions.def"
 
-#undef FUTURE_PARAM
-#undef PARAM_DECL
-#undef LAST_PARAM_DECL
-#undef FUTURE_SET
-#undef FUTURE_FUNCTION
+future_t *
+future_bulk_operation_execute (
+   mongoc_bulk_operation_ptr bulk,
+   bson_ptr reply,
+   bson_error_ptr error)
+{
+   future_t *future = future_new (future_value_uint32_t_type,
+                                  3);
+   
+   future_value_set_mongoc_bulk_operation_ptr (
+      future_get_param (future, 0), bulk);
+   
+   future_value_set_bson_ptr (
+      future_get_param (future, 1), reply);
+   
+   future_value_set_bson_error_ptr (
+      future_get_param (future, 2), error);
+   
+   future_start (future, background_bulk_operation_execute);
+   return future;
+}
 
-#undef TYPE_
-#undef TYPE
-#undef NAME_
-#undef NAME
+future_t *
+future_cursor_next (
+   mongoc_cursor_ptr cursor,
+   const_bson_ptr_ptr doc)
+{
+   future_t *future = future_new (future_value_bool_type,
+                                  2);
+   
+   future_value_set_mongoc_cursor_ptr (
+      future_get_param (future, 0), cursor);
+   
+   future_value_set_const_bson_ptr_ptr (
+      future_get_param (future, 1), doc);
+   
+   future_start (future, background_cursor_next);
+   return future;
+}
+
+future_t *
+future_client_get_database_names (
+   mongoc_client_ptr client,
+   bson_error_ptr error)
+{
+   future_t *future = future_new (future_value_char_ptr_ptr_type,
+                                  2);
+   
+   future_value_set_mongoc_client_ptr (
+      future_get_param (future, 0), client);
+   
+   future_value_set_bson_error_ptr (
+      future_get_param (future, 1), error);
+   
+   future_start (future, background_client_get_database_names);
+   return future;
+}
+
+future_t *
+future_database_get_collection_names (
+   mongoc_database_ptr database,
+   bson_error_ptr error)
+{
+   future_t *future = future_new (future_value_char_ptr_ptr_type,
+                                  2);
+   
+   future_value_set_mongoc_database_ptr (
+      future_get_param (future, 0), database);
+   
+   future_value_set_bson_error_ptr (
+      future_get_param (future, 1), error);
+   
+   future_start (future, background_database_get_collection_names);
+   return future;
+}
