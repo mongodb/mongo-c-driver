mongodb/mongo-c-driver

commit a25ee18a2bb398e56c1cba45f466febf7a9b1ee8

CDRIVER-528: Use maxWriteBatchSize from ismaster for write command batch splitting.

src/mongoc/mongoc-write-command.c
@@ -29,8 +29,6 @@
  */
 
 #define WRITE_COMMAND_WIRE_VERSION 2
-// TODO: create a mongoc_cluster_node_max_batch_size, CDRIVER-528
-#define MAX_BATCH_SIZE 1000
 
 #define WRITE_CONCERN_DOC(wc) \
    (wc && _mongoc_write_concern_needs_gle ((wc))) ? \
@@ -358,7 +356,7 @@ _mongoc_write_command_insert_legacy (mongoc_write_command_t       *command,
    char ns [MONGOC_NAMESPACE_MAX + 1];
    bool r;
    uint32_t i;
-   int max_insert_batch = MAX_BATCH_SIZE;
+   int32_t max_insert_batch;
    int32_t max_msg_size;
    int32_t max_bson_obj_size;
 
@@ -416,6 +414,7 @@ again:
 
       max_bson_obj_size = mongoc_cluster_node_max_bson_obj_size(&client->cluster, hint);
       max_msg_size = mongoc_cluster_node_max_msg_size (&client->cluster, hint);
+      max_insert_batch = mongoc_cluster_node_max_write_batch_size(&client->cluster, hint);
 
       /*
        * Check that the server can receive this document.
@@ -709,7 +708,7 @@ _mongoc_write_command_delete (mongoc_write_command_t       *command,
    bool ret = false;
    uint32_t i;
    int32_t min_wire_version;
-   int32_t max_delete_batch = MAX_BATCH_SIZE;
+   int32_t max_delete_batch;
    int32_t max_bson_obj_size;
    uint32_t key_len;
 
@@ -754,6 +753,7 @@ _mongoc_write_command_delete (mongoc_write_command_t       *command,
    }
 
    max_bson_obj_size = mongoc_cluster_node_max_bson_obj_size(&client->cluster, hint);
+   max_delete_batch = mongoc_cluster_node_max_write_batch_size(&client->cluster, hint);
 
 again:
    bson_init (&cmd);
@@ -849,7 +849,7 @@ _mongoc_write_command_insert (mongoc_write_command_t       *command,
    bool has_more;
    bool ret = false;
    uint32_t i;
-   int32_t max_insert_batch = MAX_BATCH_SIZE;
+   int32_t max_insert_batch;
    int32_t min_wire_version;
    int32_t max_bson_obj_size;
    uint32_t key_len;
@@ -864,6 +864,7 @@ _mongoc_write_command_insert (mongoc_write_command_t       *command,
    BSON_ASSERT (collection);
 
    max_bson_obj_size = mongoc_cluster_node_max_bson_obj_size(&client->cluster, hint);
+   max_insert_batch = mongoc_cluster_node_max_write_batch_size(&client->cluster, hint);
 
    /*
     * If we have an unacknowledged write and the server supports the legacy
@@ -997,7 +998,7 @@ _mongoc_write_command_update (mongoc_write_command_t       *command,
    char str [16];
    bool ret = false;
    int32_t min_wire_version;
-   int32_t max_update_batch = MAX_BATCH_SIZE;
+   int32_t max_update_batch;
    int32_t max_bson_obj_size;   bool has_more;
    uint32_t i;
    uint32_t key_len;
@@ -1042,6 +1043,7 @@ _mongoc_write_command_update (mongoc_write_command_t       *command,
    }
 
    max_bson_obj_size = mongoc_cluster_node_max_bson_obj_size(&client->cluster, hint);
+   max_update_batch = mongoc_cluster_node_max_write_batch_size(&client->cluster, hint);
 
 again:
    bson_init (&cmd);

commit 9d374015938fb5aef30e3f6933e9ed3d31d491df

CDRIVER-713 send bulk OP_INSERTs one at a time
Restores spec'ed behavior of mongoc_bulk_operation_insert that was broken in
a25ee18a. mongoc_bulk_operation_insert should send OP_INSERT one at a time in
order to do individual error reporting:

https://github.com/mongodb/specifications/blob/master/source/driver-bulk-update.rst#pre-2-6-support

src/mongoc/mongoc-write-command.c
@@ -387,6 +387,10 @@ _mongoc_write_command_insert_legacy (mongoc_write_command_t       *command,
    BSON_ASSERT (collection);
    BSON_ASSERT (command->type == MONGOC_WRITE_COMMAND_INSERT);
 
+   max_bson_obj_size = mongoc_cluster_node_max_bson_obj_size(&client->cluster, hint);
+   max_msg_size = mongoc_cluster_node_max_msg_size (&client->cluster, hint);
+   max_insert_batch = mongoc_cluster_node_max_write_batch_size(&client->cluster, hint);
+
    if (command->ordered || !command->u.insert.allow_bulk_op_insert) {
       max_insert_batch = 1;
    }
@@ -429,10 +433,6 @@ again:
 
       BSON_ASSERT (data);
       BSON_ASSERT (len >= 5);
-
-      max_bson_obj_size = mongoc_cluster_node_max_bson_obj_size(&client->cluster, hint);
-      max_msg_size = mongoc_cluster_node_max_msg_size (&client->cluster, hint);
-      max_insert_batch = mongoc_cluster_node_max_write_batch_size(&client->cluster, hint);
 
       /*
        * Check that the server can receive this document.