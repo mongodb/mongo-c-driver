5fa8774e947806a8b9600f81c56a31d88aaa31e4
Jason Carey <jcarey@argv.me>
2015-07-27 14:14:38 -0400

    Fix memory leak in mongoc_uri_parse_option

--- src/mongoc/mongoc-uri.c
+++ src/mongoc/mongoc-uri.c
@@ -494,18 +494,19 @@ mongoc_uri_parse_option (mongoc_uri_t *uri,
 {
    int32_t v_int;
    const char *end_key;
-   char *key;
-   char *value;
+   char *key = NULL;
+   char *value = NULL;
+   bool ret = false;
 
    if (!(key = scan_to_unichar(str, '=', "", &end_key))) {
-      return false;
+      goto CLEANUP;
    }
 
    value = bson_strdup(end_key + 1);
    mongoc_uri_do_unescape(&value);
    if (!value) {
       /* do_unescape detected invalid UTF-8 and freed value */
-      return false;
+      goto CLEANUP;
    }
 
    if (!strcasecmp(key, "connecttimeoutms") ||
@@ -543,18 +544,19 @@ mongoc_uri_parse_option (mongoc_uri_t *uri,
       bson_append_utf8(&uri->credentials, key, -1, value, -1);
    } else if (!strcasecmp(key, "authmechanismproperties")) {
       if (!mongoc_uri_parse_auth_mechanism_properties(uri, value)) {
-         bson_free(key);
-         bson_free(value);
-         return false;
+         goto CLEANUP;
       }
    } else {
       bson_append_utf8(&uri->options, key, -1, value, -1);
    }
 
+   ret = true;
+
+CLEANUP:
    bson_free(key);
    bson_free(value);
 
-   return true;
+   return ret;
 }
 
 
