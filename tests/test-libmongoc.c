db5852c159a70f91991e7e9239f8ec25ea5571a4
A. Jesse Jiryu Davis <jesse@mongodb.com>
2015-08-12 16:57:18 -0400

    add a test for mongoc_cond_timedwait

--- tests/test-libmongoc.c
+++ tests/test-libmongoc.c
@@ -50,6 +50,7 @@ extern void test_server_selection_install     (TestSuite *suite);
 extern void test_set_install                  (TestSuite *suite);
 extern void test_socket_install               (TestSuite *suite);
 extern void test_stream_install               (TestSuite *suite);
+extern void test_thread_install               (TestSuite *suite);
 extern void test_topology_install             (TestSuite *suite);
 extern void test_topology_reconcile_install   (TestSuite *suite);
 extern void test_topology_scanner_install     (TestSuite *suite);
@@ -918,6 +919,7 @@ main (int   argc,
    test_server_selection_install (&suite);
    test_set_install (&suite);
    test_stream_install (&suite);
+   test_thread_install (&suite);
    test_topology_install (&suite);
    test_uri_install (&suite);
    test_usleep_install (&suite);
