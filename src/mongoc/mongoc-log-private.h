5cd61e3170cfe81e4bc9be12d4b2d86e4e1ca53e
Remi Collet <fedora@famillecollet.com>
2015-08-31 18:48:15 +0200

    allow driver to use mongoc-log-private

--- src/mongoc/mongoc-log-private.h
+++ src/mongoc/mongoc-log-private.h
@@ -17,7 +17,7 @@
 #ifndef MONGOC_LOG_PRIVATE_H
 #define MONGOC_LOG_PRIVATE_H
 
-#if !defined (MONGOC_INSIDE) && !defined (MONGOC_COMPILATION)
+#if !defined (MONGOC_INSIDE) && !defined (MONGOC_COMPILATION) && !defined (MONGOC_I_AM_A_DRIVER)
 # error "Only <mongoc.h> can be included directly."
 #endif
 
