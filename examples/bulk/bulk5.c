66df1c416d0e68256b096324825b464ca126c4a5
Hannes Magnusson <bjori@php.net>
2015-11-23 15:04:00 -0800

    CDRIVER-1009: Document mongoc_bulk_operation_set_bypass_document_validation

--- examples/bulk/bulk5.c
+++ examples/bulk/bulk5.c
@@ -4,9 +4,8 @@
 #include <stdio.h>
 
 static void
-bulk5 (mongoc_collection_t *collection)
+bulk5_fail (mongoc_collection_t *collection)
 {
-   mongoc_write_concern_t *wc;
    mongoc_bulk_operation_t *bulk;
    bson_error_t error;
    bson_t *doc;
@@ -14,11 +13,44 @@ bulk5 (mongoc_collection_t *collection)
    char *str;
    bool ret;
 
-   wc = mongoc_write_concern_new ();
-   mongoc_write_concern_set_w (wc, 2);
-   mongoc_write_concern_set_wtimeout (wc, 100);  /* milliseconds */
+   bulk = mongoc_collection_create_bulk_operation (collection, true, NULL);
 
-   bulk = mongoc_collection_create_bulk_operation (collection, true, wc);
+   /* Two inserts */
+   doc = BCON_NEW ("_id", BCON_INT32 (31));
+   mongoc_bulk_operation_insert (bulk, doc);
+   bson_destroy (doc);
+
+   doc = BCON_NEW ("_id", BCON_INT32 (32));
+   mongoc_bulk_operation_insert (bulk, doc);
+   bson_destroy (doc);
+
+   /* The above documents do not comply to the schema validation rules
+    * we created previously, so this will result in an error */
+   ret = mongoc_bulk_operation_execute (bulk, &reply, &error);
+
+   str = bson_as_json (&reply, NULL);
+   printf ("%s\n", str);
+   bson_free (str);
+
+   if (!ret) {
+      printf ("Error: %s\n", error.message);
+   }
+
+   bson_destroy (&reply);
+   mongoc_bulk_operation_destroy (bulk);
+}
+
+static void
+bulk5_success (mongoc_collection_t *collection)
+{
+   mongoc_bulk_operation_t *bulk;
+   bson_error_t error;
+   bson_t *doc;
+   bson_t reply;
+   char *str;
+   bool ret;
+
+   bulk = mongoc_collection_create_bulk_operation (collection, true, NULL);
 
    /* Allow this document to bypass document validation.
     * NOTE: When authentication is enabled, the authenticated user must have
@@ -46,7 +78,6 @@ bulk5 (mongoc_collection_t *collection)
 
    bson_destroy (&reply);
    mongoc_bulk_operation_destroy (bulk);
-   mongoc_write_concern_destroy (wc);
 }
 
 int
@@ -62,13 +93,15 @@ main (int argc,
    mongoc_init ();
 
    client = mongoc_client_new ("mongodb://localhost/");
-   database = mongoc_client_get_database (client, "test");
+   database = mongoc_client_get_database (client, "testasdf");
 
+   /* Create schema validator */
    options = BCON_NEW ("validator", "{", "number", "{", "$gte", BCON_INT32 (5), "}", "}");
    collection = mongoc_database_create_collection (database, "collname", options, &error);
 
    if (collection) {
-      bulk5 (collection);
+      bulk5_fail (collection);
+      bulk5_success (collection);
       mongoc_collection_destroy (collection);
    } else {
       fprintf(stderr, "Couldn't create collection: '%s'\n", error.message);
