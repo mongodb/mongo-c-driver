9863d62f1c6bab2c4467236ad6dc0a57083105cd
Hannes Magnusson <bjori@php.net>
2016-11-07 15:28:52 -0800

    CDRIVER-671 Build against system provided libbson

--- .evergreen/compile-unix.sh
+++ .evergreen/compile-unix.sh
@@ -11,6 +11,7 @@ set -o errexit  # Exit the script with error if any of the commands fail
 #       CC                      Which compiler to use
 #       ANALYZE                 Run the build through clangs scan-build
 #       COVERAGE                Produce code coverage reports
+#       LIBBSON                 Build against bundled or external libbson
 #       EXTRA_CONFIGURE_FLAGS   Extra configure flags to use
 
 RELEASE=${RELEASE:-no}
@@ -39,6 +40,8 @@ echo "OS: $OS"
 # as an environment variable (e.g. to force 32bit)
 [ -z "$MARCH" ] && MARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
 
+INSTALL_DIR=$(pwd)/install-dir
+
 # Default configure flags for debug builds and release builds
 DEBUG_FLAGS="\
    --enable-html-docs=no \
@@ -51,7 +54,7 @@ DEBUG_FLAGS="\
    --enable-debug \
    --disable-silent-rules \
    --disable-automatic-init-and-cleanup \
-   --with-libbson=bundled \
+   --prefix=$INSTALL_DIR \
 "
 
 RELEASE_FLAGS="\
@@ -60,8 +63,15 @@ RELEASE_FLAGS="\
    --enable-extra-align=no \
    --enable-optimizations \
    --disable-automatic-init-and-cleanup \
-   --with-libbson=bundled \
+   --prefix=$INSTALL_DIR \
 "
+if [ "$LIBBSON" = "external" ]; then
+   RELEASE_FLAGS="$RELEASE_FLAGS --with-libbson=system"
+   DEBUG_FLAGS="$DEBUG_FLAGS --with-libbson=system"
+else
+   RELEASE_FLAGS="$RELEASE_FLAGS --with-libbson=bundled"
+   DEBUG_FLAGS="$DEBUG_FLAGS --with-libbson=bundled"
+fi
 
 # By default we build from git clone, which requires autotools
 # This gets overwritten if we detect we should use the release archive
@@ -153,7 +163,22 @@ export MONGOC_TEST_FUTURE_TIMEOUT_MS=30000
 export MONGOC_TEST_SKIP_LIVE=on
 export MONGOC_TEST_SKIP_SLOW=on
 
-CFLAGS="$CFLAGS" CC="$CC" $SCAN_BUILD $CONFIGURE_SCRIPT $CONFIGURE_FLAGS
+export CFLAGS="$CFLAGS"
+export CC="$CC"
+
+if [ "$LIBBSON" = "external" ]; then
+   # This usually happens in mongoc ./autogen.sh, but since we are compiling against
+   # external libbson we need to install libbson before running mongoc ./autogen.sh
+   git submodule update --init
+   cd src/libbson
+   ./autogen.sh $CONFIGURE_FLAGS
+   make all
+   make install
+   cd ../../
+   export PKG_CONFIG_PATH=$INSTALL_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
+fi
+
+$SCAN_BUILD $CONFIGURE_SCRIPT $CONFIGURE_FLAGS
 $SCAN_BUILD make all
 $SCAN_BUILD make $TARGET TEST_ARGS="-d -F test-results.json"
 
