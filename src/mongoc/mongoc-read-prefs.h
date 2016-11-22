1a0f9a9c9c665a36f62f782fd383470e780f6590
A. Jesse Jiryu Davis <jesse@mongodb.com>
2016-11-21 22:17:55 -0500

    CDRIVER-1933 smallest maxStalenessSeconds is 90
    Revert code to parse "idleWritePeriodMillis" from ismaster, and revert
    changes that allowed maxStalenessSeconds to be a float. Sync JSON tests
    from specs repo.

--- src/mongoc/mongoc-read-prefs.h
+++ src/mongoc/mongoc-read-prefs.h
@@ -28,6 +28,9 @@
 BSON_BEGIN_DECLS
 
 
+#define MONGOC_NO_MAX_STALENESS -1
+#define MONGOC_SMALLEST_MAX_STALENESS_SECONDS 90
+
 typedef struct _mongoc_read_prefs_t mongoc_read_prefs_t;
 
 
@@ -61,10 +64,10 @@ BSON_API
 void                 mongoc_read_prefs_add_tag                   (mongoc_read_prefs_t       *read_prefs,
                                                                   const bson_t              *tag);
 BSON_API
-double               mongoc_read_prefs_get_max_staleness_seconds (const mongoc_read_prefs_t *read_prefs);
+int64_t              mongoc_read_prefs_get_max_staleness_seconds (const mongoc_read_prefs_t *read_prefs);
 BSON_API
 void                 mongoc_read_prefs_set_max_staleness_seconds (mongoc_read_prefs_t       *read_prefs,
-                                                                  double                     max_staleness_seconds);
+                                                                  int64_t                    max_staleness_seconds);
 BSON_API
 bool                 mongoc_read_prefs_is_valid                  (const mongoc_read_prefs_t *read_prefs);
 
