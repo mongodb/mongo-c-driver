c63a60e2f611f30a71afc6c30b20f652ed42b960
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-01-05 06:05:23 -0500

    CDRIVER-1979 sphinx documentation config and theme

---
+++ doc/conf.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+import os.path
+import sys
+
+# Ensure we can import "mongoc" and "taglist" extension modules.
+sys.path.append(os.path.dirname(__file__))
+
+extensions = [
+    'mongoc',
+    'githubpages',
+    'taglist',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    # TODO: update to mongoc.org once libbson 1.6.0 is released.
+    'bson': ('https://s3.amazonaws.com/mciuploads/libbson/docs/latest/',
+             'libbson-objects.inv'),
+}
+
+# General information about the project.
+project = 'MongoDB C Driver'
+copyright = '2017, MongoDB, Inc'
+author = 'MongoDB, Inc'
+
+version_path = os.path.join(os.path.dirname(__file__), '..', 'VERSION_CURRENT')
+version = open(version_path).read().strip()
+release_path = os.path.join(os.path.dirname(__file__), '..', 'VERSION_RELEASED')
+release = open(release_path).read().strip()
+release_major, release_minor, release_patch = release.split('.')
+rst_prolog = """
+.. |release_major| replace:: %(release_major)s
+
+.. |release_minor| replace:: %(release_minor)s
+
+.. |release_patch| replace:: %(release_patch)s
+
+.. |release_download| replace:: https://github.com/mongodb/mongo-c-driver/releases/download/%(release)s/mongo-c-driver-%(release)s.tar.gz
+""" % locals()
+
+language = 'en'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+master_doc = 'index'
+
+# -- Options for HTML output ----------------------------------------------
+
+html_theme_path = ['.']
+html_theme = 'mongoc-theme'
+html_title = html_shorttitle = 'MongoDB C Driver %s' % version
+# html_favicon = None
+html_use_smartypants = False
+html_sidebars = {
+    '**': ['globaltoc.html'],
+    'errors': [],  # Make more room for the big table.
+}
+html_show_sourcelink = False
+
+# Note: http://www.sphinx-doc.org/en/1.5.1/config.html#confval-html_copy_source
+# This will degrade the Javascript quicksearch if we ever use it.
+html_copy_source = False
+
+# -- Options for manual page output ---------------------------------------
+
+# HACK: Just trick Sphinx's ManualPageBuilder into thinking there are pages
+# configured - we'll do it dynamically in process_nodes.
+man_pages = [True]
+
+# If true, show URL addresses after external links.
+#
+# man_show_urls = False
+from docutils.nodes import title
+
+
+# To publish HTML docs at GitHub Pages, create .nojekyll file. In Sphinx 1.4 we
+# could use the githubpages extension, but old Ubuntu still has Sphinx 1.3.
+def create_nojekyll(app, env):
+    if app.builder.format == 'html':
+        path = os.path.join(app.builder.outdir, '.nojekyll')
+        open(path, 'wt').close()
+
+
+def setup(app):
+    app.connect('doctree-read', process_nodes)
+    app.connect('env-updated', create_nojekyll)
+
+
+def process_nodes(app, doctree):
+    if man_pages == [True]:
+        man_pages.pop()
+
+    env = app.env
+    metadata = env.metadata[env.docname]
+
+    # A page like installing.rst sets its name with ":man_page: mongoc_installing"
+    page_name = metadata.get('man_page', env.docname)
+    page_title = find_node(doctree, title)
+
+    man_pages.append((env.docname, page_name, page_title.astext(), [author], 3))
+
+
+def find_node(doctree, klass):
+    matches = doctree.traverse(lambda node: isinstance(node, klass))
+    if not matches:
+        raise IndexError("No %s in %s" % (klass, doctree))
+
+    return matches[0]
---
+++ doc/mongoc/__init__.py
@@ -0,0 +1,30 @@
+from docutils.nodes import literal
+from docutils.parsers.rst import roles
+
+from sphinx.roles import XRefRole
+
+
+class SymbolRole(XRefRole):
+    def __call__(self, *args, **kwargs):
+        nodes, messages = XRefRole.__call__(self, *args, **kwargs)
+        for node in nodes:
+            attrs = node.attributes
+            target = attrs['reftarget']
+            if ':' in target:
+                # E.g., 'bson:bson_t' has domain 'bson', target 'bson_t'
+                attrs['domain'], attrs['reftarget'] = target.split(':', 1)
+
+            attrs['reftype'] = 'doc'
+            attrs['classes'].append('symbol')
+        return nodes, messages
+
+
+def setup(app):
+    roles.register_local_role(
+        'symbol', SymbolRole(warn_dangling=True, innernodeclass=literal))
+
+    return {
+        'version': '1.0',
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
