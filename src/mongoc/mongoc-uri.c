0bce8b476ce0def8e9021b96436e816d2bf933dc
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-01-11 15:26:25 -0500

    Revert "CDRIVER-1972 Support for ipv6 hostnames"
    8729c1448782481f392e4b51e513c14bb9736a5b
    
    Fixes CDRIVER-1988.

--- src/mongoc/mongoc-uri.c
+++ src/mongoc/mongoc-uri.c
@@ -102,7 +102,7 @@ mongoc_uri_append_host (mongoc_uri_t  *uri,
    } else {
       bson_snprintf (link_->host_and_port, sizeof link_->host_and_port,
                      "%s:%hu", host, port);
-      link_->family = strstr (host, ".sock") ? AF_UNIX : AF_UNSPEC;
+      link_->family = strstr (host, ".sock") ? AF_UNIX : AF_INET;
    }
    link_->host_and_port[sizeof link_->host_and_port - 1] = '\0';
    link_->port = port;
