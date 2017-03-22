258bf0582a45936c0ab29a38b0bf97643ba53eec
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-03-21 23:10:38 -0400

    typo in sphinx config

--- doc/conf.py
+++ doc/conf.py
@@ -81,7 +81,7 @@ def create_nojekyll(app, env):
 
 
 def add_ga_javascript(app, pagename, templatename, context, doctree):
-    context['metatags'] = context.get('metatags', '') + ''"""<script>
+    context['metatags'] = context.get('metatags', '') + """<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
