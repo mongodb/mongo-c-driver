620349741341c964f8b40595b98fea63bf90efb0
Hannes Magnusson <bjori@php.net>
2017-07-07 09:40:34 -0700

    Fix segfault when tracing and tags don't match

--- src/mongoc/mongoc-server-description.c
+++ src/mongoc/mongoc-server-description.c
@@ -880,11 +880,10 @@ mongoc_server_description_filter_tags (
 
       if (found) {
          for (i = 0; i < description_len; i++) {
-            if (!sd_matched[i]) {
+            if (!sd_matched[i] && descriptions[i]) {
                TRACE ("Rejected [%s] [%s], doesn't match tags",
                       mongoc_server_description_type (descriptions[i]),
                       descriptions[i]->host.host_and_port);
-
                descriptions[i] = NULL;
             }
          }
