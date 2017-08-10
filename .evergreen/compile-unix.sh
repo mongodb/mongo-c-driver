28a848277f38ce08506dd09b89dc7ea84cc0f409
Hannes Magnusson <bjori@php.net>
2017-08-10 08:45:59 -0700

    CDRIVER-2235 OpenSSL 1.0.0 version check sometimes fails in Evergreen

--- .evergreen/compile-unix.sh
+++ .evergreen/compile-unix.sh
@@ -34,54 +34,6 @@ echo "CC: $CC"
 echo "ANALYZE: $ANALYZE"
 echo "COVERAGE: $COVERAGE"
 
-install_openssl_fips() {
-   curl --retry 5 -o fips.tar.gz https://www.openssl.org/source/openssl-fips-2.0.14.tar.gz
-   tar zxvf fips.tar.gz
-   cd openssl-fips-2.0.14
-   ./config --prefix=$INSTALL_DIR -fPIC
-   cpus=$(grep -c '^processor' /proc/cpuinfo)
-   make -j${cpus} || true
-   make install || true
-   cd ..
-   SSL_EXTRA_FLAGS="--openssldir=$INSTALL_DIR --with-fipsdir=$INSTALL_DIR fips"
-   export OPENSSL_FIPS=1
-   SSL=${SSL%-fips}
-}
-install_openssl () {
-   SSL_VERSION=${SSL##openssl-}
-   tmp=$(echo $SSL_VERSION | tr . _)
-   curl -L --retry 5 -o ssl.tar.gz https://github.com/openssl/openssl/archive/OpenSSL_${tmp}.tar.gz
-   tar zxvf ssl.tar.gz
-   cd openssl-OpenSSL_$tmp
-   ./config --prefix=$INSTALL_DIR $SSL_EXTRA_FLAGS shared -fPIC
-   cpus=$(grep -c '^processor' /proc/cpuinfo)
-   make -j32 || true
-   make -j${cpus} || true
-   make install || true
-   cd ..
-
-   # x505_vfy.h has issues in 1.1.0e
-   export CFLAGS=-Wno-redundant-decls
-   export PKG_CONFIG_PATH=$INSTALL_DIR/lib/pkgconfig
-   export EXTRA_LIB_PATH="$INSTALL_DIR/lib"
-   SSL="openssl";
-}
-
-install_libressl () {
-   SSL_VERSION=${SSL##libressl-}
-   curl --retry 5 -o ssl.tar.gz https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/$SSL.tar.gz
-   tar zxvf ssl.tar.gz
-   cd $SSL
-   ./configure --prefix=$INSTALL_DIR
-   cpus=$(grep -c '^processor' /proc/cpuinfo)
-   make -j${cpus}
-   make install
-   cd ..
-
-   export PKG_CONFIG_PATH=$INSTALL_DIR/lib/pkgconfig
-   export EXTRA_LIB_PATH="$INSTALL_DIR/lib"
-   SSL="libressl";
-}
 # Get the kernel name, lowercased
 OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 echo "OS: $OS"
@@ -89,21 +41,6 @@ echo "OS: $OS"
 # Automatically retrieve the machine architecture, lowercase, unless provided
 # as an environment variable (e.g. to force 32bit)
 [ -z "$MARCH" ] && MARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
-
-case "$SSL" in
-   openssl-*-fips)
-      install_openssl_fips;
-      install_openssl;
-   ;;
-
-   openssl-*)
-      install_openssl;
-  ;;
-
-  libressl-*)
-     install_libressl;
-     ;;
-esac
 
 # Default configure flags for debug builds and release builds
 DEBUG_FLAGS="\
@@ -263,8 +200,8 @@ if [ "$LIBBSON" = "external" ]; then
    make all
    make install
    cd ../../
-   export PKG_CONFIG_PATH=$INSTALL_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 fi
+export PKG_CONFIG_PATH=$INSTALL_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
 export PATH=$INSTALL_DIR/bin:$PATH
 echo "OpenSSL Version:"
