cad0d26935d9d3534e34944bfa6127a157f5d292
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-08-23 03:57:21 -0400

    update google analytics on home page

--- homepage-config/conf.py
+++ homepage-config/conf.py
@@ -15,7 +15,6 @@ master_doc = 'index'
 project = u'mongoc.org'
 copyright = u'2017, A. Jesse Jiryu Davis'
 author = u'A. Jesse Jiryu Davis'
-googleanalytics_id = 'UA-92642455-1'
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
@@ -48,15 +47,12 @@ def download_link(typ, rawtext, text, lineno, inliner, options={}, content=[]):
 
 def add_ga_javascript(app, pagename, templatename, context, doctree):
     context['metatags'] = context.get('metatags', '') + ''"""<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', '%s', 'auto');
-  ga('send', 'pageview');
-
-</script>""" % googleanalytics_id
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(
+      {'gtm.start': new Date().getTime(),event:'gtm.js'}
+    );var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-JQHP');</script>"""
 
 
 def setup(app):
