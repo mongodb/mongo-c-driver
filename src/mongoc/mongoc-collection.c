21a22353f2773e0100bb8596849ce60c9d5abcea
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-09-01 10:44:14 -0400

    CDRIVER-2213 findAndModify respects writeConcern opt

--- src/mongoc/mongoc-collection.c
+++ src/mongoc/mongoc-collection.c
@@ -2526,19 +2526,22 @@ mongoc_collection_find_and_modify_with_opts (
       BSON_APPEND_INT32 (&command, "maxTimeMS", opts->max_time_ms);
    }
 
-   if (server_stream->sd->max_wire_version >= WIRE_VERSION_FAM_WRITE_CONCERN) {
-      if (!mongoc_write_concern_is_valid (collection->write_concern)) {
-         bson_set_error (error,
-                         MONGOC_ERROR_COMMAND,
-                         MONGOC_ERROR_COMMAND_INVALID_ARG,
-                         "The write concern is invalid.");
-         bson_destroy (&command);
-         mongoc_server_stream_cleanup (server_stream);
-         RETURN (false);
-      }
+   if (!bson_has_field (&opts->extra, "writeConcern")) {
+      if (server_stream->sd->max_wire_version >=
+          WIRE_VERSION_FAM_WRITE_CONCERN) {
+         if (!mongoc_write_concern_is_valid (collection->write_concern)) {
+            bson_set_error (error,
+                            MONGOC_ERROR_COMMAND,
+                            MONGOC_ERROR_COMMAND_INVALID_ARG,
+                            "The write concern is invalid.");
+            bson_destroy (&command);
+            mongoc_server_stream_cleanup (server_stream);
+            RETURN (false);
+         }
 
-      if (mongoc_write_concern_is_acknowledged (collection->write_concern)) {
-         _BSON_APPEND_WRITE_CONCERN (&command, collection->write_concern);
+         if (mongoc_write_concern_is_acknowledged (collection->write_concern)) {
+            _BSON_APPEND_WRITE_CONCERN (&command, collection->write_concern);
+         }
       }
    }
 
