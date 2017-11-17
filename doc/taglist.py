27b63306c20317a4132326c8715727b8fe5448ea
A. Jesse Jiryu Davis <jesse@mongodb.com>
2017-11-17 16:14:17 -0500

    enable parallel sphinx-build

--- doc/taglist.py
+++ doc/taglist.py
@@ -163,3 +163,5 @@ def setup(app):
     app.connect('doctree-read', process_tags)
     app.connect('doctree-resolved', process_taglist_nodes)
     app.connect('env-purge-doc', purge_tags)
+
+    return {'parallel_write_safe': True, 'parallel_read_safe': False}
