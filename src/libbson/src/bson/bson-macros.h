ce7b6d46ffdb8564bbe4f29a0f5cc3467cf7cc9e
A. Jesse Jiryu Davis <jesse@mongodb.com>
2018-01-04 19:54:12 -0500

    CDRIVER-2450 preserve API compatibility in macros
    BSON_STATIC_ASSERT and related macros must not change signatures.

--- src/libbson/src/bson/bson-macros.h
+++ src/libbson/src/bson/bson-macros.h
@@ -51,7 +51,7 @@
 #endif
 
 
-#if defined (__GNUC__)
+#if defined(__GNUC__)
 #define BSON_GNUC_CHECK_VERSION(major, minor) \
    ((__GNUC__ > (major)) ||                   \
     ((__GNUC__ == (major)) && (__GNUC_MINOR__ >= (minor))))
@@ -60,7 +60,7 @@
 #endif
 
 
-#if defined (__GNUC__)
+#if defined(__GNUC__)
 #define BSON_GNUC_IS_VERSION(major, minor) \
    ((__GNUC__ == (major)) && (__GNUC_MINOR__ == (minor)))
 #else
@@ -200,14 +200,22 @@
       }                                                    \
    } while (0)
 
-
+/* obsolete macros, preserved for compatibility */
 #define BSON_STATIC_ASSERT(s) BSON_STATIC_ASSERT_ (s, __LINE__, assert)
-#define BSON_STATIC_ASSERT2(_name, _s) BSON_STATIC_ASSERT_ (_s, __LINE__, _name)
-#define BSON_STATIC_ASSERT_JOIN(_a, _b, _name) \
-   BSON_STATIC_ASSERT_JOIN2 (_a, _b, _name)
-#define BSON_STATIC_ASSERT_JOIN2(_a, _b, _name) _a##_b##_name
-#define BSON_STATIC_ASSERT_(_s, _l, _name)   \
-   typedef char BSON_STATIC_ASSERT_JOIN ( \
+#define BSON_STATIC_ASSERT_JOIN(a, b) BSON_STATIC_ASSERT_JOIN2 (a, b)
+#define BSON_STATIC_ASSERT_JOIN2(a, b) a##b
+#define BSON_STATIC_ASSERT_(s, l)                             \
+   typedef char BSON_STATIC_ASSERT_JOIN (static_assert_test_, \
+                                         __LINE__)[(s) ? 1 : -1]
+
+/* modern macros */
+#define BSON_STATIC_ASSERT2(_name, _s) \
+   BSON_STATIC_ASSERT2_ (_s, __LINE__, _name)
+#define BSON_STATIC_ASSERT_JOIN3(_a, _b, _name) \
+   BSON_STATIC_ASSERT_JOIN4 (_a, _b, _name)
+#define BSON_STATIC_ASSERT_JOIN4(_a, _b, _name) _a##_b##_name
+#define BSON_STATIC_ASSERT2_(_s, _l, _name) \
+   typedef char BSON_STATIC_ASSERT_JOIN3 (  \
       static_assert_test_, __LINE__, _name)[(_s) ? 1 : -1]
 
 
