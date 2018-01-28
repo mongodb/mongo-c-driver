835db65aec0501e2048dc63a49825c18737ed515
A. Jesse Jiryu Davis <jesse@mongodb.com>
2018-01-28 10:41:46 -0500

    CDRIVER-2416 use bundled libbson in tests

--- .evergreen/compile-windows-mingw.bat
+++ .evergreen/compile-windows-mingw.bat
@@ -7,17 +7,9 @@ set CMAKE=C:\cmake\bin\cmake
 set CMAKE_MAKE_PROGRAM=C:\mingw-w64\x86_64-4.9.1-posix-seh-rt_v3-rev1\mingw64\bin\mingw32-make.exe
 set CC=C:\mingw-w64\x86_64-4.9.1-posix-seh-rt_v3-rev1\mingw64\bin\gcc.exe
 
-cd src\libbson
-%CMAKE% -G "MinGW Makefiles" -DCMAKE_MAKE_PROGRAM=%CMAKE_MAKE_PROGRAM% %CONFIGURE_FLAGS%
-
-%CMAKE_MAKE_PROGRAM%
-%CMAKE_MAKE_PROGRAM% install
-
-cd ..\..
 %CMAKE% -G "MinGW Makefiles" -DCMAKE_MAKE_PROGRAM=%CMAKE_MAKE_PROGRAM% -DCMAKE_PREFIX_PATH=%INSTALL_DIR%\lib\cmake -DCMAKE_C_FLAGS="-std=c99 -pedantic" %CONFIGURE_FLAGS%
 
 %CMAKE_MAKE_PROGRAM%
-%CMAKE_MAKE_PROGRAM% install
 
 set MONGOC_TEST_SKIP_LIVE=on
 test-libmongoc.exe --no-fork -d -F test-results.json
