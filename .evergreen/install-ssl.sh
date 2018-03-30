78603e7377b63c49faf5b999532d5ca261d70197
A. Jesse Jiryu Davis <jesse@mongodb.com>
2018-03-30 15:41:06 -0400

    update OpenSSL FIPS download link

--- .evergreen/install-ssl.sh
+++ .evergreen/install-ssl.sh
@@ -6,7 +6,7 @@ SSL=${SSL:-no}
 INSTALL_DIR=$(pwd)/install-dir
 
 install_openssl_fips() {
-   curl --retry 5 -o fips.tar.gz https://www.openssl.org/source/openssl-fips-2.0.14.tar.gz
+   curl --retry 5 -o fips.tar.gz https://www.openssl.org/source/openssl-fips-2.0.16.tar.gz
    tar zxvf fips.tar.gz
    cd openssl-fips-2.0.14
    ./config --prefix=$INSTALL_DIR -fPIC
