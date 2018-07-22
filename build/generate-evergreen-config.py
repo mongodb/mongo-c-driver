ea29177af4d347616b2213016dac59c59e2b0eb7
A. Jesse Jiryu Davis <jesse@mongodb.com>
2018-07-22 17:15:54 -0400

    CDRIVER-2479 generate evergreen tasks w/ script
    Begin to generate repetitive task definitions from a shorter Python
    script. In order to create an identical config.yml the Python script
    is full of hacks and reproduces some unintentional inconsistencies in
    the handwritten config.yml.

---
+++ build/generate-evergreen-config.py
@@ -0,0 +1,281 @@
+#!/usr/bin/env python
+#
+# Copyright 2018-present MongoDB, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Generate C Driver's config.yml for Evergreen testing.
+
+We find that generating configuration from Python data structures and a template
+file is more legible than Evergreen's matrix syntax or a handwritten file.
+
+Written for Python 2.6+, requires Jinja 2 for templating.
+"""
+
+from collections import namedtuple, OrderedDict
+from csv import DictReader
+from itertools import product
+from os.path import dirname, join as joinpath, normpath
+
+from jinja2 import Environment, FileSystemLoader  # pip install jinja2.
+
+this_dir = dirname(__file__)
+evergreen_dir = normpath(joinpath(this_dir, '../.evergreen'))
+
+integration_task_axes = OrderedDict([
+    ('valgrind', ['valgrind', False]),
+    ('version', ['latest', '4.0', '3.6', '3.4', '3.2', '3.0']),
+    ('topology', ['server', 'replica_set', 'sharded']),
+    ('auth', [True, False]),
+    ('sasl', ['sasl', 'sspi', False]),
+    ('ssl', ['openssl', 'darwinssl', 'winssl', False]),
+    ('memcheck', ['memcheck', False]),
+    ('coverage', ['coverage', False]),
+    ('asan', ['asan', False]),
+])
+
+
+class IntegrationTask(namedtuple('Task', tuple(integration_task_axes))):
+    def __new__(cls, *args, **kwargs):
+        self = super(IntegrationTask, cls).__new__(cls, *args, **kwargs)
+        self.tags = []
+        self.options = OrderedDict()
+        self.depends_on = []
+        return self
+
+    def display(self, axis_name):
+        value = getattr(self, axis_name)
+        # E.g., if self.auth is False, return 'noauth'.
+        if value is False:
+            return 'no' + axis_name
+
+        if value is True:
+            return axis_name
+
+        return value
+
+    # TODO: use this as "name", not the handwritten name.
+    @property
+    def _name(self):
+        return 'test-' + '-'.join(
+            self.display(axis_name) for axis_name in integration_task_axes
+            if getattr(axis_name))
+
+
+def matrix(cell_class, axes):
+    return set(cell_class(*cell) for cell in product(*axes.values()))
+
+
+class Prohibited(Exception):
+    pass
+
+
+def require(rule):
+    if not rule:
+        raise Prohibited()
+
+
+def prohibit(rule):
+    if rule:
+        raise Prohibited()
+
+
+def allow_integration_test_task(task):
+    if task.valgrind:
+        prohibit(task.asan)
+        prohibit(task.sasl)
+        require(task.ssl in ('openssl', False))
+        prohibit(task.coverage)
+        if not task.auth:
+            prohibit(task.sasl)
+            prohibit(task.ssl)
+
+        if task.memcheck:
+            require(task.version == 'latest')
+            require(task.auth)
+            prohibit(task.topology == 'sharded')
+
+        if task.topology == 'replica_set':
+            if task.auth:
+                require(task.version == 'latest')
+                require(task.memcheck)
+            else:
+                prohibit(task.version == '3.6')
+
+    if task.version == '3.6':
+        prohibit(task.coverage and task.topology == 'replica_set')
+
+    if task.auth:
+        require(task.ssl)
+
+    if task.sasl == 'sspi':
+        # Only one task.
+        require(task.topology == 'server')
+        require(task.version == 'latest')
+        require(task.ssl == 'winssl')
+        require(task.auth)
+
+    if not task.ssl:
+        prohibit(task.sasl)
+
+    if task.memcheck:
+        require(task.valgrind or task.asan)
+
+    if task.coverage:
+        prohibit(task.sasl)
+
+        if task.auth:
+            require(task.ssl == 'openssl')
+
+        if task.auth and task.topology == 'replica_set':
+            require(task.version == 'latest')
+
+        if not task.auth:
+            prohibit(task.ssl)
+
+        prohibit(task.topology == 'server'
+                 and task.version == 'latest'
+                 and task.ssl == 'winssl'
+                 and not task.auth)
+
+    if task.asan:
+        prohibit(task.sasl)
+        prohibit(task.coverage)
+
+        # Address sanitizer only with auth+SSL or no auth.
+        if task.auth:
+            require(task.ssl == 'openssl')
+        else:
+            prohibit(task.ssl)
+
+        # Run two tasks with ASAN and memcheck.
+        if task.memcheck:
+            require(task.topology in ('server', 'replica_set'))
+            require(task.version == 'latest')
+            require(task.auth)
+
+        # TODO: delete these arbitrary rules.
+        prohibit(task.topology == 'replica_set'
+                 and task.auth
+                 and not task.memcheck)
+
+        prohibit(task.topology == 'replica_set'
+                 and task.version == '3.6'
+                 and not task.auth)
+
+
+def make_integration_test_tasks():
+    tasks_set = set()
+    for task in matrix(IntegrationTask, integration_task_axes):
+        try:
+            allow_integration_test_task(task)
+        except Prohibited:
+            continue
+
+        tasks_set.add(task)
+
+    # TODO: temporary to enforce same order and names as handwritten YAML during
+    # migration from handwritten to fully generated config; remove.
+    tasks_list = []
+    csv_path = joinpath(this_dir, 'integration-test-tasks.csv')
+    reader = DictReader(open(csv_path))
+    for row in reader:
+        # Convert 'TRUE/FALSE' CSV values to True/False, omit 'name' column.
+        def convert(v):
+            if v == 'TRUE':
+                return True
+            if v == 'FALSE':
+                return False
+            return v
+
+        converted = dict([(axis, convert(value))
+                          for axis, value in row.items()
+                          if axis != 'name'])
+
+        task = IntegrationTask(**converted)
+
+        if task not in tasks_set:
+            # Bug in this script.
+            print('Line %d matches no matrix cell' % (reader.line_num,))
+            raise KeyError(task)
+
+        task.name = row['name']
+
+        if task.valgrind:
+            task.tags.append('test-valgrind')
+            task.options['exec_timeout_secs'] = 7200
+        elif task.coverage:
+            task.tags.append('test-coverage')
+            task.options['exec_timeout_secs'] = 3600
+        elif task.asan:
+            task.tags.append('test-asan')
+            task.options['exec_timeout_secs'] = 3600
+        else:
+            task.tags.extend([task.display('ssl'),
+                              task.display('sasl'),
+                              task.display('auth')])
+
+            # TODO: use topology as tag unchanged.
+            task.tags.append({'server': 'server',
+                              'replica_set': 'replica-set',
+                              'sharded': 'sharded-cluster',
+                              }[task.topology])
+
+            task.tags.append(task.version)
+
+        # E.g., test-latest-server-auth-sasl-ssl needs debug-compile-sasl-ssl.
+        if task.valgrind and task.memcheck:
+            task.depends_on.append('debug-compile-valgrind-memcheck')
+        elif task.asan:
+            if task.memcheck:
+                task.depends_on.append('debug-compile-asan-clang-memcheck')
+            elif task.ssl == 'openssl':
+                task.depends_on.append(
+                    'debug-compile-asan-clang-nosasl-openssl')
+            else:
+                task.depends_on.append('debug-compile-asan-clang-nosasl-nossl')
+        elif not task.coverage:
+            task.depends_on.append('debug-compile-%s-%s' % (
+                task.display('sasl'), task.display('ssl')))
+
+        tasks_list.append(task)
+
+        # Check off tasks as find them in the CSV, so we know at the end that
+        # this script produced the same set as the handwritten YML.
+        tasks_set.remove(task)
+
+    if tasks_set:
+        print('allow_integration_test_task should prohibit %d tasks:' % (
+            len(tasks_set,)))
+
+        for task in tasks_set:
+            print(task)
+
+        assert False
+
+    return tasks_list
+
+
+env = Environment(loader=FileSystemLoader(this_dir),
+                  trim_blocks=True,
+                  lstrip_blocks=True,
+                  extensions=['jinja2.ext.loopcontrols'])
+
+env.filters['tag_list'] = lambda value: (
+        '[' + ', '.join('"%s"' % (tag,) for tag in value) + ']')
+
+print('.evergreen/config.yml')
+f = open(joinpath(evergreen_dir, 'config.yml'), 'w+')
+t = env.get_template('config.yml.template')
+f.write(t.render(globals()))
+f.write('\n')
