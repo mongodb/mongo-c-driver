57ec0a1dce0a5bb4491c55393e46b99faab96bbf
Roberto C. Sánchez <roberto@connexer.com>
2018-07-25 22:09:52 -0400

    CID-104922 (Coverity) fix unchecked return value

--- src/libbson/tests/test-bson.c
+++ src/libbson/tests/test-bson.c
@@ -2313,7 +2313,7 @@ test_bson_iter_key_len (void)
    bson_t *bson = bson_with_all_types ();
    bson_iter_t iter;
 
-   bson_iter_init (&iter, bson);
+   BSON_ASSERT (bson_iter_init (&iter, bson));
    while (bson_iter_next (&iter)) {
       ASSERT_WITH_MSG (strlen (bson_iter_key (&iter)) ==
                           bson_iter_key_len (&iter),
