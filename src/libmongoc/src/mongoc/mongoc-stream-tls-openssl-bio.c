3cc855c6be4b1df5e9412f104b6c65b1a9c40817
Jeroen <jeroenooms@gmail.com>
2018-08-09 17:34:30 +0200

    Fix for LibreSSL 2.7 LibreSSL version 2.7.0 and up use the OpenSSL 1.1 API See also: https://wiki.freebsd.org/LibreSSL

--- src/libmongoc/src/mongoc/mongoc-stream-tls-openssl-bio.c
+++ src/libmongoc/src/mongoc/mongoc-stream-tls-openssl-bio.c
@@ -39,7 +39,7 @@
 #define MONGOC_LOG_DOMAIN "stream-tls-openssl-bio"
 
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000L)
 
 /* Magic vtable to make our BIO shim */
 static BIO_METHOD gMongocStreamTlsOpenSslRawMethods = {
