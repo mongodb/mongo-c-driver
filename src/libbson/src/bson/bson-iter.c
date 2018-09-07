3f5cd738cf20d8be182a4b29bc10edeb25cc8786
A. Jesse Jiryu Davis <jesse@mongodb.com>
2018-09-06 22:04:30 -0400

    simpler bson_iter_overwrite_bool()

--- src/libbson/src/bson/bson-iter.c
+++ src/libbson/src/bson/bson-iter.c
@@ -2177,7 +2177,6 @@ bson_iter_overwrite_bool (bson_iter_t *iter, /* IN */
                           bool value)        /* IN */
 {
    BSON_ASSERT (iter);
-   value = !!value;
 
    if (ITER_TYPE (iter) == BSON_TYPE_BOOL) {
       memcpy ((void *) (iter->raw + iter->d1), &value, 1);
