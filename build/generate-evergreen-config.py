e5dbb4ea3aef4f3f592d65280c940c3f73df0e2f
A. Jesse Jiryu Davis <jesse@mongodb.com>
2018-09-26 03:07:22 -0400

    CDRIVER-2479 generate some compile tasks in Python
    Also fix ENABLE_RDTSCP test.

--- build/generate-evergreen-config.py
+++ build/generate-evergreen-config.py
@@ -26,6 +26,7 @@ import sys
 from collections import namedtuple, OrderedDict as OD
 from itertools import product
 from os.path import dirname, join as joinpath, normpath
+from textwrap import dedent
 
 try:
     import yaml
@@ -39,15 +40,53 @@ this_dir = dirname(__file__)
 evergreen_dir = normpath(joinpath(this_dir, '../.evergreen'))
 
 
-class Task(object):
-    name_prefix = 'test'
+# We want legible YAML tasks:
+#
+#     - name: debug-compile
+#       tags: [zlib, snappy, compression, openssl]
+#       commands:
+#       - command: shell.exec
+#         params:
+#           script: |-
+#             set -o errexit
+#             set -o xtrace
+#             ...
+#
+# Write values compactly except multiline strings, which use "|" style. Write
+# tag sets as lists.
 
-    def __new__(cls, *args, **kwargs):
-        self = super(Task, cls).__new__(cls, *args, **kwargs)
-        self.tags = []
+class Dumper(yamlordereddictloader.Dumper):
+    def __init__(self, *args, **kwargs):
+        super(Dumper, self).__init__(*args, **kwargs)
+        self.add_representer(set, type(self).represent_set)
+
+    def represent_scalar(self, tag, value, style=None):
+        if isinstance(value, (str, unicode)) and '\n' in value:
+            style = '|'
+        return super(Dumper, self).represent_scalar(tag, value, style)
+
+    def represent_set(self, data):
+        return super(Dumper, self).represent_list(sorted(data))
+
+
+class Task(object):
+    def __init__(self, *args, **kwargs):
+        super(Task, self).__init__()
+        self.tags = set()
         self.options = OD()
         self.depends_on = []
-        return self
+
+    name_prefix = 'test'
+
+    @property
+    def name(self):
+        return 'UNSET'
+
+    def add_tags(self, *args):
+        self.tags.update(args)
+
+    def has_tags(self, *args):
+        return bool(self.tags.intersection(args))
 
     def display(self, axis_name):
         value = getattr(self, axis_name)
@@ -60,10 +99,16 @@ class Task(object):
 
         return value
 
-    def on_off(self, axis_name):
-        return 'on' if getattr(self, axis_name) else 'off'
+    def on_off(self, *args, **kwargs):
+        assert not (args and kwargs)
+        if args:
+            axis_name, = args
+            return 'on' if getattr(self, axis_name) else 'off'
 
-    def as_dict(self):
+        (axis_name, value), = kwargs
+        return 'on' if getattr(self, axis_name) == value else 'off'
+
+    def to_dict(self):
         task = OD([('name', self.name), ('tags', self.tags)])
         task.update(self.options)
         if self.depends_on:
@@ -80,8 +125,155 @@ class Task(object):
         return task
 
     def to_yaml(self):
-        return yaml.dump(self.as_dict(), Dumper=yamlordereddictloader.Dumper)
+        return yaml.dump(self.to_dict(), Dumper=Dumper)
 
+
+class CompileTask(Task):
+    def __init__(self, compile_task_name, tags=None, config='debug',
+                 compression='default', continue_on_err=False,
+                 extra_commands=None, **kwargs):
+        super(CompileTask, self).__init__()
+        self._compile_task_name = compile_task_name
+        if tags:
+            self.add_tags(*tags)
+
+        self.extra_commands = extra_commands or []
+
+        # Environment variables for .evergreen/compile.sh.
+        self.compile_sh_opt = kwargs
+        if config == 'debug':
+            self.compile_sh_opt['DEBUG'] = 'ON'
+        else:
+            assert config == 'release'
+            self.compile_sh_opt['RELEASE'] = 'ON'
+
+        if compression != 'default':
+            self.compile_sh_opt['SNAPPY'] = (
+                'ON' if compression in ('all', 'snappy') else 'OFF')
+            self.compile_sh_opt['ZLIB'] = (
+                'BUNDLED' if compression in ('all', 'zlib') else 'OFF')
+
+        self.continue_on_err = continue_on_err
+
+    @property
+    def name(self):
+        return self._compile_task_name
+
+    def to_dict(self):
+        task = super(CompileTask, self).to_dict()
+
+        script = "set -o errexit\nset -o xtrace\n"
+        for opt, value in sorted(self.compile_sh_opt.items()):
+            script += 'export %s="%s"\n' % (opt, value)
+
+        script += "CC='${CC}' MARCH='${MARCH}' sh .evergreen/compile.sh"
+        task['commands'].append(OD([
+            ('command', 'shell.exec'),
+            ('type', 'test'),
+            ('params', OD([('working_dir', 'mongoc'), ('script', script)])),
+        ]))
+
+        task['commands'].append(OD([('func', 'upload build')]))
+        task['commands'].extend(self.extra_commands)
+        return task
+
+
+class SpecialTask(CompileTask):
+    def __init__(self, *args, **kwargs):
+        super(SpecialTask, self).__init__(*args, **kwargs)
+        self.add_tags('special')
+
+
+compile_tasks = [
+    CompileTask('debug-compile-compression-zlib',
+                tags=['zlib', 'compression'],
+                compression='zlib'),
+    CompileTask('debug-compile-compression-snappy',
+                tags=['snappy', 'compression'],
+                compression='snappy'),
+    CompileTask('debug-compile-compression',
+                tags=['zlib', 'snappy', 'compression'],
+                compression='all'),
+    CompileTask('debug-compile-no-align',
+                tags=['debug-compile'],
+                compression='zlib',
+                EXTRA_CONFIGURE_FLAGS="-DENABLE_EXTRA_ALIGNMENT=OFF"),
+    CompileTask('debug-compile-nosasl-nossl',
+                tags=['debug-compile', 'nosasl', 'nossl']),
+    CompileTask('debug-compile-lto', CFLAGS='-flto'),
+    CompileTask('debug-compile-lto-thin', CFLAGS='-flto=thin'),
+    SpecialTask('debug-compile-c11',
+                tags=['debug-compile', 'c11', 'stdflags'],
+                CFLAGS='-std=c11 -D_XOPEN_SOURCE=600'),
+    SpecialTask('debug-compile-c99',
+                tags=['debug-compile', 'c99', 'stdflags'],
+                CFLAGS='-std=c99 -D_XOPEN_SOURCE=600'),
+    SpecialTask('debug-compile-c89',
+                tags=['debug-compile', 'c89', 'stdflags'],
+                CFLAGS='-std=c89 -D_POSIX_C_SOURCE=200112L -pedantic'),
+    SpecialTask('debug-compile-valgrind',
+                tags=['debug-compile', 'valgrind'],
+                SASL='OFF',
+                SSL='OPENSSL',
+                VALGRIND='ON',
+                CFLAGS='-DBSON_MEMCHECK'),
+    SpecialTask('debug-compile-coverage',
+                tags=['debug-compile', 'coverage'],
+                COVERAGE='ON',
+                extra_commands=[OD([('func', 'upload coverage')])]),
+    CompileTask('debug-compile-no-counters',
+                tags=['debug-compile', 'no-counters'],
+                ENABLE_SHM_COUNTERS='OFF'),
+    SpecialTask('debug-compile-asan-clang',
+                tags=['debug-compile', 'asan-clang'],
+                compression='zlib',
+                CC='clang-3.8',
+                CFLAGS='-fsanitize=address -fno-omit-frame-pointer'
+                       ' -DBSON_MEMCHECK',
+                CHECK_LOG='ON',
+                EXTRA_CONFIGURE_FLAGS='-DENABLE_EXTRA_ALIGNMENT=OFF',
+                PATH='/usr/lib/llvm-3.8/bin:$PATH'),
+    # include -pthread in CFLAGS on gcc to address the issue explained here:
+    # https://groups.google.com/forum/#!topic/address-sanitizer/JxnwgrWOLuc
+    SpecialTask('debug-compile-asan-gcc',
+                compression='zlib',
+                CFLAGS='-fsanitize=address -pthread',
+                CHECK_LOG='ON',
+                EXTRA_CONFIGURE_FLAGS="-DENABLE_EXTRA_ALIGNMENT=OFF"),
+    SpecialTask('debug-compile-asan-clang-openssl',
+                tags=['debug-compile', 'asan-clang'],
+                compression='zlib',
+                CC='clang-3.8',
+                CFLAGS='-fsanitize=address-fsanitize=address'
+                       ' -fno-omit-frame-pointer -DBSON_MEMCHECK',
+                CHECK_LOG='ON',
+                EXTRA_CONFIGURE_FLAGS="-DENABLE_EXTRA_ALIGNMENT=OFF",
+                PATH='/usr/lib/llvm-3.8/bin:$PATH',
+                SSL='OPENSSL'),
+    SpecialTask('debug-compile-ubsan',
+                compression='zlib',
+                CC='clang-3.8',
+                CFLAGS='-fsanitize=undefined -fno-omit-frame-pointer'
+                       ' -DBSON_MEMCHECK',
+                CHECK_LOG='ON',
+                EXTRA_CONFIGURE_FLAGS="-DENABLE_EXTRA_ALIGNMENT=OFF",
+                PATH='/usr/lib/llvm-3.8/bin:$PATH'),
+    SpecialTask('debug-compile-scan-build',
+                tags=['clang', 'debug-compile', 'scan-build'],
+                continue_on_err=True,
+                ANALYZE='ON',
+                CC='clang',
+                extra_commands=[
+                    OD([('func', 'upload scan artifacts')]),
+                    OD([('command', 'shell.exec'),
+                        ('type', 'test'),
+                        ('params', OD([
+                            ('working_dir', 'mongoc'),
+                            ('script', dedent('''\
+             if find scan -name \*.html | grep -q html; then
+               exit 123
+             fi'''))]))])]),
+]
 
 integration_task_axes = OD([
     ('valgrind', ['valgrind', False]),
@@ -110,8 +302,8 @@ class IntegrationTask(Task, namedtuple('Task', tuple(integration_task_axes))):
             name_part(axis_name) for axis_name in integration_task_axes
             if getattr(self, axis_name) or axis_name in ('auth', 'sasl', 'ssl'))
 
-    def as_dict(self):
-        task = super(IntegrationTask, self).as_dict()
+    def to_dict(self):
+        task = super(IntegrationTask, self).to_dict()
         commands = task['commands']
         if self.coverage:
             commands.append(OD([
@@ -162,8 +354,8 @@ class AuthTask(Task, namedtuple('Task', tuple(auth_task_axes))):
         else:
             return rv + '-nosasl'
 
-    def as_dict(self):
-        task = super(AuthTask, self).as_dict()
+    def to_dict(self):
+        task = super(AuthTask, self).to_dict()
         task['commands'].append(OD([
             ('func', 'run auth tests'),
         ]))
@@ -248,27 +440,31 @@ def make_integration_test_tasks():
             continue
 
         if task.valgrind:
-            task.tags.append('test-valgrind')
+            task.tags.add('test-valgrind')
             task.options['exec_timeout_secs'] = 7200
         elif task.coverage:
-            task.tags.append('test-coverage')
+            task.tags.add('test-coverage')
             task.options['exec_timeout_secs'] = 3600
         elif task.asan:
-            task.tags.append('test-asan')
+            task.tags.add('test-asan')
             task.options['exec_timeout_secs'] = 3600
         else:
-            task.tags.append(task.topology)
-            task.tags.append(task.version)
-            task.tags.extend([task.display('ssl'),
+            task.tags.add(task.topology)
+            task.tags.add(task.version)
+            task.tags.update([task.display('ssl'),
                               task.display('sasl'),
                               task.display('auth')])
 
         # E.g., test-latest-server-auth-sasl-ssl needs debug-compile-sasl-ssl.
+        # Coverage tasks use a build function instead of depending on a task.
         if task.valgrind:
             task.depends_on.append('debug-compile-valgrind')
-        elif task.asan:
-            task.depends_on.append('debug-compile-asan-clang-nosasl-%s' % (
+        elif task.asan and task.ssl:
+            task.depends_on.append('debug-compile-asan-clang-%s' % (
                 task.display('ssl'),))
+        elif task.asan:
+            assert not task.sasl
+            task.depends_on.append('debug-compile-asan-clang')
         elif not task.coverage:
             task.depends_on.append('debug-compile-%s-%s' % (
                 task.display('sasl'), task.display('ssl')))
@@ -292,9 +488,9 @@ def make_auth_test_tasks():
         except Prohibited:
             continue
 
-        task.tags.extend(['authentication-tests',
-                         task.display('ssl'),
-                         task.display('sasl')])
+        task.tags.update(['authentication-tests',
+                          task.display('ssl'),
+                          task.display('sasl')])
 
         task.depends_on.append('debug-compile-%s-%s' % (
             task.display('sasl'), task.display('ssl')))
