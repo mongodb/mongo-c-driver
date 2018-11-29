17c1451072952d33559f8c0ab5dab5d4afb97ae7
Roberto C. Sánchez <roberto@connexer.com>
2018-11-29 00:48:01 -0500

    fix debian-package-build task

--- .evergreen/debian_package_build.sh
+++ .evergreen/debian_package_build.sh
@@ -34,12 +34,17 @@ cd ..
 git clone https://salsa.debian.org/installer-team/debootstrap.git debootstrap.git
 mkdir unstable-chroot
 export DEBOOTSTRAP_DIR=`pwd`/debootstrap.git
-sudo -E ./debootstrap.git/debootstrap --include=build-essential,git-buildpackage,fakeroot,debhelper,cmake,libssl-dev,pkg-config,python3-sphinx,zlib1g-dev,libicu-dev,libsasl2-dev,libsnappy-dev unstable ./unstable-chroot/ http://cdn-aws.deb.debian.org/debian
+# perl-openssl-defaults is explicitly listed to work around https://bugs.debian.org/907015
+sudo -E ./debootstrap.git/debootstrap --include=build-essential,perl-openssl-defaults,git-buildpackage,fakeroot,debhelper,cmake,libssl-dev,pkg-config,python3-sphinx,zlib1g-dev,libicu-dev,libsasl2-dev,libsnappy-dev,python-git unstable ./unstable-chroot/ http://cdn-aws.deb.debian.org/debian
 cp -a mongoc ./unstable-chroot/tmp/
 sudo chroot ./unstable-chroot /bin/bash -c "(set -o xtrace && \
   cd /tmp/mongoc && \
   git clean -fdx && \
   git reset --hard HEAD && \
+  python build/calc_release_version.py > VERSION_CURRENT && \
+  python build/calc_release_version.py -p > VERSION_RELEASED && \
+  git add --force VERSION_CURRENT VERSION_RELEASED && \
+  git commit VERSION_CURRENT VERSION_RELEASED -m 'Set current/released versions' && \
   LANG=C /bin/bash ./debian/build_snapshot.sh && \
   dpkg -i ../*.deb && \
   gcc -I/usr/include/libmongoc-1.0 -I/usr/include/libbson-1.0 -o example-client src/libmongoc/examples/example-client.c -lmongoc-1.0 -lbson-1.0 )"
