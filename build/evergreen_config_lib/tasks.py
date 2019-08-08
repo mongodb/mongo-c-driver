328a5816264be57d7c209e89791707bae9f48b6d
Kevin Albertson <kevin.albertson@10gen.com>
2019-08-08 12:40:00 -0400

    CDRIVER-3274 temporarily remove zstd tests

--- build/evergreen_config_lib/tasks.py
+++ build/evergreen_config_lib/tasks.py
@@ -491,7 +491,7 @@ all_tasks = chain(all_tasks, DNSTask.matrix())
 
 
 class CompressionTask(MatrixTask):
-    axes = OD([('compression', ['zlib', 'snappy', 'zstd', 'compression'])])
+    axes = OD([('compression', ['zlib', 'snappy', 'compression'])]) # TODO: CDRIVER-3274 add "zstd"
     name_prefix = 'test-latest-server'
 
     def __init__(self, *args, **kwargs):
@@ -542,7 +542,7 @@ class CompressionTask(MatrixTask):
         elif self.compression == 'zstd':
             return ['zstd']
         else:
-            return ['snappy', 'zlib', 'zstd']
+            return ['snappy', 'zlib'] # TODO: CDRIVER-3247 add zstd
 
 
 all_tasks = chain(all_tasks, CompressionTask.matrix())
