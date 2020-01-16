mongodb/mongo-c-driver

commit eebc738efd2a976cd1014ba1dc6edf5045f0af04

simplify docs
- removes custom theme in favor of vendored "readable" theme
- moves remaining doc source files from gh-pages into master
- CDRIVER-3445 add NPS survey
- CDRIVER-3463 update google analytics
- CDRIVER-2833 fix the search on libmongoc/libbson sites, remove from
home page

.evergreen/check-release-archive.sh
@@ -24,7 +24,7 @@ python build/calc_release_version.py -p > VERSION_RELEASED
 # file checks.
 mkdir cmake_build
 cd cmake_build
-$CMAKE -DENABLE_MAN_PAGES=ON -DENABLE_HTML_DOCS=ON -DENABLE_ZLIB=BUNDLED -DENABLE_BSON=ON -DENABLE_CLIENT_SIDE_ENCRYPTION=OFF ../
+$CMAKE -DENABLE_MAN_PAGES=ON -DENABLE_HTML_DOCS=ON -DENABLE_ZLIB=BUNDLED -DENABLE_BSON=ON ../
 make DISTCHECK_BUILD_OPTS="-j 8" distcheck
 
 # Check that docs were included, but sphinx temp files weren't.

.evergreen/config.yml
@@ -451,6 +451,7 @@ functions:
   - command: shell.exec
     type: test
     params:
+      silent: true
       working_dir: mongoc
       shell: bash
       script: |-

build/cmake/SphinxBuild.cmake
@@ -62,7 +62,6 @@ function (sphinx_build_html target_name doc_dir)
 
    # Ensure additional Sphinx-generated content gets installed
    install (FILES
-      ${SPHINX_HTML_DIR}/genindex.html
       ${SPHINX_HTML_DIR}/search.html
       ${SPHINX_HTML_DIR}/objects.inv
       ${SPHINX_HTML_DIR}/searchindex.js

build/sphinx/CMakeLists.txt
@@ -1,14 +1,15 @@
-add_subdirectory (mongoc-theme)
+add_subdirectory (readable)
 
 set_local_dist (build_sphinx_DIST_local
    CMakeLists.txt
    mongoc_common.py
    mongoc/__init__.py
    taglist.py
+   customindexlink.html
 )
 
 set (build_sphinx_DIST
    ${build_sphinx_DIST_local}
-   ${build_sphinx_mongoc-theme_DIST}
+   ${build_sphinx_readable_DIST}
    PARENT_SCOPE
 )

build/sphinx/customindexlink.html
@@ -1,0 +1,2 @@
+<!-- Because full_index.rst includes everything that index.rst includes, we have to exclude index.rst from the table-of-contents. This page is simply a link forced into the sidebar (in conf.py) to avoid including full_index.rst in the ToC. -->
+<ul><li class='toctree-l1'><a href="full_index.html">Index</a></li></ul>

build/sphinx/homepage-config/.gitignore
@@ -1,0 +1,4 @@
+__pycache__/
+*.py[cod]
+_build/
+html/

build/sphinx/homepage-config/conf.py
@@ -1,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from docutils import nodes
+import os
+import sys
+
+# Import common docs config.
+this_path = os.path.dirname(__file__)
+sys.path.append(os.path.normpath(os.path.join(this_path, '../')))
+
+from mongoc_common import *
+
+version = release = os.environ.get('VERSION_RELEASED')
+if not release:
+    raise RuntimeError('Set the "VERSION_RELEASED" environment variable')
+
+# -- General configuration ------------------------------------------------
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+
+# General information about the project.
+project = u'mongoc.org'
+copyright = u'2017, MongoDB, Inc'
+author = u'MongoDB, Inc'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
+
+
+# Support :download-link:`bson` or :download-link:`mongoc`.
+def download_link(typ, rawtext, text, lineno, inliner, options={}, content=[]):
+    if text == "mongoc":
+        lib = "mongo-c-driver"
+    elif text == "bson":
+        lib = "libbson"
+    else:
+        raise ValueError(
+            "download link must be mongoc or libbson, not \"%s\"" % text)
+
+    title = "%s-%s.tar.gz" % (lib, version)
+    url = ("https://github.com/mongodb/%(lib)s/"
+           "releases/download/%(version)s/%(lib)s-%(version)s.tar.gz") % {
+              "lib": lib,
+              "version": version
+          }
+
+    pnode = nodes.reference(title, title, internal=False, refuri=url)
+    return [pnode], []
+
+def setup(app):
+    mongoc_common_setup(app)
+
+    app.add_role('download-link', download_link)
+
+# -- Options for HTML output ----------------------------------------------
+
+html_theme_path = ["../"]
+html_theme = 'readable'
+html_title = html_shorttitle = 'MongoDB C Driver %s' % version
+# html_favicon = None
+html_use_smartypants = False
+html_show_sourcelink = False
+html_use_index = False
+
+html_sidebars = {
+    '**': []
+}
+
+# Note: http://www.sphinx-doc.org/en/1.5.1/config.html#confval-html_copy_source
+# This will degrade the Javascript quicksearch if we ever use it.
+html_copy_source = False

build/sphinx/mongoc-theme/CMakeLists.txt
@@ -1,8 +1,0 @@
-set_dist_list (build_sphinx_mongoc-theme_DIST
-   CMakeLists.txt
-   globaltoc.html
-   layout.html
-   static/mongoc.css_t
-   static/pygments.css
-   theme.conf
-)

commit db56b7bdf2b83153bb551f545833073421c5eefc

undo change to check-release-archive
eebc738 accidentally removed ENABLE_CLIENT_SIDE_ENCRYPTION=OFF

.evergreen/check-release-archive.sh
@@ -24,7 +24,7 @@ python build/calc_release_version.py -p > VERSION_RELEASED
 # file checks.
 mkdir cmake_build
 cd cmake_build
-$CMAKE -DENABLE_MAN_PAGES=ON -DENABLE_HTML_DOCS=ON -DENABLE_ZLIB=BUNDLED -DENABLE_BSON=ON ../
+$CMAKE -DENABLE_MAN_PAGES=ON -DENABLE_HTML_DOCS=ON -DENABLE_ZLIB=BUNDLED -DENABLE_BSON=ON -DENABLE_CLIENT_SIDE_ENCRYPTION=OFF ../
 make DISTCHECK_BUILD_OPTS="-j 8" distcheck
 
 # Check that docs were included, but sphinx temp files weren't.