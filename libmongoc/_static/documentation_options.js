37e4ef645a0d6c36d72fcb82c5df5dcbe5ca469b
Kevin Albertson <kevin.albertson@mongodb.com>
2020-02-03 21:00:20 -0500

    libbson and libmongoc 1.16.1

--- libmongoc/_static/documentation_options.js
+++ libmongoc/_static/documentation_options.js
@@ -1,10 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.16.0',
+    VERSION: '1.16.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: false,
-    SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
+    SOURCELINK_SUFFIX: '.txt'
 };
