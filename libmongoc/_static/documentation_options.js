5015c075edb2250dc2c006b38984b10834017916
Kevin Albertson <kevin.albertson@mongodb.com>
2020-06-09 15:05:26 -0400

    1.17.0-beta2 docs

--- libmongoc/_static/documentation_options.js
+++ libmongoc/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.17.0-beta',
+    VERSION: '1.17.0-beta2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
