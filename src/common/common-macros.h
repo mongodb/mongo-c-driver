d5c1a7d29023cc199b2373b3372463c1825371b4
Andrew Witten <andrew.witten@mongodb.com>
2020-07-20 12:04:16 -0400

    formatting

--- src/common/common-macros.h
+++ src/common/common-macros.h
@@ -4,11 +4,10 @@
 /*
  * configure with option -DENABLE_TESTING=ON
  */
-#if defined(MONGOC_ENABLE_TESTING) && defined(BSON_OS_UNIX)
+#if defined (MONGOC_ENABLE_TESTING) && defined (BSON_OS_UNIX)
 #define MONGOC_TEST_ASSERT(statement) BSON_ASSERT (statement)
 #else
 #define MONGOC_TEST_ASSERT(statement) ((void) 0)
 #endif
-
 
 #endif
