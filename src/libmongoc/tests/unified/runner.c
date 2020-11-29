2adfec084eec4501aae92180ff1ad1dd2c292817
Kevin Albertson <kevin.albertson@mongodb.com>
2020-11-29 12:01:52 -0500

    terminate open transactions

--- src/libmongoc/tests/unified/runner.c
+++ src/libmongoc/tests/unified/runner.c
@@ -20,14 +20,18 @@
 #include "test-libmongoc.h"
 #include <signal.h>
 
+/* TODO: put in separate files? runner.c, file.c, test.c, diagnostics.c */
+
 /* test_runner_t, test_file_t, and test_t model the types described in the "Test
  * Runner Implementation" section of the Unified Test Format specification. */
 typedef struct {
    mongoc_uri_t *uri;
    mongoc_client_t *internal_client;
    semver_t server_version;
-   /* topology_type may be: {single, replicaset, sharded, sharded-replicaset} */
+   /* topology_type may be "single", "replicaset", "sharded", or
+    * "sharded-replicaset". */
    const char *topology_type;
+   mongoc_array_t server_ids;
 } test_runner_t;
 
 typedef struct {
@@ -64,32 +68,88 @@ struct {
 static const char *
 get_topology_type (mongoc_client_t *client);
 
+static bool
+is_topology_type_sharded (const char *topology_type)
+{
+   return 0 == strcmp ("sharded", topology_type) ||
+          0 == strcmp ("sharded-replicaset", topology_type);
+}
+
+static bool
+is_topology_type_compatible (const char *test_topology_type,
+                             const char *server_topology_type)
+{
+   if (0 == strcmp (test_topology_type, server_topology_type)) {
+      return true;
+   }
+   /* If a requirement specifies a "sharded" topology and server is of type
+    * "sharded-replicaset", that is also compatible. */
+   if (0 == strcmp (test_topology_type, "sharded") &&
+       is_topology_type_sharded (server_topology_type)) {
+      return true;
+   }
+   return false;
+}
+
+/* This callback tracks the set of server IDs for all connected servers.
+ * The set of server IDs is used when sending a command to each individual
+ * server.
+ */
+static void
+on_topology_changed (const mongoc_apm_topology_changed_t *event)
+{
+   test_runner_t *test_runner;
+   const mongoc_topology_description_t *td;
+   mongoc_server_description_t **sds;
+   size_t sds_len;
+   size_t i;
+
+   test_runner =
+      (test_runner_t *) mongoc_apm_topology_changed_get_context (event);
+   _mongoc_array_clear (&test_runner->server_ids);
+   td = mongoc_apm_topology_changed_get_new_description (event);
+   sds = mongoc_topology_description_get_servers (td, &sds_len);
+   for (i = 0; i < sds_len; i++) {
+      uint32_t server_id = mongoc_server_description_id (sds[i]);
+      MONGOC_DEBUG ("Topology changed, adding server id: %d", (int) server_id);
+      _mongoc_array_append_val (&test_runner->server_ids, server_id);
+   }
+   mongoc_server_descriptions_destroy_all (sds, sds_len);
+}
+
 static test_runner_t *
 test_runner_new (void)
 {
    test_runner_t *test_runner;
    char *uri_str;
    bson_error_t error;
+   mongoc_apm_callbacks_t *callbacks;
 
    test_runner = bson_malloc0 (sizeof (test_runner_t));
    /* Create an client for internal test operations (e.g. checking server
     * version) */
-   if (test_framework_getenv ("MONGOC_TEST_URI") != NULL) {
-      uri_str = test_framework_getenv ("MONGOC_TEST_URI");
-   } else {
+   uri_str = test_framework_getenv ("MONGOC_TEST_URI");
+   if (uri_str == NULL) {
       uri_str = bson_strdup ("mongodb://localhost:27017");
    }
 
    test_runner->uri = mongoc_uri_new_with_error (uri_str, &error);
    ASSERT_OR_PRINT (test_runner->uri, error);
+   _mongoc_array_init (&test_runner->server_ids, sizeof (uint32_t));
+   callbacks = mongoc_apm_callbacks_new ();
+   mongoc_apm_set_topology_changed_cb (callbacks, on_topology_changed);
    test_runner->internal_client = mongoc_client_new_from_uri (test_runner->uri);
-   bson_free (uri_str);
-
-   test_runner->topology_type = get_topology_type (test_runner->internal_client);
+   mongoc_client_set_apm_callbacks (
+      test_runner->internal_client, callbacks, test_runner);
+   mongoc_client_set_error_api (test_runner->internal_client,
+                                MONGOC_ERROR_API_VERSION_2);
+   test_runner->topology_type =
+      get_topology_type (test_runner->internal_client);
    server_semver (test_runner->internal_client, &test_runner->server_version);
-
    test_diagnostics.test_runner = test_runner;
 
+   mongoc_apm_callbacks_destroy (callbacks);
+   bson_free (uri_str);
    return test_runner;
 }
 
@@ -98,21 +158,105 @@ test_runner_destroy (test_runner_t *test_runner)
 {
    test_diagnostics.test_runner = NULL;
    mongoc_client_destroy (test_runner->internal_client);
+   _mongoc_array_destroy (&test_runner->server_ids);
    mongoc_uri_destroy (test_runner->uri);
    bson_free (test_runner);
 }
 
+/* Run a "killAllSessions" command against a server.
+ * Returns a 0 delimited array of all known servers that the test runner
+ * is connected to.
+ */
+static void
+test_runner_get_all_server_ids (test_runner_t *test_runner, mongoc_array_t *out)
+{
+   bson_error_t error;
+   bool ret;
+
+   /* Run a 'ping' command to make sure topology has been scanned. */
+   ret = mongoc_client_command_simple (test_runner->internal_client,
+                                       "admin",
+                                       tmp_bson ("{'ping': 1}"),
+                                       NULL /* read prefs */,
+                                       NULL /* reply */,
+                                       NULL /* error */);
+   ASSERT_OR_PRINT (ret, error);
+
+   _mongoc_array_copy (out, &test_runner->server_ids);
+}
+
+/*
+ * Run killAllSessions against the primary or each mongos to terminate any
+ * lingering open transactions.
+ * See also: Spec section "Terminating Open Transactions"
+ */
+static void
+test_runner_terminate_open_transactions (test_runner_t *test_runner)
+{
+   bson_t *kill_all_sessions_cmd;
+   bool ret;
+   bson_error_t error;
+
+   kill_all_sessions_cmd = tmp_bson ("{'killAllSessions': []}");
+   /* Run on each mongos. Target each server individually. */
+   if (is_topology_type_sharded (test_runner->topology_type)) {
+      mongoc_array_t server_ids;
+      size_t i;
+
+      _mongoc_array_init (&server_ids, sizeof (uint32_t));
+      test_runner_get_all_server_ids (test_runner, &server_ids);
+      for (i = 0; i < server_ids.len; i++) {
+         uint32_t server_id = _mongoc_array_index (&server_ids, uint32_t, i);
+         ret = mongoc_client_command_simple_with_server_id (
+            test_runner->internal_client,
+            "admin",
+            kill_all_sessions_cmd,
+            NULL /* read prefs. */,
+            server_id,
+            NULL,
+            &error);
+
+         /* Ignore error code 11601 as a workaround for SERVER-38335. */
+         if (!ret && error.code != 11601) {
+            test_error (
+               "Unexpected error running killAllSessions on server (%d): %s",
+               (int) server_id,
+               error.message);
+         }
+      }
+      _mongoc_array_destroy (&server_ids);
+      return;
+   }
+
+   /* Run on primary. */
+   ret = mongoc_client_command_simple (test_runner->internal_client,
+                                       "admin",
+                                       kill_all_sessions_cmd,
+                                       NULL /* read prefs. */,
+                                       NULL,
+                                       &error);
+
+   /* Ignore error code 11601 as a workaround for SERVER-38335. */
+   if (!ret && error.code != 11601) {
+      test_error ("Unexpected error running killAllSessions on primary: %s",
+                  error.message);
+   }
+}
+
 static test_file_t *
-test_file_new (test_runner_t* test_runner, bson_t* bson) {
+test_file_new (test_runner_t *test_runner, bson_t *bson)
+{
    test_file_t *test_file;
 
    test_file = bson_malloc0 (sizeof (test_file_t));
    test_file->test_runner = test_runner;
    test_file->description = bson_lookup_utf8 (bson, "description");
 
-   semver_parse (bson_lookup_utf8 (bson, "schemaVersion"), &test_file->schema_version);
+   semver_parse (bson_lookup_utf8 (bson, "schemaVersion"),
+                 &test_file->schema_version);
    if (bson_has_field (bson, "runOnRequirements")) {
-      test_file->run_on_requirements = bson_lookup_bson (bson, "runOnRequirements");
+      test_file->run_on_requirements =
+         bson_lookup_bson (bson, "runOnRequirements");
    }
    if (bson_has_field (bson, "createEntities")) {
       test_file->create_entities = bson_lookup_bson (bson, "createEntities");
@@ -125,7 +269,9 @@ test_file_new (test_runner_t* test_runner, bson_t* bson) {
    return test_file;
 }
 
-static void test_file_destroy (test_file_t *test_file) {
+static void
+test_file_destroy (test_file_t *test_file)
+{
    test_diagnostics.test_file = NULL;
    bson_destroy (test_file->tests);
    bson_destroy (test_file->initial_data);
@@ -134,7 +280,9 @@ static void test_file_destroy (test_file_t *test_file) {
    bson_free (test_file);
 }
 
-static test_t* test_new (test_file_t *test_file, bson_t* bson) {
+static test_t *
+test_new (test_file_t *test_file, bson_t *bson)
+{
    test_t *test;
 
    test = bson_malloc0 (sizeof (test_t));
@@ -158,7 +306,9 @@ static test_t* test_new (test_file_t *test_file, bson_t* bson) {
    return test;
 }
 
-static void test_destroy (test_t *test) {
+static void
+test_destroy (test_t *test)
+{
    test_diagnostics.test = NULL;
    bson_destroy (test->outcome);
    bson_destroy (test->expect_events);
@@ -257,11 +407,13 @@ handle_abort (int signo)
 {
    MONGOC_ERROR ("Test aborting");
    if (test_diagnostics.test_runner) {
-      MONGOC_ERROR ("URI: %s", mongoc_uri_get_string (test_diagnostics.test_runner->uri));
+      MONGOC_ERROR ("URI: %s",
+                    mongoc_uri_get_string (test_diagnostics.test_runner->uri));
    }
-   
+
    if (test_diagnostics.test_file) {
-      MONGOC_ERROR ("Test file description: %s", test_diagnostics.test_file->description);
+      MONGOC_ERROR ("Test file description: %s",
+                    test_diagnostics.test_file->description);
    }
 
    if (test_diagnostics.test) {
@@ -269,12 +421,16 @@ handle_abort (int signo)
    }
 }
 
-static void test_diagnostics_init (void) {
+static void
+test_diagnostics_init (void)
+{
    memset (&test_diagnostics, 0, sizeof (test_diagnostics));
    signal (SIGABRT, handle_abort);
 }
 
-static void test_diagnostics_cleanup (void) {
+static void
+test_diagnostics_cleanup (void)
+{
    signal (SIGABRT, SIG_DFL);
 }
 
@@ -302,7 +458,8 @@ check_schema_version (test_file_t *test_file)
       }
    }
 
-   test_error ("Unsupported schema version: %s", semver_to_string(&test_file->schema_version));
+   test_error ("Unsupported schema version: %s",
+               semver_to_string (&test_file->schema_version));
 }
 
 static bool
@@ -350,15 +507,10 @@ check_run_on_requirement (bson_t *run_on_requirement,
          bson_iter_bson (&reqiter, &topologies);
          BSON_FOREACH_BEGIN (&topologies, topology_iter)
          {
-            const char *topology = bson_iter_utf8 (&topology_iter, NULL);
-            if (0 == strcmp (topology, server_topology_type)) {
-               found = true;
-               break;
-            } else if (0 == strcmp (topology, "sharded") &&
-                       0 ==
-                          strcmp (server_topology_type, "sharded-replicaset")) {
-               /* If a requirement specifies a "sharded" topology and we are
-                * connected to a "sharded-replicaset", that is ok. */
+            const char *test_topology_type =
+               bson_iter_utf8 (&topology_iter, NULL);
+            if (is_topology_type_compatible (test_topology_type,
+                                             server_topology_type)) {
                found = true;
                break;
             }
@@ -382,7 +534,8 @@ check_run_on_requirement (bson_t *run_on_requirement,
 }
 
 static void
-check_run_on_requirements (test_runner_t *test_runner, bson_t* run_on_requirements)
+check_run_on_requirements (test_runner_t *test_runner,
+                           bson_t *run_on_requirements)
 {
    bson_string_t *fail_reasons;
    bool requirements_satisfied = false;
@@ -427,16 +580,18 @@ run_one_testfile (bson_t *bson)
    test_diagnostics_init ();
 
    test_runner = test_runner_new ();
+   test_runner_terminate_open_transactions (test_runner);
    test_file = test_file_new (test_runner, bson);
 
-   MONGOC_DEBUG ("running test: %s", test_file->description);
+   MONGOC_DEBUG ("running test file: %s", test_file->description);
 
    check_schema_version (test_file);
    if (test_file->run_on_requirements) {
       check_run_on_requirements (test_runner, test_file->run_on_requirements);
    }
 
-   BSON_FOREACH_BEGIN (test_file->tests, test_iter) {
+   BSON_FOREACH_BEGIN (test_file->tests, test_iter)
+   {
       test_t *test;
       bson_t test_bson;
 
