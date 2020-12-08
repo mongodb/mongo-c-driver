556d6f0d55f17eecaf30d1ab33b59c7f8ac175f0
davkor <david@adalogics.com>
2020-12-08 21:46:23 +0000

    fuzzer: fix tabs.

--- src/libbson/fuzz/fuzz_test_libbson.c
+++ src/libbson/fuzz/fuzz_test_libbson.c
@@ -4,16 +4,16 @@
 #include <bson/bson.h>
 
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-        char *nt = malloc(size+1);
-        memcpy(nt, data, size);
-        nt[size] = '\0';
-        bson_error_t error;
+    char *nt = malloc(size+1);
+    memcpy(nt, data, size);
+    nt[size] = '\0';
+    bson_error_t error;
 
-        bson_t b;
-        if (bson_init_from_json(&b, nt, -1, &error)) {
-                bson_destroy(&b);
-        }
+    bson_t b;
+    if (bson_init_from_json(&b, nt, -1, &error)) {
+        bson_destroy(&b);
+    }
 
-        free(nt);
-        return 0;
+    free(nt);
+    return 0;
 }
