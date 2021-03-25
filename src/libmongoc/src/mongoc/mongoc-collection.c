1e1d6d003679dcca520810695fb923fd31953772
Benjamin Rewis <benji.rewis@mongodb.com>
2021-03-25 14:49:36 -0400

    Check if reply is null

--- src/libmongoc/src/mongoc/mongoc-collection.c
+++ src/libmongoc/src/mongoc/mongoc-collection.c
@@ -909,7 +909,9 @@ mongoc_collection_estimated_document_count (
          /* Collection does not exist. From spec: return 0 but no err:
           * https://github.com/mongodb/specifications/blob/master/source/crud/crud.rst#estimateddocumentcount
           */
-         bson_reinit (reply);
+         if (reply) {
+            bson_reinit (reply);
+         }
          memset (error, 0, sizeof *error);
          count = 0;
          GOTO (done);
