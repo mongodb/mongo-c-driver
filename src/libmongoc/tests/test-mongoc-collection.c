7b6254ff04e462dc7b82eed7131019e2760f5a0b
Andreas Braun <git@alcaeus.org>
2021-05-21 09:43:25 +0200

    Add todo and fix clang-format

--- src/libmongoc/tests/test-mongoc-collection.c
+++ src/libmongoc/tests/test-mongoc-collection.c
@@ -6674,20 +6674,24 @@ test_collection_install (TestSuite *suite)
                       NULL,
                       test_framework_skip_if_slow_or_live);
    TestSuite_AddLive (suite, "/Collection/many_return", test_many_return);
-   TestSuite_AddFull (suite,
-                      "/Collection/insert_one_validate",
-                      test_insert_one_validate,
-                      NULL,
-                      NULL,
-                      TestSuite_CheckLive,
-                      test_framework_skip_if_max_wire_version_more_than_9);
-   TestSuite_AddFull (suite,
-                      "/Collection/insert_many_validate",
-                      test_insert_many_validate,
-                      NULL,
-                      NULL,
-                      TestSuite_CheckLive,
-                      test_framework_skip_if_max_wire_version_more_than_9);
+   TestSuite_AddFull (
+      suite,
+      "/Collection/insert_one_validate",
+      test_insert_one_validate,
+      NULL,
+      NULL,
+      TestSuite_CheckLive,
+      /* TODO: remove checks when removing validation for dots and dollars */
+      test_framework_skip_if_max_wire_version_more_than_9);
+   TestSuite_AddFull (
+      suite,
+      "/Collection/insert_many_validate",
+      test_insert_many_validate,
+      NULL,
+      NULL,
+      TestSuite_CheckLive,
+      /* TODO: remove checks when removing validation for dots and dollars */
+      test_framework_skip_if_max_wire_version_more_than_9);
    TestSuite_AddMockServerTest (suite, "/Collection/limit", test_find_limit);
    TestSuite_AddMockServerTest (
       suite, "/Collection/batch_size", test_find_batch_size);
@@ -6739,13 +6743,15 @@ test_collection_install (TestSuite *suite)
    TestSuite_AddLive (suite,
                       "/Collection/estimated_document_count_live",
                       test_estimated_document_count_live);
-   TestSuite_AddFull (suite,
-                      "/Collection/insert_bulk_validate",
-                      test_insert_bulk_validate,
-                      NULL,
-                      NULL,
-                      TestSuite_CheckLive,
-                      test_framework_skip_if_max_wire_version_more_than_9);
+   TestSuite_AddFull (
+      suite,
+      "/Collection/insert_bulk_validate",
+      test_insert_bulk_validate,
+      NULL,
+      NULL,
+      TestSuite_CheckLive,
+      /* TODO: remove checks when removing validation for dots and dollars */
+      test_framework_skip_if_max_wire_version_more_than_9);
    TestSuite_AddMockServerTest (suite,
                                 "/Collection/aggregate_with_batch_size",
                                 test_aggregate_with_batch_size);
