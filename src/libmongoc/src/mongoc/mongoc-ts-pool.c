93abae48ba1226c4dffb18f525ea98764040269b
vector-of-bool <vectorofbool@gmail.com>
2021-08-16 13:25:23 -0600

    Track pool size, and don't loop while holding the lock to clear

--- src/libmongoc/src/mongoc/mongoc-ts-pool.c
+++ src/libmongoc/src/mongoc/mongoc-ts-pool.c
@@ -12,6 +12,7 @@ typedef struct _mongoc_ts_pool {
    void (*element_dtor) (void *);
    pool_node *head;
    size_t element_size;
+   size_t size;
    bson_mutex_t mtx;
 } _mongoc_ts_pool;
 
@@ -22,6 +23,7 @@ mongoc_ts_pool_new (size_t element_size, void (*dtor) (void *))
    r->element_dtor = dtor;
    r->head = NULL;
    r->element_size = element_size;
+   r->size = 0;
    bson_mutex_init (&r->mtx);
    return r;
 }
@@ -37,14 +39,18 @@ mongoc_ts_pool_free (mongoc_ts_pool pool)
 void
 mongoc_ts_pool_clear (mongoc_ts_pool pool)
 {
+   pool_node *node;
    bson_mutex_lock (&pool->mtx);
-   while (pool->head) {
-      pool_node *n = pool->head;
+   node = pool->head;
+   pool->head = NULL;
+   pool->size = 0;
+   bson_mutex_unlock (&pool->mtx);
+   while (node) {
+      pool_node *n = node;
       pool->element_dtor (n->data);
-      pool->head = n->next;
+      node = n->next;
       bson_free (n);
    }
-   bson_mutex_unlock (&pool->mtx);
 }
 
 void *
@@ -60,7 +66,10 @@ mongoc_ts_pool_try_pop (mongoc_ts_pool pool, bool *const did_pop)
    pool_node *node;
    bson_mutex_lock (&pool->mtx);
    node = pool->head;
-   pool->head = node ? node->next : NULL;
+   if (node) {
+      pool->head = node->next;
+      pool->size--;
+   }
    bson_mutex_unlock (&pool->mtx);
    if (did_pop) {
       *did_pop = node != NULL;
@@ -77,6 +86,7 @@ mongoc_ts_pool_push (mongoc_ts_pool pool, void *userdata)
    bson_mutex_lock (&pool->mtx);
    node->next = pool->head;
    pool->head = node;
+   pool->size++;
    bson_mutex_unlock (&pool->mtx);
    return;
 }
@@ -84,11 +94,7 @@ mongoc_ts_pool_push (mongoc_ts_pool pool, void *userdata)
 bool
 mongoc_ts_pool_is_empty (mongoc_ts_pool pool)
 {
-   bool r;
-   bson_mutex_lock (&pool->mtx);
-   r = pool->head == NULL;
-   bson_mutex_unlock (&pool->mtx);
-   return r;
+   return mongoc_ts_pool_size (pool) == 0;
 }
 
 size_t
@@ -96,9 +102,7 @@ mongoc_ts_pool_size (mongoc_ts_pool pool)
 {
    size_t r = 0;
    bson_mutex_lock (&pool->mtx);
-   for (pool_node *node = pool->head; node; node = node->next) {
-      ++r;
-   }
+   r = pool->size;
    bson_mutex_unlock (&pool->mtx);
    return r;
 }
