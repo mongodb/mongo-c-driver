0b2fe646b7a60db23286082317da1278c2dd10f9
Jesse Williamson <jesse.williamson@mongodb.com>
2021-08-26 17:57:54 -0700

    Move inline documentation out of source.
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>

--- src/libmongoc/src/mongoc/mongoc-handshake.h
+++ src/libmongoc/src/mongoc/mongoc-handshake.h
@@ -28,79 +28,12 @@ BSON_BEGIN_DECLS
 
 #define MONGOC_HANDSHAKE_APPNAME_MAX 128
 
-/**
- * mongoc_add_driver_info:
- *
- * This function is intended for use by drivers which wrap the C Driver.
- * Calling this function will store the given strings as handshake data about
- * the system and driver by appending them to the handshake data for the
- * underlying C Driver. These values, along with other handshake data collected
- * during mongoc_init will be sent to the server as part of the initial
- * connection handshake in the "client" document. This function cannot be
- * called more than once, or after a handshake has been initiated.
- *
- * The passed in strings are copied, and don't have to remain valid after the
- * call to mongoc_add_driver_info(). The driver may store truncated
- * versions of the passed in strings.
- *
- * Note:
- *   This function allocates memory, and therefore caution should be used when
- *   using this in conjunction with bson_mem_set_vtable. If this function is
- *   called before bson_mem_set_vtable, then bson_mem_restore_vtable must be
- *   called before calling mongoc_cleanup. Failure to do so will result in
- *   memory being freed by the wrong allocator.
- *
- *
- * @driver_name: An optional string storing the name of the wrapping driver
- * @driver_version: An optional string storing the version of the wrapping
- * driver.
- * @platform: An optional string storing any information about the current
- *            platform, for example configure options or compile flags.
- *
- *
- * Returns true if the given fields are set successfully. Otherwise, it returns
- * false and logs an error.
- *
- * The default handshake data the driver sends with "hello" looks something
- * like:
- *  client: {
- *    driver: {
- *      name: "mongoc",
- *      version: "1.5.0"
- *    },
- *    os: {...},
- *    platform: "CC=gcc CFLAGS=-Wall -pedantic"
- *  }
- *
- * If we call
- *   mongoc_add_driver_info ("phongo", "1.1.8", "CC=clang")
- * and it returns true, the driver sends handshake data like:
- *  client: {
- *    driver: {
- *      name: "mongoc / phongo",
- *      version: "1.5.0 / 1.1.8"
- *    },
- *    os: {...},
- *    platform: "CC=gcc CFLAGS=-Wall -pedantic / CC=clang"
- *  }
- *
- */
 MONGOC_EXPORT (bool) 
 mongoc_add_driver_info (const char *driver_name,
                         const char *driver_version,
                         const char *platform);
 
-/**
- * mongoc_handshake_data_append:
- * 
- * This function is intended for use by drivers which wrap the C Driver. It
- * calls mongoc_add_driver_info().
- *
- * @See: mongoc_add_driver_info().
- * @Note: This is the older name for this previously-undocumented function; 
- *        mongoc_add_driver_info() should be preferred.
- *
-*/
+// Note: this just calls mongoc_add_driver_info():
 MONGOC_EXPORT (bool)
 mongoc_handshake_data_append (const char *driver_name,
                               const char *driver_version,
