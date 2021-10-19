a79042b5a784c42013b611b3b679b72b3e9c4004
Kevin Albertson <kevin.albertson@mongodb.com>
2021-10-19 13:46:04 -0400

    TEMP: add more logging

--- src/libmongoc/src/mongoc/mongoc-crypt.c
+++ src/libmongoc/src/mongoc/mongoc-crypt.c
@@ -378,6 +378,10 @@ _state_need_mongo_keys (_state_machine_t *state_machine, bson_error_t *error)
    /* 2. Feed all resulting documents back (if any) with repeated calls to
     * mongocrypt_ctx_mongo_feed. */
    while (mongoc_cursor_next (cursor, &key_bson)) {
+      char *doc = bson_as_json (key_bson, NULL);
+      MONGOC_DEBUG ("_state_need_mongo_keys got document: %s", doc);
+      bson_free (doc);
+
       mongocrypt_binary_destroy (key_bin);
       key_bin = mongocrypt_binary_new_from_data (
          (uint8_t *) bson_get_data (key_bson), key_bson->len);
@@ -649,7 +653,7 @@ _state_machine_run (_state_machine_t *state_machine,
 
    bson_init (result);
    while (true) {
-      MONGOC_DEBUG ("state is %s\n", state_to_string (mongocrypt_ctx_state (state_machine->ctx)));
+      MONGOC_DEBUG ("%s: state is %s\n", state_machine->db_name ? state_machine->db_name : "NULL", state_to_string (mongocrypt_ctx_state (state_machine->ctx)));
       switch (mongocrypt_ctx_state (state_machine->ctx)) {
       default:
       case MONGOCRYPT_CTX_ERROR:
