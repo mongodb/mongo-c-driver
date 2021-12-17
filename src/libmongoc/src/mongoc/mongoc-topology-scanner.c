9850339347f3515b250a611edebcc1da8c0755d4
Kevin Albertson <kevin.albertson@mongodb.com>
2021-12-17 11:52:37 -0500

    clang-format

--- src/libmongoc/src/mongoc/mongoc-topology-scanner.c
+++ src/libmongoc/src/mongoc/mongoc-topology-scanner.c
@@ -947,11 +947,17 @@ mongoc_topology_scanner_node_setup_tcp (mongoc_topology_scanner_node_t *node,
    }
 
    if (node->successful_dns_result) {
-      _begin_hello_cmd (node, NULL, false, node->successful_dns_result, 0, true /* use_handshake */);
+      _begin_hello_cmd (node,
+                        NULL,
+                        false,
+                        node->successful_dns_result,
+                        0,
+                        true /* use_handshake */);
    } else {
       LL_FOREACH2 (node->dns_results, iter, ai_next)
       {
-         _begin_hello_cmd (node, NULL, false, iter, delay, true /* use_handshake */);
+         _begin_hello_cmd (
+            node, NULL, false, iter, delay, true /* use_handshake */);
          /* each subsequent DNS result will have an additional 250ms delay. */
          delay += HAPPY_EYEBALLS_DELAY_MS;
       }
@@ -1057,7 +1063,12 @@ mongoc_topology_scanner_node_setup (mongoc_topology_scanner_node_t *node,
 
    /* if there is already a working stream, push it back to be re-scanned. */
    if (node->stream) {
-      _begin_hello_cmd (node, node->stream, true /* is_setup_done */, NULL, 0, false /* use_handshake */);
+      _begin_hello_cmd (node,
+                        node->stream,
+                        true /* is_setup_done */,
+                        NULL,
+                        0,
+                        false /* use_handshake */);
       node->stream = NULL;
       return;
    }
@@ -1069,7 +1080,8 @@ mongoc_topology_scanner_node_setup (mongoc_topology_scanner_node_t *node,
          node->ts->uri, &node->host, node->ts->initiator_context, error);
       if (stream) {
          success = true;
-         _begin_hello_cmd (node, stream, false, NULL, 0, true /* use_handshake */);
+         _begin_hello_cmd (
+            node, stream, false, NULL, 0, true /* use_handshake */);
       }
    } else {
       if (node->host.family == AF_UNIX) {
