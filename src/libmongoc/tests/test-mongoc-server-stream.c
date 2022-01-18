c4d2fd4119f2295f5a1cac6bc9a95e1c3c0aa4e6
Ezra Chung <ezra.chung@mongodb.com>
2022-01-18 11:28:09 -0600

    Update /server_stream/ties_server_description tests per feedback

--- src/libmongoc/tests/test-mongoc-server-stream.c
+++ src/libmongoc/tests/test-mongoc-server-stream.c
@@ -38,7 +38,64 @@
             " 'minWireVersion': %d,"      \
             " 'maxWireVersion': %d }",    \
             WIRE_VERSION_MIN,             \
-            WIRE_VERSION_MIN + 1)
+            WIRE_VERSION_DELETE_HINT)
+
+/* run_delete_with_hint_and_wc0 runs a delete command with a "hint" option and
+ * unacknowledged write concern.
+ *
+ * If @expect_error is true, expect a client-side error from a maxWireVersion <
+ * WIRE_VERSION_DELETE_HINT. */
+static void
+run_delete_with_hint_and_wc0 (bool expect_error,
+                              mongoc_client_t *client,
+                              mock_server_t *server)
+{
+   mongoc_collection_t *coll;
+   mongoc_write_concern_t *wc;
+   bson_t *delete_selector;
+   bson_t *delete_opts;
+   bool r;
+   bson_error_t error;
+   future_t *future;
+   request_t *request;
+
+   coll = mongoc_client_get_collection (client, "db", "coll");
+
+   wc = mongoc_write_concern_new ();
+   mongoc_write_concern_set_w (wc, MONGOC_WRITE_CONCERN_W_UNACKNOWLEDGED);
+
+   delete_selector = bson_new ();
+   delete_opts = BCON_NEW ("hint", "{", "}");
+   r = mongoc_write_concern_append (wc, delete_opts);
+   ASSERT_WITH_MSG (r, "mongoc_write_concern_append failed");
+
+   future = future_collection_delete_one (
+      coll, delete_selector, delete_opts, NULL /* reply */, &error);
+   if (expect_error) {
+      /* Expect a client side error. The server does not receive anything. */
+      r = future_get_bool (future);
+      ASSERT_ERROR_CONTAINS (
+         error,
+         MONGOC_ERROR_COMMAND,
+         MONGOC_ERROR_COMMAND_INVALID_ARG,
+         "The selected server does not support hint for delete");
+      ASSERT (!r);
+   } else {
+      request = mock_server_receives_msg (server,
+                                          MONGOC_MSG_MORE_TO_COME,
+                                          tmp_bson ("{ 'delete': 'coll' }"),
+                                          tmp_bson ("{'q': {}, 'hint': {}}"));
+      mock_server_replies_ok_and_destroys (request);
+      r = future_get_bool (future);
+      ASSERT (r);
+   }
+
+   future_destroy (future);
+   bson_destroy (delete_opts);
+   bson_destroy (delete_selector);
+   mongoc_write_concern_destroy (wc);
+   mongoc_collection_destroy (coll);
+}
 
 /* Test that a connection uses the server description from the handshake when
  * checking wire version (instead of the server description from the topology
@@ -107,8 +164,17 @@ test_server_stream_ties_server_description_pooled (void *unused)
       client_two, true /* for writes */, NULL /* read prefs */, &error);
    ASSERT_OR_PRINT (sd, error);
    ASSERT_MATCH (mongoc_server_description_hello_response (sd),
-                 tmp_str ("{'maxWireVersion': %d}", WIRE_VERSION_MIN + 1));
+                 tmp_str ("{'maxWireVersion': %d}", WIRE_VERSION_DELETE_HINT));
    mongoc_server_description_destroy (sd);
+
+   /* Expect client_one to continue to use maxWireVersion=WIRE_VERSION_MIN for
+    * wire version checks. Expect an error when using delete with hint and
+    * unacknowledged write concern. */
+   run_delete_with_hint_and_wc0 (true, client_one, server);
+   /* Expect client_two to continue to use
+    * maxWireVersion=WIRE_VERSION_DELETE_HINT for wire version checks. Expect no
+    * error when using delete with hint and unacknowledged write concern. */
+   run_delete_with_hint_and_wc0 (false, client_two, server);
 
    mock_server_destroy (server);
    mongoc_uri_destroy (uri);
@@ -181,6 +247,11 @@ test_server_stream_ties_server_description_single (void *unused)
    ASSERT_MATCH (mongoc_server_description_hello_response (sd),
                  tmp_str ("{'maxWireVersion': %d}", WIRE_VERSION_MIN));
    mongoc_server_description_destroy (sd);
+   
+   /* Expect client to continue to use maxWireVersion=WIRE_VERSION_DELETE_HINT for
+    * wire version checks. Expect no error when using delete with hint and
+    * unacknowledged write concern. */
+   run_delete_with_hint_and_wc0 (false, client, server);
 
    mock_server_destroy (server);
    mongoc_uri_destroy (uri);
