a6354fb8cdb16b9e29ec8be2be770c072a1aec57
vector-of-bool <vectorofbool@gmail.com>
2022-01-24 20:28:17 +0000

    Support a new sanitize option in evergreen generator

--- build/cmake/Sanitizers.cmake
+++ build/cmake/Sanitizers.cmake
@@ -5,6 +5,7 @@ set (MONGO_SANITIZE "" CACHE STRING "Semicolon/comma-separated list of sanitizer
 string(REPLACE ";" "," _sanitize "${MONGO_SANITIZE}")
 
 if (_sanitize)
+    message (STATUS "Building with -fsanitize=${_sanitize}")
     add_compile_options ("-fsanitize=${_sanitize}")
     link_libraries ("-fsanitize=${_sanitize}")
 endif ()
