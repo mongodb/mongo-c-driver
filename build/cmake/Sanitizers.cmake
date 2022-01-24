0960bb3e9013ea41a074fb1a7d3f8ed4e77907f4
vector-of-bool <vectorofbool@gmail.com>
2022-01-24 20:28:17 +0000

    Check sanitizer flags

--- build/cmake/Sanitizers.cmake
+++ build/cmake/Sanitizers.cmake
@@ -1,3 +1,6 @@
+include (CheckCSourceCompiles)
+include (CMakePushCheckState)
+
 ## Directly control the options passed to -fsanitize
 set (MONGO_SANITIZE "" CACHE STRING "Semicolon/comma-separated list of sanitizers to apply when building")
 
@@ -5,7 +8,28 @@ set (MONGO_SANITIZE "" CACHE STRING "Semicolon/comma-separated list of sanitizer
 string(REPLACE ";" "," _sanitize "${MONGO_SANITIZE}")
 
 if (_sanitize)
-    message (STATUS "Building with -fsanitize=${_sanitize}")
-    add_compile_options ("-fsanitize=${_sanitize}")
-    link_libraries ("-fsanitize=${_sanitize}")
+    string (MAKE_C_IDENTIFIER "HAVE_SANITIZE_${_sanitize}" ident)
+    string (TOUPPER "${ident}" varname)
+    set (flag "-fsanitize=${_sanitize}")
+
+    cmake_push_check_state ()
+        set (CMAKE_REQUIRED_FLAGS "${flag}")
+        set (CMAKE_REQUIRED_LIBRARIES "${flag}")
+        check_c_source_compiles ([[
+            #include <stdio.h>
+
+            int main (void) {
+                puts ("Hello, world!");
+                return 0;
+            }
+        ]] "${varname}")
+    cmake_pop_check_state ()
+
+    if (NOT "${${varname}}")
+        message (SEND_ERROR "Requested sanitizer option '${flag}' is not supported by the compiler+linker")
+    else ()
+        message (STATUS "Building with ${flag}")
+        add_compile_options ("${flag}")
+        link_libraries ("${flag}")
+    endif ()
 endif ()
