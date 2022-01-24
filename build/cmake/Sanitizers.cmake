24c3c0dbe4ed0be0e46c4545ad175c41fe17d2fe
vector-of-bool <vectorofbool@gmail.com>
2022-01-24 20:28:17 +0000

    File that allows easier toggling of sanitization options

---
+++ build/cmake/Sanitizers.cmake
@@ -0,0 +1,10 @@
+## Directly control the options passed to -fsanitize
+set (MONGO_SANITIZE "" CACHE STRING "Semicolon/comma-separated list of sanitizers to apply when building")
+
+# Replace commas with semicolons for the genex
+string(REPLACE ";" "," _sanitize "${MONGO_SANITIZE}")
+
+if (_sanitize)
+    add_compile_options ("-fsanitize=${_sanitize}")
+    link_libraries ("-fsanitize=${_sanitize}")
+endif ()
