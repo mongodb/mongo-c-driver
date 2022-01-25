2754eb24a5b8c05d467524a3dba1819923d100a0
vector-of-bool <vectorofbool@gmail.com>
2022-01-25 02:37:21 +0000

    Doc comment in mdb-ctl

--- build/cmake/mdb-ctrl.cmake
+++ build/cmake/mdb-ctrl.cmake
@@ -1,3 +1,41 @@
+#[[
+
+This CMake script is intended to be executed with '-P' to control a MongoDB
+server executable as a test fixture. It uses the following arguments:
+
+DO={START,STOP} (required)
+    The action to perform. Must be 'START' or 'STOP'
+
+RUNDIR=<dirpath> (required)
+    A base directory in which to base all server executions.
+
+FIXTURE_NAME=<string> (required)
+    A unique name of the fixture server to control. The name is arbitrary, but
+    should be unique.
+
+MDB_EXE=<filepath> (required)
+    The path to a MongoDB server executable to use to control the test fixture.
+
+MDB_PORT=<int> (required for DO=START)
+    An integer TCP port on which the server should bind.
+
+SERVER_ARGS=<semicolon-separated-list> (optional for DO=START)
+    Additional arguments that should be given to the server executable when it
+    is started. MAY NOT be any of `--port`, `--pidfilepath`, `--fork`,
+    `--verbose`, `--logpath`, `--dbpath`, or `--shutdown`, as those options are
+    controlled by this script.
+
+---
+
+If DO=START, and this script detects that there was a prior server test fixture
+with the given name, it will ensure that the test fixture is not running before
+trying to start it again.
+
+If DO=STOP, this script will attempt to stop the server test fixture. If it
+fails to stop, then this script will exit with an error
+
+]]
+
 # Fixture properties:
 get_filename_component (__fixture_dir "${RUNDIR}/_test-db/${FIXTURE_NAME}" ABSOLUTE)
 set (__mongod_exe "${MDB_EXE}")
