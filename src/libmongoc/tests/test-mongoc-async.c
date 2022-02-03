893d040abba35c0222191a9848bca0e5f54f20f1
Jesse Williamson <jesse.williamson@mongodb.com>
2022-02-02 16:11:58 -0800

    Comment added as per review suggestion.
    Signed-off-by: Jesse Williamson <jesse.williamson@mongodb.com>

--- src/libmongoc/tests/test-mongoc-async.c
+++ src/libmongoc/tests/test-mongoc-async.c
@@ -299,7 +299,7 @@ test_large_hello (void *ctx)
                          NULL,
                          "admin",
                          &q,
-                         MONGOC_OPCODE_QUERY,
+                         MONGOC_OPCODE_QUERY, /* used by legacy hello */
                          &test_large_hello_helper,
                          NULL,
                          TIMEOUT);
