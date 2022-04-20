28d76973dec24ce4fe516412230455f02e2c16b3
Ezra Chung <ezra.chung@mongodb.com>
2022-04-20 16:15:32 -0500

    Add client_side_encryption/unified to unified test suite

--- src/libmongoc/tests/unified/runner.c
+++ src/libmongoc/tests/unified/runner.c
@@ -1748,4 +1748,6 @@ test_install_unified (TestSuite *suite)
    run_unified_tests (suite, JSON_DIR, "change_streams/unified");
 
    run_unified_tests (suite, JSON_DIR, "load_balancers");
+
+   run_unified_tests (suite, JSON_DIR, "client_side_encryption/unified");
 }
