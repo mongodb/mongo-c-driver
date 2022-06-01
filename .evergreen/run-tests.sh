529dd918ec6986b62c59648eb39975a8b7095b62
Kevin Albertson <kevin.albertson@mongodb.com>
2022-06-01 10:18:37 -0400

    fix setting env var for shared library path

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -111,9 +111,9 @@ if [ "$CLIENT_SIDE_ENCRYPTION" = "on" ]; then
    wait_for_kms_server 5698
    echo "Waiting for mock KMS servers to start... done."
    if ! test -d /cygdrive/c; then
-      # We have trouble with this test on Windows. only set csflePath on other platforms
-      export MONGOC_TEST_CSFLE_PATH="$(find . -wholename '*src/libmongoc/mongo_csfle_v1.*' -and -regex '.*\(.dll\|.dylib\|.so\)' | head -n1)"
-      echo "Setting env csflePath: [$MONGOC_TEST_CSFLE_PATH]"
+      # We have trouble with this test on Windows. only set cryptSharedLibPath on other platforms
+      export MONGOC_TEST_CRYPT_SHARED_LIB_PATH="$(find . -wholename '*src/libmongoc/mongo_crypt_v1.*' -and -regex '.*\(.dll\|.dylib\|.so\)' | head -n1)"
+      echo "Setting env cryptSharedLibPath: [$MONGOC_TEST_CRYPT_SHARED_LIB_PATH]"
    fi
 fi
 
