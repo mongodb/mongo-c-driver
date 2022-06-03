f2c650bda53b9cc5d22c1907255b7158c460da28
Kevin Albertson <kevin.albertson@mongodb.com>
2022-06-02 20:50:28 -0400

    TEMP: test with fix-leak branch of libmongocrypt

--- .evergreen/compile-unix.sh
+++ .evergreen/compile-unix.sh
@@ -228,7 +228,7 @@ pkg-config --modversion libssl || true
 if [ "$COMPILE_LIBMONGOCRYPT" = "ON" ]; then
    # Build libmongocrypt, using the previously fetched installed source.
    # TODO (CDRIVER-4397): add "--branch 1.5.0-rc0" in git clone once libmongocrypt 1.5.0-rc0 is released.
-   git clone https://github.com/mongodb/libmongocrypt
+   git clone https://github.com/kevinAlbs/libmongocrypt --branch fix-leak
    mkdir libmongocrypt/cmake-build
    cd libmongocrypt/cmake-build
    $CMAKE -DENABLE_SHARED_BSON=ON -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" -DCMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH" ../
