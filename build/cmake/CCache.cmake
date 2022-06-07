4053110e72c3183134535ac7feec106c0a8c40b2
Ezra Chung <ezra.chung@mongodb.com>
2022-06-07 12:42:32 -0500

    Guard against redundant ccache detection

--- build/cmake/CCache.cmake
+++ build/cmake/CCache.cmake
@@ -8,31 +8,33 @@
     ON or OFF.
 ]]
 
-# Find and enable ccache for compiling
-find_program (CCACHE_EXECUTABLE ccache)
-if (CCACHE_EXECUTABLE)
-    message (STATUS "Found ccache: ${CCACHE_EXECUTABLE}")
+# Find and enable ccache for compiling if not already found.
+if (NOT DEFINED MONGO_USE_CCACHE)
+    find_program (CCACHE_EXECUTABLE ccache)
+    if (CCACHE_EXECUTABLE)
+        message (STATUS "Found ccache: ${CCACHE_EXECUTABLE}")
 
-    execute_process (
-        COMMAND ${CCACHE_EXECUTABLE} --version | perl -ne "print $1 if /^ccache version (.+)$/"
-        OUTPUT_VARIABLE CCACHE_VERSION
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
+        execute_process (
+            COMMAND ${CCACHE_EXECUTABLE} --version | perl -ne "print $1 if /^ccache version (.+)$/"
+            OUTPUT_VARIABLE CCACHE_VERSION
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
 
-    # Assume `ccache --version` mentions a simple version string, e.g. 1.2.3
-    set (SIMPLE_SEMVER_REGEX "([0-9]+)\.([0-9]+)\.([0-9]+)")
-    string (REGEX MATCH "${SIMPLE_SEMVER_REGEX}" CCACHE_VERSION ${CCACHE_VERSION})
+        # Assume `ccache --version` mentions a simple version string, e.g. 1.2.3
+        set (SIMPLE_SEMVER_REGEX "([0-9]+)\.([0-9]+)\.([0-9]+)")
+        string (REGEX MATCH "${SIMPLE_SEMVER_REGEX}" CCACHE_VERSION ${CCACHE_VERSION})
 
-    # Avoid spurious "ccache.conf: No such file or directory" errors due to ccache being invoked in parallel, which was patched in ccache version 3.4.3.
-    if (${CCACHE_VERSION} VERSION_LESS 3.4.3)
-        message (STATUS "Detected ccache version ${CCACHE_VERSION} is less than 3.4.3, which may lead to spurious failures when run in parallel. See https://github.com/ccache/ccache/issues/260 for more information.")
-        message (STATUS "Compiling with CCache disabled. Enable by setting MONGO_USE_CCACHE to ON")
-        option (MONGO_USE_CCACHE "Use CCache when compiling" OFF)
-    else ()
-        message (STATUS "Compiling with CCache enabled. Disable by setting MONGO_USE_CCACHE to OFF")
-        option (MONGO_USE_CCACHE "Use CCache when compiling" ON)
-    endif ()
-endif ()
+        # Avoid spurious "ccache.conf: No such file or directory" errors due to ccache being invoked in parallel, which was patched in ccache version 3.4.3.
+        if (${CCACHE_VERSION} VERSION_LESS 3.4.3)
+            message (STATUS "Detected ccache version ${CCACHE_VERSION} is less than 3.4.3, which may lead to spurious failures when run in parallel. See https://github.com/ccache/ccache/issues/260 for more information.")
+            message (STATUS "Compiling with CCache disabled. Enable by setting MONGO_USE_CCACHE to ON")
+            option (MONGO_USE_CCACHE "Use CCache when compiling" OFF)
+        else ()
+            message (STATUS "Compiling with CCache enabled. Disable by setting MONGO_USE_CCACHE to OFF")
+            option (MONGO_USE_CCACHE "Use CCache when compiling" ON)
+        endif ()
+    endif (CCACHE_EXECUTABLE)
+endif (NOT DEFINED MONGO_USE_CCACHE)
 
 if (MONGO_USE_CCACHE)
     set (CMAKE_CXX_COMPILER_LAUNCHER "${CCACHE_EXECUTABLE}")
