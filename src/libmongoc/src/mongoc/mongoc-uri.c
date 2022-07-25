1c3a7e940303dfc990017e2a920a5031624ae389
Ezra Chung <ezra.chung@mongodb.com>
2022-07-25 10:51:03 -0500

    Fix alignment of allocated mongoc_uri_t

--- src/libmongoc/src/mongoc/mongoc-uri.c
+++ src/libmongoc/src/mongoc/mongoc-uri.c
@@ -1912,7 +1912,8 @@ mongoc_uri_new_with_error (const char *uri_string, bson_error_t *error)
    mongoc_uri_t *uri;
    int32_t max_staleness_seconds;
 
-   uri = (mongoc_uri_t *) bson_malloc0 (sizeof *uri);
+   uri =
+      bson_aligned_alloc0 (BSON_ALIGNOF (mongoc_uri_t), sizeof (mongoc_uri_t));
    bson_init (&uri->raw);
    bson_init (&uri->options);
    bson_init (&uri->credentials);
@@ -2314,7 +2315,8 @@ mongoc_uri_copy (const mongoc_uri_t *uri)
 
    BSON_ASSERT (uri);
 
-   copy = (mongoc_uri_t *) bson_malloc0 (sizeof (*copy));
+   copy =
+      bson_aligned_alloc0 (BSON_ALIGNOF (mongoc_uri_t), sizeof (mongoc_uri_t));
 
    copy->str = bson_strdup (uri->str);
    copy->is_srv = uri->is_srv;
