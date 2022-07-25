d7fb641d70cffdbeb110ce035e7b5a32e2987c1d
Ezra Chung <ezra.chung@mongodb.com>
2022-07-25 13:26:50 -0500

    Fix alignment of allocated mongoc_gridfs_file_t

--- src/libmongoc/src/mongoc/mongoc-gridfs-file.c
+++ src/libmongoc/src/mongoc/mongoc-gridfs-file.c
@@ -220,7 +220,8 @@ _mongoc_gridfs_file_new_from_bson (mongoc_gridfs_t *gridfs, const bson_t *data)
    BSON_ASSERT (gridfs);
    BSON_ASSERT (data);
 
-   file = (mongoc_gridfs_file_t *) bson_malloc0 (sizeof *file);
+   file = bson_aligned_alloc0 (BSON_ALIGNOF (mongoc_gridfs_file_t),
+                               sizeof (mongoc_gridfs_file_t));
 
    file->gridfs = gridfs;
    bson_copy_to (data, &file->bson);
@@ -318,7 +319,8 @@ _mongoc_gridfs_file_new (mongoc_gridfs_t *gridfs, mongoc_gridfs_file_opt_t *opt)
       opt = &default_opt;
    }
 
-   file = (mongoc_gridfs_file_t *) bson_malloc0 (sizeof *file);
+   file = bson_aligned_alloc0 (BSON_ALIGNOF (mongoc_gridfs_file_t),
+                               sizeof (mongoc_gridfs_file_t));
 
    file->gridfs = gridfs;
    file->is_dirty = 1;
