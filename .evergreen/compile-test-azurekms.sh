e47105aa990ba0dce4b54c492bd6948caefaa71d
Kevin Albertson <kevin.albertson@mongodb.com>
2022-10-18 17:55:15 +0000

    clone libmongocrypt 1.6.0

--- .evergreen/compile-test-azurekms.sh
+++ .evergreen/compile-test-azurekms.sh
@@ -8,7 +8,7 @@ ROOT=$(pwd)
 INSTALL_DIR=$ROOT/install
 . .evergreen/find-cmake.sh
 echo "Installing libmongocrypt ... begin"
-git clone https://github.com/mongodb/libmongocrypt --branch 1.5.2
+git clone https://github.com/mongodb/libmongocrypt --branch 1.6.0
 cd libmongocrypt
 $CMAKE -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
     -DBUILD_TESTING=OFF \
