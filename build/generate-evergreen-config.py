ba7db5b32082f78b3fb46ecdeff12a3e599c2afa
Kevin Albertson <kevin.albertson@mongodb.com>
2022-10-19 10:52:08 -0400

    add minimal support for TaskGroup

--- build/generate-evergreen-config.py
+++ build/generate-evergreen-config.py
@@ -41,6 +41,8 @@ drivers-evergreen-tools#subdirectory=evergreen_config_generator\
 from evergreen_config_lib.functions import all_functions
 from evergreen_config_lib.tasks import all_tasks
 from evergreen_config_lib.variants import all_variants
+from evergreen_config_lib.taskgroups import all_task_groups
+from evergreen_config_lib.testazurekms import testazurekms_generate
 
 config = OD([
     ('stepback', True),
@@ -66,6 +68,7 @@ config = OD([
         OD([('func', 'backtrace')])
     ]),
     ('tasks', all_tasks),
+    ('task_groups', all_task_groups),
     ('buildvariants', all_variants),
 ])
 
