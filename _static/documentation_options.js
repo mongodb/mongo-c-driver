24a85ff1c383784994d65f046a5212e44b5d2d9c
vector-of-bool <vectorofbool@gmail.com>
2022-10-19 19:09:18 +0000

    libbson and libmongoc 1.23.1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.23.0',
-    LANGUAGE: 'en',
+    VERSION: '1.23.1',
+    LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: false,
