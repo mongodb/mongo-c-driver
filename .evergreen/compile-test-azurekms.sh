4a6de7764e0a7a2c89a98627ae155dccf841a354
Kevin Albertson <kevin.albertson@mongodb.com>
2022-11-03 08:27:49 -0400

    CDRIVER-4454 add integration tests for Azure KMS (#1124)

---
+++ .evergreen/compile-test-azurekms.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Working directory is expected to be mongo-c-driver repo.
+ROOT=$(pwd)
+INSTALL_DIR=$ROOT/install
+. .evergreen/find-cmake.sh
+echo "Installing libmongocrypt ... begin"
+git clone https://github.com/mongodb/libmongocrypt --branch 1.6.0
+$CMAKE -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+    -DBUILD_TESTING=OFF \
+    "-H$ROOT/libmongocrypt" \
+    "-B$ROOT/libmongocrypt"
+$CMAKE --build "$ROOT/libmongocrypt" --target install
+echo "Installing libmongocrypt ... end"
+
+echo "Compile test-azurekms ... begin"
+# Disable unnecessary dependencies. test-azurekms is copied to a remote host for testing, which may not have all dependent libraries.
+$CMAKE \
+    -DENABLE_SASL=OFF \
+    -DENABLE_SNAPPY=OFF \
+    -DENABLE_ZSTD=OFF \
+    -DENABLE_ZLIB=OFF \
+    -DENABLE_ICU=OFF \
+    -DENABLE_SRV=OFF \
+    -DENABLE_CLIENT_SIDE_ENCRYPTION=ON \
+    -DCMAKE_PREFIX_PATH=$INSTALL_DIR \
+    .
+$CMAKE --build . --target test-azurekms
+echo "Compile test-azurekms ... end"
