cebe32f3aaaeb7f8449431d6b7347ff44fa8434d
Ezra Chung <ezra.chung@mongodb.com>
2023-02-13 16:04:26 -0600

    Remove TEST_NOSSL_MATRIX from sanitizers.asan_sasl

--- .evergreen/config_generator/components/sanitizers/asan_sasl.py
+++ .evergreen/config_generator/components/sanitizers/asan_sasl.py
@@ -2,7 +2,6 @@ from config_generator.etc.compile import generate_compile_tasks
 
 from config_generator.etc.sanitizers.test import generate_test_tasks
 
-from config_generator.components.sasl.nossl import SaslOffNoSSLCompile
 from config_generator.components.sasl.openssl import SaslCyrusOpenSSLCompile
 
 from config_generator.components.sanitizers.asan import TAG
@@ -11,16 +10,11 @@ from config_generator.components.sanitizers.asan import TAG
 # pylint: disable=line-too-long
 # fmt: off
 COMPILE_MATRIX = [
-    ('ubuntu1604', 'clang', None, ['off', 'cyrus']),
-    ('ubuntu1804', 'clang', None, ['off', 'cyrus']),
+    ('ubuntu1604', 'clang', None, ['cyrus']),
+    ('ubuntu1804', 'clang', None, ['cyrus']),
 ]
 
-TEST_NOSSL_MATRIX = [
-    ('ubuntu1604', 'clang', None, 'off', ['noauth'], ['server', 'replica', 'sharded'], ['3.6',                                            ]),
-    ('ubuntu1804', 'clang', None, 'off', ['noauth'], ['server', 'replica', 'sharded'], [       '4.0', '4.2', '4.4', '5.0', '6.0', 'latest']),
-]
-
-TEST_OPENSSL_MATRIX = [
+TEST_MATRIX = [
     ('ubuntu1604', 'clang', None, 'cyrus', ['auth'], ['server', 'replica', 'sharded'], ['3.6',                                            ]),
     ('ubuntu1804', 'clang', None, 'cyrus', ['auth'], ['server', 'replica', 'sharded'], [       '4.0', '4.2', '4.4', '5.0', '6.0', 'latest']),
 ]
@@ -28,46 +22,19 @@ TEST_OPENSSL_MATRIX = [
 # pylint: enable=line-too-long
 
 
-MORE_TAGS = ['asan']
-
-SASL_TO_FUNC = {
-    'off': SaslOffNoSSLCompile,
-    'cyrus': SaslCyrusOpenSSLCompile,
-}
-
-
-def nossl_tasks():
-    res = []
-
-    SSL = 'nossl'
-
-    res += generate_compile_tasks(
-        SSL, TAG, SASL_TO_FUNC, COMPILE_MATRIX, MORE_TAGS
-    )
-
-    res += generate_test_tasks(SSL, TAG, TEST_NOSSL_MATRIX, MORE_TAGS)
-
-    return res
-
-
-def openssl_tasks():
+def tasks():
     res = []
 
     SSL = 'openssl'
 
+    SASL_TO_FUNC = {
+        'cyrus': SaslCyrusOpenSSLCompile,
+    }
+
     res += generate_compile_tasks(
-        SSL, TAG, SASL_TO_FUNC, COMPILE_MATRIX, MORE_TAGS
+        SSL, TAG, SASL_TO_FUNC, COMPILE_MATRIX, ['asan']
     )
 
-    res += generate_test_tasks(SSL, TAG, TEST_OPENSSL_MATRIX, MORE_TAGS)
-
-    return res
-
-
-def tasks():
-    res = []
-
-    res += nossl_tasks()
-    res += openssl_tasks()
+    res += generate_test_tasks(SSL, TAG, TEST_MATRIX, ['asan'])
 
     return res
