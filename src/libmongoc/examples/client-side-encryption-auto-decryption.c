fbaadfa87a9524056810010feb0985dfe2f4add8
Ezra Chung <ezra.chung@mongodb.com>
2023-03-08 12:19:57 -0600

    client-side-encryption-auto-decryption.c: address C4267 size_t conversion warnings

--- src/libmongoc/examples/client-side-encryption-auto-decryption.c
+++ src/libmongoc/examples/client-side-encryption-auto-decryption.c
@@ -166,6 +166,9 @@ main (void)
       goto fail;
    }
 
+   const size_t len = strlen (to_encrypt.value.v_utf8.str);
+   BSON_ASSERT (bson_in_range_unsigned (uint32_t, len));
+
    /* Explicitly encrypt a field. */
    encrypt_opts = mongoc_client_encryption_encrypt_opts_new ();
    mongoc_client_encryption_encrypt_opts_set_algorithm (
@@ -174,7 +177,7 @@ main (void)
       encrypt_opts, "mongoc_encryption_example_4");
    to_encrypt.value_type = BSON_TYPE_UTF8;
    to_encrypt.value.v_utf8.str = "123456789";
-   to_encrypt.value.v_utf8.len = strlen (to_encrypt.value.v_utf8.str);
+   to_encrypt.value.v_utf8.len = (uint32_t) len;
 
    ret = mongoc_client_encryption_encrypt (
       client_encryption, &to_encrypt, encrypt_opts, &encrypted_field, &error);
