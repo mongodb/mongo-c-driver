22a574c2994ec4f4988b23efd7e6a3001d3227eb
Ezra Chung <ezra.chung@mongodb.com>
2023-03-10 13:41:36 -0600

    mock-server.c: address C4267 size_t conversion warnings

--- src/libmongoc/tests/mock_server/mock-server.c
+++ src/libmongoc/tests/mock_server/mock-server.c
@@ -363,8 +363,14 @@ mock_server_run (mock_server_t *server)
       bind_addr_len = sizeof (default_bind_addr);
    }
 
-   if (-1 == mongoc_socket_bind (
-                ssock, (struct sockaddr *) bind_addr, bind_addr_len)) {
+   // socklen_t: an unsigned opaque integral type of length of at least 32 bits.
+   // To forestall portability problems, it is recommended that applications
+   // should not use values larger than 2^32 - 1.
+   BSON_ASSERT (bson_in_range_unsigned (uint32_t, bind_addr_len));
+
+   if (-1 == mongoc_socket_bind (ssock,
+                                 (struct sockaddr *) bind_addr,
+                                 (uint32_t) bind_addr_len)) {
       perror ("Failed to bind socket");
       return 0;
    }
