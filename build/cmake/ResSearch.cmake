ea2ef8a4c0b1ef46fdc31ff919f207d3029b900a
vector-of-bool <vectorofbool@gmail.com>
2023-06-06 06:45:21 +0000

    Refactor pkg-config generation to use target properties
    This change uses target properties to fill in the slots in the
    generated pkg-config files. Previously this was unstructured strings
    and flag juggling.

--- build/cmake/ResSearch.cmake
+++ build/cmake/ResSearch.cmake
@@ -1,10 +1,12 @@
 include(CheckSymbolExists)
 
-set(resolve_libs)
+# The name of the library that performs name resolution, suitable for giving to the "-l" link flag
+set(RESOLVE_LIB_NAME)
+# If TRUE, then the C runtime provides the name resolution that we need
 set(resolve_is_libc FALSE)
 
 if(WIN32)
-    set(resolve_libs Dnsapi)
+    set(RESOLVE_LIB_NAME Dnsapi)
     set(_MONGOC_HAVE_DNSAPI 1)
 else()
     # Thread-safe DNS query function for _mongoc_client_get_srv.
@@ -14,7 +16,7 @@ else()
     check_symbol_exists(res_nclose resolv.h _MONGOC_HAVE_RES_NCLOSE)
     if(MONGOC_HAVE_RES_NSEARCH)
         # We have res_nsearch. Call res_ndestroy (BSD/Mac) or res_nclose (Linux)?
-        set(resolve_libs resolv)
+        set(RESOLVE_LIB_NAME resolv)
     elseif(CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
         # On FreeBSD, the following line does not properly detect res_search,
         # which is included in libc on FreeBSD:
@@ -32,7 +34,7 @@ else()
         # Thread-unsafe function.
         check_symbol_exists(res_search resolv.h _MONGOC_HAVE_RES_SEARCH)
         if(_MONGOC_HAVE_RES_SEARCH)
-            set(resolve_libs resolv)
+            set(RESOLVE_LIB_NAME resolv)
         endif()
     endif()
 endif()
@@ -43,11 +45,12 @@ _mongo_pick(MONGOC_HAVE_RES_NDESTROY 1 0 _MONGOC_HAVE_RES_NDESTROY)
 _mongo_pick(MONGOC_HAVE_RES_NCLOSE 1 0 _MONGOC_HAVE_RES_NCLOSE)
 _mongo_pick(MONGOC_HAVE_RES_SEARCH 1 0 _MONGOC_HAVE_RES_SEARCH)
 
-if(resolve_libs OR resolve_is_libc)
+if(RESOLVE_LIB_NAME OR resolve_is_libc)
     # Define the resolver interface:
     add_library(_mongoc-resolve INTERFACE)
-    target_link_libraries(_mongoc-resolve INTERFACE ${resolve_libs})
-    add_library(mongo::c::detail::resolve ALIAS _mongoc-resolve)
-    set_property(TARGET _mongoc-resolve PROPERTY EXPORT_NAME c::detail::resolve)
+    add_library(mongo::detail::c_resolve ALIAS _mongoc-resolve)
+    set_target_properties(_mongoc-resolve PROPERTIES
+        INTERFACE_LINK_LIBRARIES "${RESOLVE_LIB_NAME}"
+        EXPORT_NAME detail::c_resolve)
     install(TARGETS _mongoc-resolve EXPORT mongoc-targets)
 endif()
