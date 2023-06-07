961982ae8bc0fabd7132a90134037752384c7e84
Kevin Albertson <kevin.albertson@mongodb.com>
2023-06-07 15:26:43 -0400

    silence scan-build

--- src/libmongoc/tests/test-conveniences.c
+++ src/libmongoc/tests/test-conveniences.c
@@ -1302,7 +1302,7 @@ match_bson_arrays (const bson_t *array, const bson_t *pattern, match_ctx_t *ctx)
 
       derive (ctx, &derived, bson_iter_key (&array_iter));
 
-      if (ctx->visitor_fn) {
+      if (ctx && ctx->visitor_fn) {
          match_action_t action =
             ctx->visitor_fn (ctx, &pattern_iter, &array_iter);
          if (action == MATCH_ACTION_ABORT) {
