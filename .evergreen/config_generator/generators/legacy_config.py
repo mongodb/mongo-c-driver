6995e88968091bfb51d9551c7356735cec01e912
vector-of-bool <vectorofbool@gmail.com>
2023-06-13 19:29:21 +0000

    Use absolute path to launch the legacy config

--- .evergreen/config_generator/generators/legacy_config.py
+++ .evergreen/config_generator/generators/legacy_config.py
@@ -1,12 +1,16 @@
 import subprocess
+from pathlib import Path
 import sys
+
+HERE = Path(__file__).parent.resolve()
+EVG_DIR = HERE.parent.parent
 
 
 def generate():
     subprocess.run(
         args=[
             sys.executable,
-            '.evergreen/legacy_config_generator/generate-evergreen-config.py',
+            str(EVG_DIR / "legacy_config_generator/generate-evergreen-config.py"),
         ],
         check=True,
     )
