dcd71bd5e1cc04a42b0909ab7c652aba5261ad96
Kevin Albertson <kevin.albertson@mongodb.com>
2023-06-26 13:23:10 -0400

    add a failing test for CDRIVER-4666

--- src/libmongoc/tests/test-mongoc-topology.c
+++ src/libmongoc/tests/test-mongoc-topology.c
@@ -2573,6 +2573,108 @@ test_hello_ok_pooled (void)
    _test_hello_ok (true);
 }
 
+// initiator_fail is a stream initiator that always fails.
+static mongoc_stream_t *
+initiator_fail (const mongoc_uri_t *uri,
+                const mongoc_host_list_t *host,
+                void *user_data,
+                bson_error_t *error)
+{
+   bson_set_error (error,
+                   MONGOC_ERROR_STREAM,
+                   MONGOC_ERROR_STREAM_CONNECT,
+                   "failing in initiator");
+   printf ("failing in initiator\n");
+   return false;
+}
+
+// Test failure in `mongoc_topology_scanner_node_setup` during retry of scanning
+// a known server. This is a regression test of CDRIVER-4666.
+static void
+test_failure_to_setup_after_retry (void)
+{
+   mock_server_t *server;
+   mongoc_uri_t *uri;
+   mongoc_client_t *client;
+   future_t *future;
+   bson_error_t error;
+
+   server = mock_server_new ();
+   mock_server_run (server);
+   uri = mongoc_uri_copy (mock_server_get_uri (server));
+   client = mongoc_client_new_from_uri_with_error (uri, &error);
+   ASSERT_OR_PRINT (client, error);
+
+   // Override the heartbeatFrequencyMS (default 60 seconds) and
+   // minHeartbeatFrequencyMS (default 500ms) to speed up the test.
+   const int64_t overridden_heartbeat_ms = 1;
+   {
+      mc_tpld_modification tdmod = mc_tpld_modify_begin (client->topology);
+      tdmod.new_td->heartbeat_msec = overridden_heartbeat_ms;
+      mc_tpld_modify_commit (tdmod);
+      client->topology->min_heartbeat_frequency_msec = overridden_heartbeat_ms;
+   }
+
+   future = future_client_command_simple (
+      client, "test", tmp_bson ("{'ping': 1}"), NULL, NULL, &error);
+
+   // The first command starts the first topology scan.
+   // Expect legacy hello with handshake.
+   {
+      request_t *request = mock_server_receives_legacy_hello (server, "{}");
+      char *reply = bson_strdup_printf ("{'ok': 1,"
+                                        " 'minWireVersion': %d,"
+                                        " 'maxWireVersion': %d }",
+                                        WIRE_VERSION_MIN,
+                                        WIRE_VERSION_MAX);
+
+      reply_to_request_simple (request, reply);
+      bson_free (reply);
+      request_destroy (request);
+   }
+
+   // Expect "ping" command.
+   {
+      request_t *request = mock_server_receives_msg (
+         server, MONGOC_MSG_NONE, tmp_bson ("{'ping': 1}"));
+      reply_to_request_with_ok_and_destroy (request);
+      ASSERT_OR_PRINT (future_get_bool (future), error);
+      future_destroy (future);
+   }
+
+   // Wait until ready for next topology scan.
+   _mongoc_usleep (overridden_heartbeat_ms * 1000);
+
+   // Send another command.
+   future = future_client_command_simple (
+      client, "test", tmp_bson ("{'ping': 1}"), NULL, NULL, &error);
+
+   // Expect legacy hello with handshake.
+   {
+      request_t *request = mock_server_receives_legacy_hello (server, "{}");
+      // Set the initiator to fail.
+      mongoc_client_set_stream_initiator (client, initiator_fail, NULL);
+      // A network error on a previously known server triggers the retry.
+      reply_to_request_with_hang_up (request);
+      request_destroy (request);
+   }
+
+   // The initiator fails in `mongoc_topology_scanner_node_setup`. Causes a
+   // deadlock similar to that observed in CDRIVER-4666.
+   // Test fails on macOS due to deadlock with "future_get_bool timed out".
+
+   ASSERT (!future_get_bool (future));
+   future_destroy (future);
+   ASSERT_ERROR_CONTAINS (error,
+                          MONGOC_ERROR_SERVER_SELECTION,
+                          MONGOC_ERROR_SERVER_SELECTION_FAILURE,
+                          "No suitable servers found");
+
+   mongoc_client_destroy (client);
+   mongoc_uri_destroy (uri);
+   mock_server_destroy (server);
+}
+
 void
 test_topology_install (TestSuite *suite)
 {
@@ -2737,4 +2839,7 @@ test_topology_install (TestSuite *suite)
       suite, "/Topology/hello_ok/single", test_hello_ok_single);
    TestSuite_AddMockServerTest (
       suite, "/Topology/hello_ok/pooled", test_hello_ok_pooled);
+   TestSuite_AddMockServerTest (suite,
+                                "/Topology/failure_to_setup_after_retry",
+                                test_failure_to_setup_after_retry);
 }
