f19cae63e6e0aa99b9f3777a3fa5657fa8948f79
vector-of-bool <vectorofbool@gmail.com>
2023-06-26 15:23:21 -0600

    Define a mongo::detail::c_platform support library
    mongo::detail::c_platform is intended to hold usage requirements common to all
    libraries in the project. This will replace usage of directory-level
    compile/link options, which do not propagate on the generated libraries when
    they are installed.
    
    This allows one to export/install libmongoc libraries that expose
    additional usage requirements, such as linking instrumentation runtimes
    (coverage, sanitizers, etc.)
    
    Also: Sanitizers.cmake and ENABLE_COVERAGE now attach their options
    to the platform-support library. This allows installed libmongoc with
    sanitizers enabled to "just work," as downstream users will now receive
    link options for coverage and sanitizers as usage requirements from the
    libbson and libmongoc libraries.
    
    Also: Enable threading on c_platform rather than linking. Fix the bson
    and mongoc packages to pull Threads::Threads as a dependency
    
    This also affects generated pkg-config files automatically.

---
+++ build/cmake/MongoPlatform.cmake
@@ -0,0 +1,49 @@
+#[[
+
+Defines a target mongo::detail::c_platform (alias of mongo-platform), which
+exposes system-level supporting compile and link usage requirements. All targets
+should link to this target with level PUBLIC.
+
+Use mongo_add_platform_compile_options and mongo_add_platform_link_options to
+add usage requirements to this library.
+
+The mongo::detail::c_platform library is installed and exported with the
+bson-targets export set as an implementation detail. It is installed with this
+export set so that it is available to both libbson and libmongoc (attempting to
+install this target in both bson-targets and mongoc-targets export sets would
+lead to duplicate definitions of mongo::detail::c_platform for downstream users).
+
+]]
+
+add_library(_mongo-platform INTERFACE)
+add_library(mongo::detail::c_platform ALIAS _mongo-platform)
+set_property(TARGET _mongo-platform PROPERTY EXPORT_NAME detail::c_platform)
+install(TARGETS _mongo-platform EXPORT bson-targets)
+
+
+#[[
+Define additional platform-support compile options
+
+These options are added to the mongo::detail::c_platform INTERFACE library.
+]]
+function (mongo_add_platform_compile_options)
+    list(APPEND CMAKE_MESSAGE_CONTEXT ${CMAKE_CURRENT_FUNCTION})
+    message(DEBUG "Add platform-support compilation options: ${ARGN}")
+    target_compile_options(_mongo-platform INTERFACE ${ARGN})
+endfunction ()
+
+#[[
+Define additional platform-support link options.
+
+These options are added to the mongo::detail::c_platform INTERFACE library.
+]]
+function(mongo_add_platform_link_options)
+    list(APPEND CMAKE_MESSAGE_CONTEXT ${CMAKE_CURRENT_FUNCTION})
+    message(DEBUG "Add platform-support runtime linking options: ${ARGN}")
+    target_link_options(_mongo-platform INTERFACE ${ARGN})
+endfunction()
+
+# Enable multi-threading:
+set(THREADS_PREFER_PTHREAD_FLAG TRUE)
+find_package(Threads REQUIRED)
+target_link_libraries(_mongo-platform INTERFACE Threads::Threads)
