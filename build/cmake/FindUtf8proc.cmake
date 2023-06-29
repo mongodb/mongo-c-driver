d302e23ef38305b56e4a004c7dae2fa32ba9e873
Josh Siegel <josh.siegel@mongodb.com>
2023-06-29 14:22:44 +0000

    change names to match pattern and satisfy cmake

--- build/cmake/FindUtf8proc.cmake
+++ build/cmake/FindUtf8proc.cmake
@@ -1,18 +1,18 @@
 find_package(PkgConfig)
-pkg_check_modules(PC_utf8proc QUIET utf8proc)
+pkg_check_modules(PC_Utf8proc QUIET utf8proc)
 
-find_path(utf8proc_INCLUDE_DIR
+find_path(Utf8proc_INCLUDE_DIR
     NAMES utf8proc.h
     PATHS ${PC_utf8proc_INCLUDE_DIRS}
     PATH_SUFFIXES utf8proc-2.8.0
 )
 
-set(utf8proc_VERSION ${PC_utf8proc_VERSION})
+set(Utf8proc_VERSION ${PC_utf8proc_VERSION})
 
-mark_as_advanced(utf8proc_FOUND utf8proc_INCLUDE_DIR utf8proc_VERSION)
+mark_as_advanced(Utf8proc_FOUND Utf8proc_INCLUDE_DIR Utf8proc_VERSION)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(utf8proc
-    REQUIRED_VARS utf8proc_INCLUDE_DIR
-    VERSION_VAR utf8proc_VERSION
+find_package_handle_standard_args(Utf8proc
+    REQUIRED_VARS Utf8proc_INCLUDE_DIR
+    VERSION_VAR Utf8proc_VERSION
 )
