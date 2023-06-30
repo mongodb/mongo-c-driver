79677a259a5f2602837a30c38bda6a46017bb073
Josh Siegel <josh.siegel@mongodb.com>
2023-06-30 18:22:14 +0000

    revert bundling steps

--- build/cmake/FindUtf8Proc.cmake
+++ build/cmake/FindUtf8Proc.cmake
@@ -1,11 +1,20 @@
 if(USE_BUNDLED_UTF8PROC STREQUAL "TRUE")
-  add_subdirectory("../utf8proc-2.8.0" ".../cmake-build")
+  message (STATUS "Enabling utf8proc (bundled)")
+  add_library (utf8proc_obj OBJECT "${UTF8PROC_SOURCES}")
+  set_property (TARGET utf8proc_obj PROPERTY POSITION_INDEPENDENT_CODE TRUE)
+  set (SOURCES ${SOURCES} $<TARGET_OBJECTS:utf8proc_obj>)
+  target_compile_definitions (utf8proc_obj PUBLIC UTF8PROC_STATIC)
 elseif (USE_BUNDLED_UTF8PROC STREQUAL "FALSE")
+ message (STATUS "Enabling utf8proc")
   find_package(PkgConfig)
   pkg_check_modules(PC_UTF8PROC REQUIRED libutf8proc)
   add_library(utf8proc STATIC IMPORTED)
-  message("${PC_UTF8PROC_LINK_LIBRARIES} HERE")
-  set_target_properties(utf8proc PROPERTIES COMPILE_OPTIONS ${PC_UTF8PROC_STATIC_CFLAGS} IMPORTED_LOCATION ${PC_UTF8PROC_LINK_LIBRARIES})
+  set_target_properties(utf8proc 
+                        PROPERTIES
+                        COMPILE_OPTIONS ${PC_UTF8PROC_STATIC_CFLAGS} 
+                        LINK_LIBRARIES ${PC_UTF8PROC_LINK_LIBRARIES} 
+                        INCLUDE_DIRECTORIES ${PC_UTF8PROC_STATIC_LIBRARY_DIRS} 
+                        IMPORTED_LOCATION ${PC_UTF8PROC_LINK_LIBRARIES})
 else ()
    message (FATAL_ERROR
             "USE_BUNDLED_UTF8PROC must be set to either TRUE or FALSE"
