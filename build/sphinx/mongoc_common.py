6d3f58616646c40d2ac6072fdef913bb31727ecb
vector-of-bool <vectorofbool@gmail.com>
2023-07-06 11:54:46 -0600

    Fix parallel Sphinx builds when building man output

--- build/sphinx/mongoc_common.py
+++ build/sphinx/mongoc_common.py
@@ -1,12 +1,18 @@
 import os
-from docutils import nodes
-try:
-    from sphinx.util.compat import Directive
-except ImportError:
-    from docutils.parsers.rst import Directive
+import re
+from pathlib import Path
+from typing import Any, Iterable, Sequence
 
-needs_sphinx = '1.6'
-author = 'MongoDB, Inc'
+from docutils import nodes
+from docutils.nodes import Node, document
+
+from sphinx.application import Sphinx
+from sphinx.config import Config
+from sphinx.project import Project
+from sphinx.util.docutils import SphinxDirective
+
+needs_sphinx = "1.6"
+author = "MongoDB, Inc"
 
 # -- Options for HTML output ----------------------------------------------
 
@@ -17,11 +23,43 @@ html_show_sourcelink = False
 # This will degrade the Javascript quicksearch if we ever use it.
 html_copy_source = False
 
+
+def _file_man_page_name(fpath: Path) -> str | None:
+    "Given an rST file input, find the :man_page: frontmatter value, if present"
+    lines = fpath.read_text().splitlines()
+    for line in lines:
+        mat = re.match(r":man_page:\s+(.+)", line)
+        if not mat:
+            continue
+        return mat[1]
+
+
+def _collect_man(app: Sphinx, config: Config) -> None:
+    "Populate the man_pages value from the given source directory input"
+    # Note: 'app' is partially-formed, as this is called from the Sphinx.__init__
+    docdir = Path(app.srcdir)
+    proj = Project(app.srcdir, config.source_suffix)
+    # Find everything:
+    children = docdir.rglob("*")
+    # Find only regular files:
+    files = filter(Path.is_file, children)
+    # Find files that have a .rst extension:
+    rst_files = (f for f in files if f.suffix == ".rst")
+    # Pair each file with its :man_page: frontmatter, if present:
+    with_man_name = ((f, _file_man_page_name(f)) for f in rst_files)
+    # Filter out pages that do not have a :man_page: item:s
+    pairs: Iterable[tuple[Path, str]] = ((f, m) for f, m in with_man_name if m)
+    # Populate the man_pages:
+    for filepath, man_name in pairs:
+        docname = proj.path2doc(str(filepath))
+        assert docname, filepath
+        man_pages.append((docname, man_name, "", [author], 3))
+
+
 # -- Options for manual page output ---------------------------------------
 
-# HACK: Just trick Sphinx's ManualPageBuilder into thinking there are pages
-# configured - we'll do it dynamically in process_nodes.
-man_pages = [True]
+# NOTE: This starts empty, but we populate it during "config-inited" in _collect_man() (see above)
+man_pages: list[tuple[str, str, str, list[str], int]] = []
 
 # If true, show URL addresses after external links.
 #
@@ -29,24 +67,15 @@ man_pages = [True]
 
 # -- Sphinx customization ---------------------------------------
 
-from docutils.nodes import title
 
-
-# To publish HTML docs at GitHub Pages, create .nojekyll file. In Sphinx 1.4 we
-# could use the githubpages extension, but old Ubuntu still has Sphinx 1.3.
-def create_nojekyll(app, env):
-    if app.builder.format == 'html':
-        path = os.path.join(app.builder.outdir, '.nojekyll')
-        with open(path, 'wt') as f:
-            f.write('foo')
-
-
-def add_ga_javascript(app, pagename, templatename, context, doctree):
+def add_ga_javascript(app: Sphinx, pagename: str, templatename: str, context: dict[str, Any], doctree: document):
     if not app.env.config.analytics:
         return
 
     # Add google analytics and NPS survey.
-    context['metatags'] = context.get('metatags', '') + """
+    context["metatags"] = (
+        context.get("metatags", "")
+        + """
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-7301842-14"></script>
 <script>
@@ -63,75 +92,52 @@ def add_ga_javascript(app, pagename, templatename, context, doctree):
   delighted.survey();
 </script>
 """
+    )
 
 
-def process_nodes(app, doctree):
-    if man_pages == [True]:
-        man_pages.pop()
+class VersionList(SphinxDirective):
+    """Custom directive to generate the version list from an environment variable"""
 
-    env = app.env
-    metadata = env.metadata[env.docname]
-
-    # A page like installing.rst sets its name with ":man_page: bson_installing"
-    page_name = metadata.get('man_page')
-    if not page_name:
-        print('Not creating man page for %s' % env.docname)
-        return
-
-    page_title = find_node(doctree, title)
-
-    man_pages.append((env.docname, page_name, page_title.astext(), [author], 3))
-
-
-def find_node(doctree, klass):
-    matches = doctree.traverse(lambda node: isinstance(node, klass))
-    if not matches:
-        raise IndexError("No %s in %s" % (klass, doctree))
-
-    return list(matches)[0]
-
-class VersionList(Directive):
-    """ Custom directive to generate the version list from an environment variable """
     option_spec = {}
     has_content = True
-    def run(self):        
-        if self.content[0] != 'libmongoc' and self.content[0] != 'libbson':
-            print ('versionlist must be libmongoc or libbson')
+
+    def run(self) -> Sequence[Node]:
+        if self.content[0] != "libmongoc" and self.content[0] != "libbson":
+            print("versionlist must be libmongoc or libbson")
             return []
 
         libname = self.content[0]
-        env_name = libname.upper() + '_VERSION_LIST'
+        env_name = libname.upper() + "_VERSION_LIST"
         if env_name not in os.environ:
-            print (env_name + ' not set, not generating version list')
+            print(env_name + " not set, not generating version list")
             return []
-        
-        versions = os.environ[env_name].split(',')
 
-        header = nodes.paragraph('','')
-        p = nodes.paragraph('', '')
-        uri = 'https://www.mongoc.org/%s/%s/index.html' % (libname, versions[0])
-        p += nodes.reference('', 'Latest Release (%s)' % versions[0], internal=False, refuri=uri)
+        versions = os.environ[env_name].split(",")
+
+        header = nodes.paragraph("", "")
+        p = nodes.paragraph("", "")
+        uri = "https://www.mongoc.org/%s/%s/index.html" % (libname, versions[0])
+        p += nodes.reference("", "Latest Release (%s)" % versions[0], internal=False, refuri=uri)
         header += p
-        p = nodes.paragraph('', '')
-        uri = 'https://s3.amazonaws.com/mciuploads/mongo-c-driver/docs/%s/latest/index.html' % (libname)
-        p += nodes.reference('', 'Current Development (master)', internal=False, refuri=uri)
+        p = nodes.paragraph("", "")
+        uri = "https://s3.amazonaws.com/mciuploads/mongo-c-driver/docs/%s/latest/index.html" % (libname)
+        p += nodes.reference("", "Current Development (master)", internal=False, refuri=uri)
         header += p
 
         blist = nodes.bullet_list()
         for v in versions:
             item = nodes.list_item()
-            p = nodes.paragraph('', '')
-            uri = 'https://www.mongoc.org/%s/%s/index.html' % (libname, v)
-            p += nodes.reference('', v, internal=False, refuri=uri)
+            p = nodes.paragraph("", "")
+            uri = "https://www.mongoc.org/%s/%s/index.html" % (libname, v)
+            p += nodes.reference("", v, internal=False, refuri=uri)
             item += p
             blist += item
         return [header, blist]
 
 
-def mongoc_common_setup(app):
-    app.connect('doctree-read', process_nodes)
-    app.connect('env-updated', create_nojekyll)
-    app.connect('html-page-context', add_ga_javascript)
+def mongoc_common_setup(app: Sphinx):
+    app.connect("config-inited", _collect_man)
+    app.connect("html-page-context", add_ga_javascript)
     # Run sphinx-build -D analytics=1 to enable Google Analytics.
-    app.add_config_value('analytics', False, 'html')
-    app.add_directive ('versionlist', VersionList)
+    app.add_config_value("analytics", False, "html")
+    app.add_directive("versionlist", VersionList)
