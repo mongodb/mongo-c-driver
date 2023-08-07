e8b7c917c12625fc31a4c137e6c8f553ce1765b0
Kevin Albertson <kevin.albertson@mongodb.com>
2023-08-07 16:19:46 -0400

    libbson and libmongoc 1.24.3

--- libmongoc/_static/documentation_options.js
+++ libmongoc/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.24.2',
-    LANGUAGE: 'None',
+    VERSION: '1.24.3',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: false,
