3f21b5d7fb259944381a0d68479b38f722d47a2a
vector-of-bool <vectorofbool@gmail.com>
2023-08-17 01:08:57 -0600

    Install shared/static separately:
    Each type can be disabled individually, and even packaged individually.
    Installs two export sets that are lazily included based on their presence
    at import-time.

--- src/libbson/src/bson-config.cmake
+++ src/libbson/src/bson-config.cmake
@@ -1,4 +1,15 @@
 include(CMakeFindDependencyMacro)
 find_dependency(Threads)  # Required for Threads::Threads
 
+# Import common targets first:
 include("${CMAKE_CURRENT_LIST_DIR}/bson-targets.cmake")
+
+# Now import the targets of each link-kind that's available. Only the targets of
+# libbson libraries that were actually installed alongside this file will be
+# imported.
+foreach(set IN ITEMS bson_shared bson_static)
+    set(inc "${CMAKE_CURRENT_LIST_DIR}/${set}-targets.cmake")
+    if(EXISTS "${inc}")
+        include("${inc}")
+    endif()
+endforeach()
