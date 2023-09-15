0c7694670c339aaf82ddaeee999299daf420ccd4
vector-of-bool <vectorofbool@gmail.com>
2023-09-15 14:56:29 -0600

    Revert from using pkg-config, and find the files ourselves

--- build/cmake/FindSASL2.cmake
+++ build/cmake/FindSASL2.cmake
@@ -1,74 +1,84 @@
 #[[
 
-Searches for a Cyrus "libsasl2" library available on the system using
-pkg-config. The libsasl2.pc file must be available for pkg-config!
+Searches for a Cyrus "libsasl2" library available on the system.
 
 Upon success, Defines an imported target `SASL2::SASL2` that can be linked into
 other targts.
 
 ]]
 
-if(SASL2_FIND_COMPONENTS)
-    message(FATAL_ERROR "This find_package(SASL2) does not support package components (Got “${SASL2_FIND_COMPONENTS}”)")
-endif()
-set(_modspec "libsasl2")
-if(DEFINED SASL2_FIND_VERSION)
-    set(_modspec "libsasl2>=${SASL2_FIND_VERSION}")
-endif()
-
-if(TARGET SASL2::SASL2)
-    message(DEBUG "SASL2 ${SASL2_VERSION} was already found")
-    # There is already a libsasl2 imported
-    if(NOT DEFINED SASL2_FIND_VERISON OR SASL2_FIND_VERSION VERSION_LESS_EQUAL SASL2_VERSION)
-        # Okay: We are satisfied by the version that has already been found
-        set(SASL2_FOUND TRUE)
-        return()
-    endif()
-    # Eh: The importer has requested a version of libsasl2 that is greater than the
-    # version of libsasl2 that we previously imported. The pkg-config invocation
-    # will likely fail, but we'll let it say that for itself.
-    message(DEBUG "Need to find SASL2 again: Version requirement increased ${SASL2_FIND_VERSION} > ${SASL2_VERSION}")
-endif()
+include(FindPackageHandleStandardArgs)
 
 # Upon early return, tell the caller that we don't have it:
 set(SASL2_FOUND FALSE)
 
-# Forward the QUIET+REQUIRED args to pkg-config
-set(_required)
-set(_quiet)
-if(SASL2_FIND_QUIETLY)
-    set(_quiet QUIET)
-endif()
-if(SASL2_FIND_REQUIRED)
-    set(_required REQUIRED)
+if(SASL2_FIND_COMPONENTS)
+    message(FATAL_ERROR "This find_package(SASL2) does not support package components (Got “${SASL2_FIND_COMPONENTS}”)")
 endif()
 
-# We use pkg-config to find libsasl2.
-find_package(PkgConfig ${_required} ${_quiet})
-if(NOT PkgConfig_FOUND)
-    # The find_package(PkgConfig) will have generated errors for us if we were REQUIRED,
-    # so we can just return without setting any message
-    set(SASL2_NOT_FOUND_MESSAGE [[No pkg-config executable was found]])
+# Search for the proper qualified path <sasl/sasl.h>, not sasl.h
+find_path(
+    SASL2_INCLUDE_DIR "sasl/sasl.h"
+    DOC "Header include-directory for Cyrus libsasl2"
+    PATHS ${SASL2_PREFIX} ${SASL2_ROOT_DIR}
+)
+
+# Use the header path as a hint for the library path:
+unset(_hint)
+if(SASL2_INCLUDE_DIR)
+    get_filename_component(_hint "${SASL2_INCLUDE_DIR}" DIRECTORY)
+endif()
+
+# The library filename is libsasl2.so, libsasl.dylib, etc.
+find_library(
+    SASL2_LIBRARY sasl2
+    DOC "Library file for Cyrus libsasl2"
+    HINTS ${_hint}
+    PATHS ${SASL2_PREFIX} ${SASL2_ROOT_DIR}
+)
+
+if(SASL2_INCLUDE_DIR)
+    message(DEBUG "Found SASL2 include-dir: ${SASL2_INCLUDE_DIR}")
+    # Extract the library version from the sasl.h header file:
+    file(READ "${SASL2_INCLUDE_DIR}/sasl/sasl.h" _sasl_h)
+    # It is defined via three macro definitions:
+    string(CONCAT _version_regex
+        "define[ \t]+SASL_VERSION_MAJOR[ \t]+([0-9]+).+"
+        "define[ \t]+SASL_VERSION_MINOR[ \t]+([0-9]+).+"
+        "define[ \t]+SASL_VERSION_STEP[ \t]+([0-9]+)"
+        )
+    if(NOT _sasl_h MATCHES "${_version_regex}")
+        # Very strange...
+        set(SASL2_NOT_FOUND_MESSAGE [[A sasl/sasl.h file was found, but we could not extract version information]])
+    else()
+        set(SASL2_VERSION "${CMAKE_MATCH_1}.${CMAKE_MATCH_2}.${CMAKE_MATCH_3}")
+        message(DEBUG "Found libsasl2 version from sasl.h: ${SASL2_VERSION}")
+    endif()
+endif()
+
+find_package_handle_standard_args(SASL2
+    REQUIRED_VARS SASL2_VERSION SASL2_INCLUDE_DIR SASL2_LIBRARY
+    VERSION_VAR SASL2_VERSION
+)
+
+if(NOT SASL2_FOUND)
     return()
 endif()
 
-# Now ask pkg-config to find libsasl2 for us
-message(DEBUG "Using pkg-config to search for module “${_modspec}”")
-pkg_check_modules(libsasl2 ${_required} ${_quiet} IMPORTED_TARGET "${_modspec}" GLOBAL)
-# Note: pkg_check_modules() does support static libs, but we don't use them here,
-# and static libsasl2 is uncommon.
-if(NOT libsasl2_FOUND)
-    # The pkg_check_modules() will have issued the error for us if we were REQUIRED.
-    set(SASL2_NOT_FOUND_MESSAGE [[pkg-config failed to find a libsasl2 (Cyrus) dynamic library package]])
-    return()
+message(DEBUG "Found SASL2 library: ${SASL2_LIBRARY}")
+
+# Generate an imported target based on the paths that we found.
+if(NOT TARGET SASL2::SASL2)
+    # (Guard against double-import)
+    add_library(SASL2::SASL2 IMPORTED UNKNOWN GLOBAL)
 endif()
+set_target_properties(SASL2::SASL2 PROPERTIES
+    IMPORTED_LOCATION "${SASL2_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SASL2_INCLUDE_DIR}"
+    INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${SASL2_INCLUDE_DIR}"
+    INTERFACE_LINK_LIBRARIES ""  # Clear this property in case of double-import
+    VERSION "${SASL2_VERSION}"
+)
 
-# Instead of using the PkgConfig-generated target directly, add a level of
-# indirection so that importers do not encode the dependency on the PkgConfig
-# behavior. Exported users will have a link to SASL2::SASL2, but not to the
-# PkgConfig target.
-set(SASL2_FOUND TRUE)
-add_library(SASL2::SASL2 IMPORTED INTERFACE GLOBAL)
-target_link_libraries(SASL2::SASL2 INTERFACE PkgConfig::libsasl2)
-
-set(SASL2_VERSION "${libsasl2_VERSION}" CACHE INTERNAL "")
+# libsasl2 requires dlopen():
+set_property(TARGET SASL2::SASL2 APPEND PROPERTY INTERFACE_LINK_LIBRARIES ${CMAKE_DL_LIBS})
--- src/libmongoc/src/mongoc-config.cmake
+++ src/libmongoc/src/mongoc-config.cmake
@@ -22,44 +22,11 @@ endif()
 
 # Find dependencies for SASL
 set(_sasl_backend [[@SASL_BACKEND@]])
-if(NOT TARGET _mongoc::detail::sasl AND _sasl_backend)
-  add_library(_mongoc::detail::sasl IMPORTED INTERFACE GLOBAL)
-endif()
-set(_sasl_sspi_libraries [[@SASL_SSPI_LIBRARIES@]])
 if(_sasl_backend STREQUAL "Cyrus")
-  # We need libsasl2. At build-time, we use FindSASL2.cmake, but that will probably
-  # not be available. Instead, replicate the minimum logic to define our own
-  # SASL2::SASL2 IMPORTED target to satisfy the dependency
-  find_package(PkgConfig QUIET)
-  if(PkgConfig_FOUND)
-    pkg_check_modules(_MONGOC_LIBSASL2 ${_quiet} IMPORTED_TARGET GLOBAL libsasl2)
-  endif()
-  if(_MONGOC_LIBSASL2_FOUND)
-    target_link_libraries(_mongoc::detail::sasl INTERFACE PkgConfig::_MONGOC_LIBSASL2)
-  else()
-    # We don't have the full libsasl2 dev package, but we were compiled against one.
-    # Do the next best thing: Find the shared library file and use that for our imported target.
-    find_library(
-      _MONGOC_LIBSASL2_DYNAMIC_LIBRARY
-      NAMES
-        sasl2
-        ${CMAKE_SHARED_LIBRARY_PREFIX}sasl2${CMAKE_SHARED_LIBRARY_SUFFIX}.3
-        ${CMAKE_SHARED_LIBRARY_PREFIX}sasl2${CMAKE_SHARED_LIBRARY_SUFFIX}.2
-        ${CMAKE_SHARED_LIBRARY_PREFIX}sasl2${CMAKE_SHARED_LIBRARY_SUFFIX}.1
-        ${CMAKE_SHARED_LIBRARY_PREFIX}sasl2${CMAKE_SHARED_LIBRARY_SUFFIX}
-      DOC "Path to a Cyrus libsasl2 library for linking against with libmongoc"
-      )
-    mark_as_advanced(_MONGOC_LIBSASL2_DYNAMIC_LIBRARY)
-    if(_MONGOC_LIBSASL2_DYNAMIC_LIBRARY)
-      target_link_libraries(_mongoc::detail::sasl INTERFACE "${_MONGOC_LIBSASL2_DYNAMIC_LIBRARY}")
-    else()
-      #[[
-        Worst case: No libsasl2.pc, no libsasl2.so, and no suffixed version.
-        We're okay in most cases though, because the dynamic library will contain
-        a runtime link dep on the sasl2 library.
-      ]]
-    endif()
-  endif()
-elseif(_sasl_backend STREQUAL "SSPI")
-  target_link_libraries(_mongoc::detail::sasl INTERFACE ${_sasl_sspi_libraries})
+  # We need libsasl2. The find-module should be installed within this package.
+  # temporarily place it on the module search path:
+  set(_prev_path "${CMAKE_MODULE_PATH}")
+  list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/3rdParty")
+  find_dependency(SASL2 2.0)
+  set(CMAKE_MODULE_PATH "${_prev_path}")
 endif()
