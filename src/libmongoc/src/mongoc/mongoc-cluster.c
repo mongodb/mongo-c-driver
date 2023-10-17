3fec27d07e9903edf7fdd4b12849e7afa1de9fe6
Kevin Albertson <kevin.albertson@mongodb.com>
2023-10-17 15:19:56 -0400

    CDRIVER-4718 store `recoveryToken` for Load Balanced topology (#1417)
    * add regression test
    
    * format mongoc-cluster.c
    
    * store recoveryToken for loadBalanced topology
    
    * make `_in_sharded_txn` static
    
    * use `static`
    
    Co-authored-by: Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
    
    * removing redundant calls to `_mongoc_topology_get_type`
    
    Co-authored-by: Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>

--- src/libmongoc/src/mongoc/mongoc-cluster.c
+++ src/libmongoc/src/mongoc/mongoc-cluster.c
@@ -492,12 +492,30 @@ done:
    RETURN (ret);
 }
 
-bool
+static bool
 _in_sharded_txn (const mongoc_client_session_t *session)
 {
    return session && _mongoc_client_session_in_txn_or_ending (session) &&
           _mongoc_topology_get_type (session->client->topology) ==
              MONGOC_TOPOLOGY_SHARDED;
+}
+
+static bool
+_in_sharded_or_loadbalanced_txn (const mongoc_client_session_t *session)
+{
+   if (!session) {
+      return false;
+   }
+
+   if (!_mongoc_client_session_in_txn_or_ending (session)) {
+      return false;
+   }
+
+   mongoc_topology_description_type_t type =
+      _mongoc_topology_get_type (session->client->topology);
+
+   return (type == MONGOC_TOPOLOGY_SHARDED) ||
+          (type == MONGOC_TOPOLOGY_LOAD_BALANCED);
 }
 
 static void
@@ -654,7 +672,7 @@ mongoc_cluster_run_command_monitored (mongoc_cluster_t *cluster,
 
    _handle_txn_error_labels (retval, error, cmd, reply);
 
-   if (retval && _in_sharded_txn (cmd->session) &&
+   if (retval && _in_sharded_or_loadbalanced_txn (cmd->session) &&
        bson_iter_init_find (&iter, reply, "recoveryToken")) {
       bson_destroy (cmd->session->recovery_token);
       if (BSON_ITER_HOLDS_DOCUMENT (&iter)) {
