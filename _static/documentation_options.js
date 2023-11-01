55fc54b2a9288dc45d5b1095fb47fb2fb19059be
Kevin Albertson <kevin.albertson@mongodb.com>
2023-11-01 10:27:22 -0400

    libbson and libmongoc 1.25.0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,9 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.24.4',
+    VERSION: '1.25.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
     HAS_SOURCE: false,
-    SOURCELINK_SUFFIX: '.txt'
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
