43644f8b30c77a018712a2a29e2dba00799ae643
Jeremy Mikola <jmikola@gmail.com>
2024-01-16 15:06:02 -0500

    CDRIVER-4754 add const modifier to _mongoc_write_error_handle_labels param (#1515)

--- src/libmongoc/src/mongoc/mongoc-error-private.h
+++ src/libmongoc/src/mongoc/mongoc-error-private.h
@@ -74,7 +74,7 @@ void
 _mongoc_write_error_handle_labels (bool cmd_ret,
                                    const bson_error_t *cmd_err,
                                    bson_t *reply,
-                                   mongoc_server_description_t *sd);
+                                   const mongoc_server_description_t *sd);
 
 bool
 _mongoc_error_is_shutdown (bson_error_t *error);
