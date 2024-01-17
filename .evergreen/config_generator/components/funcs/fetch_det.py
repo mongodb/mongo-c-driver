bb84366322a12f2573548f5db3cca43ebe77dd8c
Kevin Albertson <kevin.albertson@mongodb.com>
2024-01-17 11:49:33 -0500

    CDRIVER-4512 do not use `execdir`
    Resolves insecure error produced by `find`

--- .evergreen/config_generator/components/funcs/fetch_det.py
+++ .evergreen/config_generator/components/funcs/fetch_det.py
@@ -21,7 +21,11 @@ class FetchDET(Function):
         bash_exec(
             command_type=EvgCommandType.SETUP,
             working_dir="drivers-evergreen-tools",
-            script='find .evergreen -type f -name "*.sh" -execdir chmod +rx "{}" \;',
+            script='''\
+                for file in $(find .evergreen -type f -name "*.sh"); do
+                    chmod +rx "$file" || exit
+                done
+            '''
         ),
 
         # python is used frequently enough by many tasks that it is worth
