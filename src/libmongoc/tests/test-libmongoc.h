7121c0773d618fedad2ea78541f8aef6921404d6
Kevin Albertson <kevin.albertson@mongodb.com>
2024-03-12 10:45:21 -0400

    add wire version checks for 7.2, 7.3, and 8.0

--- src/libmongoc/tests/test-libmongoc.h
+++ src/libmongoc/tests/test-libmongoc.h
@@ -209,6 +209,12 @@ WIRE_VERSION_CHECK_DECLS (19)
 WIRE_VERSION_CHECK_DECLS (21)
 /* wire version 22 begins with the 7.1 release. */
 WIRE_VERSION_CHECK_DECLS (22)
+/* wire version 23 begins with the 7.2 release. */
+WIRE_VERSION_CHECK_DECLS (23)
+/* wire version 24 begins with the 7.3 release. */
+WIRE_VERSION_CHECK_DECLS (24)
+/* wire version 25 begins with the 8.0 release. */
+WIRE_VERSION_CHECK_DECLS (25)
 
 #undef WIRE_VERSION_CHECK_DECLS
 
