28ac7e72259cbaad2115c430913de52bd12cc1fa
Kevin Albertson <kevin.albertson@mongodb.com>
2024-04-03 16:04:04 -0400

    disable SSL, and revise comment

--- .evergreen/scripts/link-sample-program-mingw.cmd
+++ .evergreen/scripts/link-sample-program-mingw.cmd
@@ -1,8 +1,8 @@
 echo on
 echo
 
-rem Use ENABLE_SSL=WINDOWS to link to Secure Channel. CMake is unable to locate OpenSSL installed on Windows hosts due to https://gitlab.kitware.com/cmake/cmake/-/issues/25859
-set CMAKE_FLAGS=-DENABLE_SSL=WINDOWS -DENABLE_SASL=CYRUS
+rem Use DENABLE_SSL=OFF. Windows hosts do not have a MinGW ABI compatible OpenSSL install.
+set CMAKE_FLAGS=-DENABLE_SSL=OFF -DENABLE_SASL=CYRUS
 set TAR=C:\cygwin\bin\tar
 set CMAKE_MAKE_PROGRAM=C:\mingw-w64\x86_64-4.9.1-posix-seh-rt_v3-rev1\mingw64\bin\mingw32-make.exe
 set CC=C:\mingw-w64\x86_64-4.9.1-posix-seh-rt_v3-rev1\mingw64\bin\gcc.exe
