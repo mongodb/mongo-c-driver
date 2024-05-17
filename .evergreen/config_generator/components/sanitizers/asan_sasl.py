eb4d429f5afb2d051d33c0dba8b71331b4b710c6
Kevin Albertson <kevin.albertson@mongodb.com>
2024-05-17 08:25:07 -0400

    update config generator scripts
    copy `7.0` tasks to `8.0`

--- .evergreen/config_generator/components/sanitizers/asan_sasl.py
+++ .evergreen/config_generator/components/sanitizers/asan_sasl.py
@@ -20,7 +20,7 @@ TEST_MATRIX = [
     ('ubuntu1804', 'clang', None, 'cyrus', ['auth'], ['server', 'replica', 'sharded'], [       '4.0', '4.2', '4.4', '5.0', '6.0']),
 
     # Test 7.0+ with Ubuntu 20.04+ since MongoDB 7.0 no longer ships binaries for Ubuntu 18.04.
-    ('ubuntu2004', 'clang', None, 'cyrus', ['auth'], ['server', 'replica', 'sharded'], ['7.0', 'latest']),
+    ('ubuntu2004', 'clang', None, 'cyrus', ['auth'], ['server', 'replica', 'sharded'], ['7.0', '8.0', 'latest']),
 ]
 # fmt: on
 # pylint: enable=line-too-long
