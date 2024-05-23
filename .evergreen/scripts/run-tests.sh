dda7bb960b90479abae161f94e430e4940ef52df
Kyle Kloberdanz <kyle.kloberdanz@mongodb.com>
2024-05-23 14:03:17 -0500

    try this

--- .evergreen/scripts/run-tests.sh
+++ .evergreen/scripts/run-tests.sh
@@ -248,6 +248,7 @@ if [[ "${OIDC}" == "oidc" ]]; then
   test_args+=("-l" "/unified/mongodb-oidc-no-retry")
   echo "TESTING OIDC"
   ps aux | grep mongod
+  ps aux
 fi
 
 if [[ ! "${test_args[*]}" =~ "-l" ]]; then
