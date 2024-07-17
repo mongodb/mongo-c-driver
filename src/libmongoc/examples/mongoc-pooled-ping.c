c3f96fedf3e150193b340e11aa48f55e1cf038e4
Julia Garland <julia.garland@mongodb.com>
2024-07-17 11:50:02 -0400

    Nicer pooled benchmark

--- src/libmongoc/examples/mongoc-pooled-ping.c
+++ src/libmongoc/examples/mongoc-pooled-ping.c
@@ -1,3 +1,11 @@
+/*
+ * Used as a benchmark for CDRIVER-4656 to test the performance effect of sharing the OpenSSL context among all clients
+ * in a pool.
+ *
+ * TO RUN: % ./cmake-build/src/libmongoc/mongoc-pooled-ping [number of clients to check out]
+ * The integer argument is optional, if not provided 100 clients are created by default.
+ */
+
 #include <mongoc/mongoc.h>
 #include <pthread.h>
 #include <stdio.h>
@@ -24,8 +32,8 @@ worker (void *data)
       r = mongoc_database_command_with_opts (database, &ping, NULL, NULL, NULL, &error);
 
       if (!r) {
-        fprintf (stderr, "Ping failure: %s\n", error.message);
-      } 
+         fprintf (stderr, "Ping failure: %s\n", error.message);
+      }
 
       mongoc_client_pool_push (pool, client);
 
@@ -45,31 +53,44 @@ worker (void *data)
 int
 main (int argc, char *argv[])
 {
-   const char *certificate_path;
-   const char *ca_path;
+   int num_clients = 100;
+
+   if (argc > 1) {
+      num_clients = atoi (argv[1]);
+   }
+
    mongoc_uri_t *uri;
    mongoc_client_pool_t *pool;
-   pthread_t threads[2000];
+   pthread_t threads[num_clients];
    unsigned i;
    void *ret;
 
    pthread_mutex_init (&mutex, NULL);
    mongoc_init ();
 
-   certificate_path = "/Users/julia.garland/Desktop/Code/drivers-evergreen-tools/.evergreen/x509gen/server.pem";
-   ca_path = "/Users/julia.garland/Desktop/Code/drivers-evergreen-tools/.evergreen/x509gen/ca.pem";
-
    uri = mongoc_uri_new ("mongodb://localhost:27017/");
-   mongoc_uri_set_option_as_bool (uri, MONGOC_URI_TLS, true);
 
+#ifdef MONGOC_ENABLE_SSL
+   const char *certificate_path;
+   const char *ca_path;
+
+   // Use built-in test CA and PEM files.
+   certificate_path = "./src/libmongoc/tests/x509gen/client.pem";
+   ca_path = "./src/libmongoc/tests/x509gen/ca.pem";
+
+   mongoc_uri_set_option_as_bool (uri, MONGOC_URI_TLS, true);
    mongoc_uri_set_option_as_utf8 (uri, MONGOC_URI_TLSCERTIFICATEKEYFILE, certificate_path);
    mongoc_uri_set_option_as_utf8 (uri, MONGOC_URI_TLSCAFILE, ca_path);
-   mongoc_uri_set_option_as_int32(uri, MONGOC_URI_MAXPOOLSIZE, 2000);
+#endif
+
+   if (num_clients > 100) {
+      mongoc_uri_set_option_as_int32 (uri, MONGOC_URI_MAXPOOLSIZE, num_clients);
+   }
 
    pool = mongoc_client_pool_new (uri);
    mongoc_client_pool_set_error_api (pool, 2);
 
-   for (i = 0; i < 2000; i++) {
+   for (i = 0; i < num_clients; i++) {
       pthread_create (&threads[i], NULL, worker, pool);
    }
 
@@ -78,7 +99,7 @@ main (int argc, char *argv[])
    in_shutdown = true;
    pthread_mutex_unlock (&mutex);
 
-   for (i = 0; i < 2000; i++) {
+   for (i = 0; i < num_clients; i++) {
       pthread_join (threads[i], &ret);
    }
 
