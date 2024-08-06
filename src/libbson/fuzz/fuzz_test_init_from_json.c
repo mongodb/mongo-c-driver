d36b42b85d57d4309add568df351eb755a4462d8
Spencer Jackson <spencer.jackson@mongodb.com>
2024-08-06 03:10:39 +0000

    Lint

--- src/libbson/fuzz/fuzz_test_init_from_json.c
+++ src/libbson/fuzz/fuzz_test_init_from_json.c
@@ -9,7 +9,7 @@ LLVMFuzzerTestOneInput (const uint8_t *data, size_t size)
    bson_error_t error;
 
    bson_t b;
-   if (bson_init_from_json (&b, (const char*) data, size, &error)) {
+   if (bson_init_from_json (&b, (const char *) data, size, &error)) {
       bson_destroy (&b);
    }
 
