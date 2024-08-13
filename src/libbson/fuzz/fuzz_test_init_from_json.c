8cc212b0cc9cc653fff3fd764dc0fead16b9d01f
Spencer Jackson <spencer.jackson@mongodb.com>
2024-08-13 11:29:10 -0400

    CDRIVER-5648 fix libfuzzer integration (#1686)
    * Implement and simplify libfuzzer entrypoint
    
    * Structure libbson/CMakeLists.txt to isolate fuzzer definitions
    
    * Rework fuzzer enablement to use a switch

--- src/libbson/fuzz/fuzz_test_init_from_json.c
+++ src/libbson/fuzz/fuzz_test_init_from_json.c
@@ -6,16 +6,12 @@
 int
 LLVMFuzzerTestOneInput (const uint8_t *data, size_t size)
 {
-   char *nt = malloc (size + 1);
-   memcpy (nt, data, size);
-   nt[size] = '\0';
    bson_error_t error;
 
    bson_t b;
-   if (bson_init_from_json (&b, nt, -1, &error)) {
+   if (bson_init_from_json (&b, (const char *) data, size, &error)) {
       bson_destroy (&b);
    }
 
-   free (nt);
    return 0;
 }
