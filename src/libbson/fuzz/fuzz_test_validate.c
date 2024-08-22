e4e3e43c82d9f319db4684787d9e222cd96c44cc
Spencer Jackson <spencerandrewjackson@yahoo.com>
2024-08-22 15:48:03 -0400

    Update src/libbson/fuzz/fuzz_test_validate.c
    Co-authored-by: Kevin Albertson <kevin.albertson@10gen.com>

--- src/libbson/fuzz/fuzz_test_validate.c
+++ src/libbson/fuzz/fuzz_test_validate.c
@@ -9,7 +9,7 @@ LLVMFuzzerTestOneInput (const uint8_t *data, size_t size)
    bson_t b;
    if (bson_init_static (&b, data, size)) {
       bson_validate (&b,
-                     BSON_VALIDATE_NONE | BSON_VALIDATE_UTF8 | BSON_VALIDATE_DOLLAR_KEYS | BSON_VALIDATE_DOT_KEYS |
+                     BSON_VALIDATE_UTF8 | BSON_VALIDATE_DOLLAR_KEYS | BSON_VALIDATE_DOT_KEYS |
                         BSON_VALIDATE_UTF8_ALLOW_NULL | BSON_VALIDATE_EMPTY_KEYS,
                      NULL);
    }
