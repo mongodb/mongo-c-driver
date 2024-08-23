3eb069e362b28a91f0bf720b9e7a512a64668a20
Spencer Jackson <spencerandrewjackson@yahoo.com>
2024-08-23 11:22:30 -0400

    Update src/libbson/fuzz/fuzz_test_validate.c
    Co-authored-by: Ezra Chung <88335979+eramongodb@users.noreply.github.com>

--- src/libbson/fuzz/fuzz_test_validate.c
+++ src/libbson/fuzz/fuzz_test_validate.c
@@ -12,7 +12,7 @@ LLVMFuzzerTestOneInput (const uint8_t *data, size_t size)
                      BSON_VALIDATE_UTF8 | BSON_VALIDATE_DOLLAR_KEYS | BSON_VALIDATE_DOT_KEYS |
                         BSON_VALIDATE_UTF8_ALLOW_NULL | BSON_VALIDATE_EMPTY_KEYS,
                      NULL);
+      return 0;
    }
-
-   return 0;
+   return -1;
 }
