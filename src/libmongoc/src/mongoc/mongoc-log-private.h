1442a1c8b6ca355c82af83bf9c5578cd9e2a353c
Joshua Siegel <josh.siegel@mongodb.com>
2024-08-28 16:07:51 -0400

    wrap macro in if/else block

--- src/libmongoc/src/mongoc/mongoc-log-private.h
+++ src/libmongoc/src/mongoc/mongoc-log-private.h
@@ -34,14 +34,12 @@ mongoc_log_trace_bytes (const char *domain, const uint8_t *_b, size_t _l);
 void
 mongoc_log_trace_iovec (const char *domain, const mongoc_iovec_t *_iov, size_t _iovcnt);
 
-BSON_BEGIN_DECLS
-#define STOP_LOGGING_CHECK                                          \
-   int stop_logging;                                                \
-   stop_logging = !gLogFunc;                                        \
-   stop_logging = stop_logging || !_mongoc_log_trace_is_enabled (); \
-   if (stop_logging) {                                              \
-      return;                                                       \
-   }
+#define STOP_LOGGING_CHECK                                 \
+   if (1) {                                                \
+      if (!gLogFunc || !_mongoc_log_trace_is_enabled ()) { \
+         return;                                           \
+      }                                                    \
+   } else                                                  \
+      (void) 0
 
-BSON_END_DECLS
 #endif /* MONGOC_LOG_PRIVATE_H */
