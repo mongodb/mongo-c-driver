77bcfc7c4ddebe808ca468647d0fa8914fdca336
Joshua Siegel <josh.siegel@mongodb.com>
2024-10-09 18:31:17 -0400

    fix RHEL8.9 typo

--- .evergreen/config_generator/components/loadbalanced.py
+++ .evergreen/config_generator/components/loadbalanced.py
@@ -13,7 +13,7 @@ from config_generator.etc.distros import make_distro_str, find_small_distro, fin
 from config_generator.etc.utils import Task, bash_exec
 
 # Use `rhel8.9` distro. `rhel8.9` distro includes necessary dependency: `haproxy`.
-_DISTRO_NAME = "rhel89"
+_DISTRO_NAME = "rhel8.9"
 _COMPILER = "gcc"
 
 
