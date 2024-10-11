77a479e96c520b7d2546c32e9ebd2153f244c508
Kevin Albertson <kevin.albertson@10gen.com>
2024-10-11 09:03:58 -0400

    remove unnecessary check of `>= 0`
    Co-authored-by: Ezra Chung <88335979+eramongodb@users.noreply.github.com>

--- src/libmongoc/examples/client-side-encryption-explicit.c
+++ src/libmongoc/examples/client-side-encryption-explicit.c
@@ -118,7 +118,7 @@ main (void)
    to_encrypt.value_type = BSON_TYPE_UTF8;
    to_encrypt.value.v_utf8.str = "123456789";
    const size_t len = strlen (to_encrypt.value.v_utf8.str);
-   BSON_ASSERT (len >= 0 && (uint64_t) len <= UINT32_MAX);
+   BSON_ASSERT (len <= UINT32_MAX);
    to_encrypt.value.v_utf8.len = (uint32_t) len;
 
    ret = mongoc_client_encryption_encrypt (client_encryption, &to_encrypt, encrypt_opts, &encrypted_field, &error);
