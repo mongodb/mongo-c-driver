0dc839cf252de13ad2099fde5d38d060d5c651b2
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2025-02-26 09:10:14 -0600

    CDRIVER-5891 address implicit bool-to-null conversion in return statements (#1888)

--- src/libmongoc/examples/client-side-encryption-helpers.c
+++ src/libmongoc/examples/client-side-encryption-helpers.c
@@ -15,7 +15,7 @@ hex_to_bin (const char *hex, uint32_t *len)
 
    size_t num_bytes = hex_len / 2u;
    if (num_bytes >= UINT32_MAX) {
-      return false;
+      return NULL;
    }
 
    *len = (uint32_t) (hex_len / 2u);
