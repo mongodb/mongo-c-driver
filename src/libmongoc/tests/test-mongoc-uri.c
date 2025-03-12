5f1d121ceb4964e8580a6584e530980b28a09b68
Ezra Chung <ezra.chung@mongodb.com>
2025-03-12 16:34:49 -0500

    Add missing ASSERT_OR_PRINT

--- src/libmongoc/tests/test-mongoc-uri.c
+++ src/libmongoc/tests/test-mongoc-uri.c
@@ -921,6 +921,7 @@ test_mongoc_uri_auth_mechanism_mongodb_aws (void)
          mongoc_uri_t *const uri =
             mongoc_uri_new_with_error ("mongodb://:@localhost/?" MONGOC_URI_AUTHMECHANISM "=MONGODB-AWS", &error);
          ASSERT_NO_CAPTURED_LOGS ("mongoc_uri_new_with_error");
+         ASSERT_OR_PRINT (uri, error);
          ASSERT_CMPSTR (mongoc_uri_get_username (uri), "");
          mongoc_uri_destroy (uri);
       }
