c6bfc504913711d4481bb9404b9a96a833c28381
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2025-03-17 14:25:38 -0500

    Fix upload of ABI Compatibility Report HTML on failure (#1925)
    * CDRIVER-5930 temporarily disable abi-compliance-check task failure for v2.0

--- .evergreen/config_generator/components/abi_compliance_check.py
+++ .evergreen/config_generator/components/abi_compliance_check.py
@@ -15,32 +15,20 @@ class CheckABICompliance(Function):
             script='.evergreen/scripts/abi-compliance-check-setup.sh'
         ),
         bash_exec(
-            command_type=EvgCommandType.SETUP,
+            command_type=EvgCommandType.TEST,
             add_expansions_to_env=True,
             working_dir='mongoc',
             script='.evergreen/scripts/abi-compliance-check.sh'
-        ),
-        bash_exec(
-            command_type=EvgCommandType.TEST,
-            working_dir='mongoc',
-            env={
-                'AWS_ACCESS_KEY_ID': '${aws_key}',
-                'AWS_SECRET_ACCESS_KEY': '${aws_secret}',
-            },
-            script='''\
-                aws s3 cp abi-compliance/compat_reports s3://mciuploads/${project}/${build_variant}/${revision}/${version_id}/${build_id}/abi-compliance/compat_reports --recursive --acl public-read --region us-east-1
-                [[ ! -f ./abi-compliance/abi-error.txt ]]
-            '''
         ),
         s3_put(
             aws_key='${aws_key}',
             aws_secret='${aws_secret}',
             bucket='mciuploads',
             content_type='text/html',
-            display_name='ABI Report:',
-            local_files_include_filter='mongoc/abi-compliance/compat_reports/**/*.html',
+            display_name='ABI Compliance Check: ',
+            local_files_include_filter='abi-compliance/compat_reports/**/compat_report.html',
             permissions='public-read',
-            remote_file='${project}/${build_variant}/${revision}/${version_id}/${build_id}/abi-compliance/compat_report.html',
+            remote_file='${project}/${build_variant}/${revision}/${version_id}/${build_id}/abi-compliance/compat_reports/',
         ),
     ]
 
