4fa042ce7c7bd38c5d86f8db8940245b55fbf713
Kevin Albertson <kevin.albertson@mongodb.com>
2025-03-31 11:16:23 -0400

    CDRIVER-5951 remove deprecated TLS API (#1962)
    * remove `mongoc_stream_tls_do_handshake`
    * remove `mongoc_stream_tls_check_cert`
    * remove `mongoc_stream_tls_new`

--- src/libmongoc/src/mongoc/mongoc-stream-tls.h
+++ src/libmongoc/src/mongoc/mongoc-stream-tls.h
@@ -40,19 +40,9 @@ mongoc_stream_tls_handshake_block (mongoc_stream_t *stream,
                                    int32_t timeout_msec,
                                    bson_error_t *error);
 
-BSON_DEPRECATED_FOR (mongoc_stream_tls_handshake)
-MONGOC_EXPORT (bool) mongoc_stream_tls_do_handshake (mongoc_stream_t *stream, int32_t timeout_msec);
-
-BSON_DEPRECATED_FOR (mongoc_stream_tls_handshake)
-MONGOC_EXPORT (bool) mongoc_stream_tls_check_cert (mongoc_stream_t *stream, const char *host);
-
 MONGOC_EXPORT (mongoc_stream_t *)
 mongoc_stream_tls_new_with_hostname (mongoc_stream_t *base_stream, const char *host, mongoc_ssl_opt_t *opt, int client)
    BSON_GNUC_WARN_UNUSED_RESULT;
-
-BSON_DEPRECATED_FOR (mongoc_stream_tls_new_with_hostname)
-MONGOC_EXPORT (mongoc_stream_t *)
-   mongoc_stream_tls_new (mongoc_stream_t *base_stream, mongoc_ssl_opt_t *opt, int client) BSON_GNUC_WARN_UNUSED_RESULT;
 
 
 BSON_END_DECLS
