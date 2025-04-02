52ed37c59763e4db86bff103254780d8119e9ce7
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2025-04-02 14:06:25 -0500

    CDRIVER-5930 re-enable ABI compliance checks (#1976)

--- .evergreen/scripts/abi-compliance-check-setup.sh
+++ .evergreen/scripts/abi-compliance-check-setup.sh
@@ -18,26 +18,29 @@ fi
 declare parallel_level
 parallel_level="$(("$(nproc)" + 1))"
 
+export PATH
+PATH="${MONGO_C_DRIVER_CACHE_DIR:?}/bin:${PATH:-}" # abi-compliance-checker
+
 # Obtain abi-compliance-checker.
 echo "Fetching abi-compliance-checker..."
-[[ -d checker ]] || {
-  git clone -b "2.3" --depth 1 https://github.com/lvc/abi-compliance-checker.git checker
-  pushd checker
-  make -j "${parallel_level:?}" --no-print-directory install prefix="${working_dir:?}/install"
-  popd # checker
+[[ -d "${MONGO_C_DRIVER_CACHE_DIR:?}/checker-2.3" ]] || {
+  git clone -b "2.3" --depth 1 https://github.com/lvc/abi-compliance-checker.git "${MONGO_C_DRIVER_CACHE_DIR:?}/checker-2.3"
+  pushd "${MONGO_C_DRIVER_CACHE_DIR:?}/checker-2.3"
+  make -j "${parallel_level:?}" --no-print-directory install prefix="${MONGO_C_DRIVER_CACHE_DIR:?}"
+  popd # "${MONGO_C_DRIVER_CACHE_DIR:?}/checker-2.3"
 } >/dev/null
 echo "Fetching abi-compliance-checker... done."
 
 # Obtain ctags.
 echo "Fetching ctags..."
-[[ -d ctags ]] || {
-  git clone -b "v6.0.0" --depth 1 https://github.com/universal-ctags/ctags.git ctags
-  pushd ctags
+[[ -d "${MONGO_C_DRIVER_CACHE_DIR:?}/ctags-6.0.0" ]] || {
+  git clone -b "v6.0.0" --depth 1 https://github.com/universal-ctags/ctags.git "${MONGO_C_DRIVER_CACHE_DIR:?}/ctags-6.0.0"
+  pushd "${MONGO_C_DRIVER_CACHE_DIR:?}/ctags-6.0.0"
   ./autogen.sh
-  ./configure --prefix="${working_dir}/install"
+  ./configure --prefix="${MONGO_C_DRIVER_CACHE_DIR:?}"
   make -j "${parallel_level:?}"
   make install
-  popd # ctags
+  popd # "${MONGO_C_DRIVER_CACHE_DIR:?}/ctags-6.0.0"
 } >/dev/null
 echo "Fetching ctags... done."
 
