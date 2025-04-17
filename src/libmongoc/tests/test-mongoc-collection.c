42f985ec51d299dead6a29b0f89ad97622635c23
Kevin Albertson <kevin.albertson@mongodb.com>
2025-04-17 08:45:33 -0400

    remove test check for wireversion=6

--- src/libmongoc/tests/test-mongoc-collection.c
+++ src/libmongoc/tests/test-mongoc-collection.c
@@ -4003,19 +4003,11 @@ _test_update_and_replace (bool is_replace, bool is_multi)
       ret =
          fn (coll, tmp_bson ("{'_id': 6}"), update, tmp_bson ("{'arrayFilters': [{'i.x': {'$gt': 1}}]}"), &reply, &err);
 
-      if (test_framework_max_wire_version_at_least (6)) {
-         ASSERT_OR_PRINT (ret, err);
-         ASSERT_MATCH (&reply,
-                       "{'modifiedCount': 1, 'matchedCount': 1, "
-                       "'upsertedId': {'$exists': false}}");
-         _test_docs_in_coll_matches (coll, tmp_bson ("{'_id':6}"), "{'a': [{'x':1},{'x':3}]}", 1);
-      } else {
-         BSON_ASSERT (!ret);
-         ASSERT_ERROR_CONTAINS (err,
-                                MONGOC_ERROR_COMMAND,
-                                MONGOC_ERROR_PROTOCOL_BAD_WIRE_VERSION,
-                                "The selected server does not support array filters");
-      }
+      ASSERT_OR_PRINT (ret, err);
+      ASSERT_MATCH (&reply,
+                    "{'modifiedCount': 1, 'matchedCount': 1, "
+                    "'upsertedId': {'$exists': false}}");
+      _test_docs_in_coll_matches (coll, tmp_bson ("{'_id':6}"), "{'a': [{'x':1},{'x':3}]}", 1);
 
       bson_destroy (&reply);
 
