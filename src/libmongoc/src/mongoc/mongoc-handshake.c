751203ef5dfbc4084af97caf7a2ea785b6320cd8
mdb-ad <198671546+mdb-ad@users.noreply.github.com>
2025-05-28 19:35:11 -0700

    have to format with clang-format 17

--- src/libmongoc/src/mongoc/mongoc-handshake.c
+++ src/libmongoc/src/mongoc/mongoc-handshake.c
@@ -531,7 +531,7 @@ _mongoc_handshake_cleanup (void)
    bson_free (h->compiler_info);
    bson_free (h->flags);
    bson_free (h->env_region);
-   *h = (mongoc_handshake_t) {0};
+   *h = (mongoc_handshake_t){0};
 
    bson_mutex_destroy (&gHandshakeLock);
 }
