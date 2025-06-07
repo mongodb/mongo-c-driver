b6ecff7a7771bedda430c16db6ce31d03c309479
mdb-ad <198671546+mdb-ad@users.noreply.github.com>
2025-06-06 17:16:40 -0700

    read sessionToken for AWS

--- src/libmongoc/tests/unified/entity-map.c
+++ src/libmongoc/tests/unified/entity-map.c
@@ -1056,6 +1056,11 @@ _parse_kms_provider_aws (
          if (!_append_kms_provider_value_or_getenv (&child, key, value, envvar, error)) {
             return false;
          }
+      } else if (strcmp (key, "sessionToken") == 0) {
+         const char *envvar = "MONGOC_TEST_AWS_TEMP_SESSION_TOKEN";
+         if (!_append_kms_provider_value_or_getenv (&child, key, value, envvar, error)) {
+            return false;
+         }
       } else {
          test_set_error (error, "unexpected field '%s'", key);
          return false;
