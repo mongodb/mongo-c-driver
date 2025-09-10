5cca39d0dc801c99e5170133f158bccbfbf4db12
Ezra Chung <ezra.chung@mongodb.com>
2025-09-10 15:29:41 -0500

    Add exception for Python >=3.12 requirement

--- tools/ruff-format-all.sh
+++ tools/ruff-format-all.sh
@@ -10,7 +10,14 @@
 set -o errexit
 set -o pipefail
 
+# Scripts which require a different Python version than the one specified in pyproject.toml.
+# See: https://github.com/astral-sh/ruff/issues/10457
+py312=(
+  src/libbson/tests/validate-tests.py
+)
+
 # Python scripts.
 # https://github.com/astral-sh/ruff/issues/8232
-uv run --frozen --group format-scripts ruff check --select I --fix
+uv run --frozen --group format-scripts ruff check --select I --fix --exclude "${py312[@]:?}"
+uv run --frozen --group format-scripts --isolated ruff check --select I --fix --target-version py312 "${py312[@]:?}"
 uv run --frozen --group format-scripts ruff format
