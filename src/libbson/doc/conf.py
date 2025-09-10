6ec4052b9dc7e83e4289719bffe8668101686869
Ezra Chung <ezra.chung@mongodb.com>
2025-09-10 15:29:42 -0500

    Address ruff check warning F403

--- src/libbson/doc/conf.py
+++ src/libbson/doc/conf.py
@@ -7,7 +7,7 @@ import sys
 this_path = os.path.dirname(__file__)
 sys.path.append(os.path.normpath(os.path.join(this_path, '../../../build/sphinx')))
 
-from mongoc_common import *  # noqa: E402
+from mongoc_common import mongoc_common_setup  # noqa: E402
 
 extensions = [
     'mongoc',
--- src/libmongoc/doc/conf.py
+++ src/libmongoc/doc/conf.py
@@ -32,7 +32,7 @@ this_path = os.path.dirname(__file__)
 sys.path.append(this_path)
 sys.path.append(os.path.normpath(os.path.join(this_path, '../../../build/sphinx')))
 
-from mongoc_common import *  # noqa: E402
+from mongoc_common import mongoc_common_setup  # noqa: E402
 
 extensions = [
     'mongoc',
