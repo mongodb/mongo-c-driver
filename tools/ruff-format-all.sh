3fde50b7d727f374d21ec59fb7375a336db04585
Ezra Chung <ezra.chung@mongodb.com>
2025-09-25 12:53:12 -0500

    Exclude bottle.py and mongodl.py from Python formatting

--- tools/ruff-format-all.sh
+++ tools/ruff-format-all.sh
@@ -10,14 +10,21 @@
 set -o errexit
 set -o pipefail
 
+# Vendored scripts to keep in sync with upstream or external sources.
+excludes=(
+  --exclude build/bottle.py
+  --exclude build/mongodl.py
+)
+
 # Scripts which require a different Python version than the one specified in pyproject.toml.
 # See: https://github.com/astral-sh/ruff/issues/10457
 py312=(
   src/libbson/tests/validate-tests.py
 )
 
-# Python scripts.
-# https://github.com/astral-sh/ruff/issues/8232
-uv run --frozen --group format-scripts ruff check --select I --fix --exclude "${py312[@]:?}"
+# Format imports: https://github.com/astral-sh/ruff/issues/8232
 uv run --frozen --group format-scripts --isolated ruff check --select I --fix --target-version py312 "${py312[@]:?}"
-uv run --frozen --group format-scripts ruff format
+uv run --frozen --group format-scripts ruff check --select I --fix --exclude "${py312[@]:?}" "${excludes[@]:?}"
+
+# Format Python scripts.
+uv run --frozen --group format-scripts ruff format "${excludes[@]:?}"
