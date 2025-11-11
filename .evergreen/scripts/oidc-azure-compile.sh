465f6e8317a940420347f185ada90d969cffc848
Kevin Albertson <kevin.albertson@mongodb.com>
2025-11-11 08:08:36 -0500

    use bash arrays
    To avoid backslashes with multi-line commands.

--- .evergreen/scripts/oidc-azure-compile.sh
+++ .evergreen/scripts/oidc-azure-compile.sh
@@ -28,16 +28,17 @@ find_ccache_and_export_vars "$(pwd)" || true
 
 echo "Compile test-libmongoc ... begin"
 # Disable unnecessary dependencies. test-libmongoc is copied to a remote host for testing, which may not have all dependent libraries.
-cmake \
-  -DENABLE_SASL=OFF \
-  -DENABLE_SNAPPY=OFF \
-  -DENABLE_ZSTD=OFF \
-  -DENABLE_ZLIB=OFF \
-  -DENABLE_SRV=OFF \
-  -DENABLE_CLIENT_SIDE_ENCRYPTION=OFF \
-  -DENABLE_EXAMPLES=OFF \
-  -DENABLE_SRV=OFF \
-  -S. -Bcmake-build
+cmake_flags=(
+  -DENABLE_SASL=OFF
+  -DENABLE_SNAPPY=OFF
+  -DENABLE_ZSTD=OFF
+  -DENABLE_ZLIB=OFF
+  -DENABLE_SRV=OFF
+  -DENABLE_CLIENT_SIDE_ENCRYPTION=OFF
+  -DENABLE_EXAMPLES=OFF
+  -DENABLE_SRV=OFF
+)
+cmake "${cmake_flags[@]}" -Bcmake-build
 cmake --build cmake-build --target test-libmongoc --parallel
 echo "Compile test-libmongoc ... end"
 
@@ -45,11 +46,13 @@ echo "Compile test-libmongoc ... end"
 echo "Creating test-libmongoc tarball ... begin"
 
 # Copy test binary and JSON test files. All JSON test files are needed to start test-libmongoc.
-tar -czf test-libmongoc.tar.gz \
-  .evergreen/scripts/oidc-azure-test.sh \
-  ./cmake-build/src/libmongoc/test-libmongoc \
-  src/libmongoc/tests/json \
+files=(
+  .evergreen/scripts/oidc-azure-test.sh
+  cmake-build/src/libmongoc/test-libmongoc
+  src/libmongoc/tests/json
   src/libbson/tests/json
+)
+tar -czf test-libmongoc.tar.gz "${files[@]}"
 echo "Creating test-libmongoc tarball ... end"
 
 cat <<EOT > oidc-remote-test-expansion.yml
