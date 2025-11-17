7898f603ea186a887ca78c652cb43f8407a8afae
Kevin Albertson <kevin.albertson@mongodb.com>
2025-11-17 12:59:26 -0500

    remove VS 2015 tasks

--- .evergreen/config_generator/components/cse/winssl.py
+++ .evergreen/config_generator/components/cse/winssl.py
@@ -13,14 +13,11 @@ TAG = f'cse-matrix-{SSL}'
 # fmt: off
 COMPILE_MATRIX = [
     ('windows-vsCurrent', 'vs2022x64', None, ['cyrus']),
-    ('windows-vsCurrent', 'vs2015x64', None, ['cyrus']),
 ]
 
 # QE (subset of CSFLE) requires 7.0+ and are skipped by "server" tasks.
 TEST_MATRIX = [
     ('windows-vsCurrent', 'vs2022x64', None, 'cyrus', ['auth'], ['server', 'replica', 'sharded'], ['4.2', '4.4', '5.0', '6.0', '7.0', '8.0', 'latest']),
-
-    ('windows-vsCurrent', 'vs2015x64', None, 'cyrus', ['auth'], ['sharded'], ['4.2', 'latest']),
 ]
 # fmt: on
 # pylint: enable=line-too-long
