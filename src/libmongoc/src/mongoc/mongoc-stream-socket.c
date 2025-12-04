596727c96696b294c3984abc515c4c057ae351c7
Connor MacDonald <connor.macdonald@mongodb.com>
2025-12-04 11:51:45 -0500

    Treat 0 timeout as infinite

--- src/libmongoc/src/mongoc/mongoc-stream-socket.c
+++ src/libmongoc/src/mongoc/mongoc-stream-socket.c
@@ -36,10 +36,8 @@ struct _mongoc_stream_socket_t {
 static BSON_INLINE int64_t
 get_expiration(int32_t timeout_msec)
 {
-   if (timeout_msec < 0) {
+   if (timeout_msec <= 0) {
       return -1;
-   } else if (timeout_msec == 0) {
-      return 0;
    } else {
       return (bson_get_monotonic_time() + ((int64_t)timeout_msec * 1000L));
    }
