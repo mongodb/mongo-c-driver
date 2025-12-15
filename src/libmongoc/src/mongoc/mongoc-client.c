a1364fcbf1f896c6e33fa39db999d9182fe9eab7
Kevin Albertson <kevin.albertson@mongodb.com>
2025-12-15 13:22:01 -0500

    use `const bool` for `reconnect_ok`

--- src/libmongoc/src/mongoc/mongoc-client.c
+++ src/libmongoc/src/mongoc/mongoc-client.c
@@ -2153,12 +2153,9 @@ _mongoc_client_kill_cursor(mongoc_client_t *client,
    BSON_ASSERT(cursor_id);
 
    bson_error_t error = {0};
-   bool reconnect_ok = true;
-   if (_mongoc_topology_get_type(client->topology) == MONGOC_TOPOLOGY_LOAD_BALANCED) {
-      // Do not attempt to reconnect when in load balanced mode. Cursors are pinned to connections in load balanced
-      // mode. A new connection might not connect to the same backing server.
-      reconnect_ok = false;
-   }
+   // Do not attempt to reconnect when in load balanced mode. Cursors are pinned to connections in load balanced mode. A
+   // new connection might not connect to the same backing server.
+   const bool reconnect_ok = _mongoc_topology_get_type(client->topology) != MONGOC_TOPOLOGY_LOAD_BALANCED;
    // Try to reconnect. Log on error.
    server_stream = mongoc_cluster_stream_for_server(&client->cluster, server_id, reconnect_ok, NULL, NULL, &error);
 
