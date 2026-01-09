776f3c70a4836375fbe09e11fb639ebd018146bd
Kevin Albertson <kevin.albertson@mongodb.com>
2026-01-09 13:21:39 -0500

    use `if/else`

--- src/libmongoc/src/mongoc/mongoc-change-stream.c
+++ src/libmongoc/src/mongoc/mongoc-change-stream.c
@@ -523,15 +523,15 @@ mongoc_change_stream_next(mongoc_change_stream_t *stream, const bson_t **bson)
             goto end;
          }
          BSON_ASSERT(err_doc);
-         resumable = _is_resumable_error(stream, err_doc);
          if (stream->cursor->had_stream_timeout) {
             iteration_timeout_count++;
          }
-
          if (iteration_timeout_count >= 2) {
             // CDRIVER-6182: Do not resume if two iteration timeouts occur. Intended to avoid a possible resume loop
             // when `aggregate` succeeds but `getMore` consistently times out.
             resumable = false;
+         } else {
+            resumable = _is_resumable_error(stream, err_doc);
          }
       }
 
