eb4031f847881fd21e7a9a427ec99b3a9e31901a
Connor MacDonald <connor.macdonald@mongodb.com>
2026-01-30 07:42:52 -0500

    Fix use of incorrect bson_error_t

--- src/libmongoc/tests/unified/operation.c
+++ src/libmongoc/tests/unified/operation.c
@@ -2484,7 +2484,7 @@ operation_drop_index(test_t *test, operation_t *op, result_t *result, bson_error
    }
 
    coll = entity_map_get_collection(test->entity_map, op->object, error);
-   mongoc_collection_drop_index(coll, index, error);
+   mongoc_collection_drop_index(coll, index, &op_error);
    result_from_val_and_reply(result, NULL, NULL, &op_error);
    ret = true;
 
