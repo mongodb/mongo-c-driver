79387971b02bb52318d215a1ad7e77b1dc40d5d4
Christian Schmitz <support@monkeybreadsoftware.de>
2026-08-07 13:47:37 +0200

    Update export qualifiers for bulk operation functions
    Added missing MONGOC_EXPORT.

--- src/libmongoc/src/mongoc/mongoc-bulk-operation.h
+++ src/libmongoc/src/mongoc/mongoc-bulk-operation.h
@@ -154,19 +154,19 @@ mongoc_bulk_operation_get_server_id(const mongoc_bulk_operation_t *bulk);
 MONGOC_EXPORT(const mongoc_write_concern_t *)
 mongoc_bulk_operation_get_write_concern(const mongoc_bulk_operation_t *bulk);
 
-bool
+MONGOC_EXPORT(bool)
 mongoc_bulk_operation_get_bypass_document_validation(const mongoc_bulk_operation_t *bulk);
 
-const char*
+MONGOC_EXPORT(const char*)
 mongoc_bulk_operation_get_collection(mongoc_bulk_operation_t *bulk);
 
-const char*
+MONGOC_EXPORT(const char*)
 mongoc_bulk_operation_get_database(mongoc_bulk_operation_t *bulk);
 
-const bson_value_t*
+MONGOC_EXPORT(const bson_value_t*)
 mongoc_bulk_operation_get_comment(mongoc_bulk_operation_t *bulk);
 
-const bson_t*
+MONGOC_EXPORT(const bson_t*)
 mongoc_bulk_operation_get_let(mongoc_bulk_operation_t *bulk);
 
 BSON_END_DECLS
