d0cc12d707f8c8d75a80ae3c138c9415f55d65e9
Ezra Chung <88335979+eramongodb@users.noreply.github.com>
2026-09-23 15:27:39 -0500

    CDRIVER-6445 Add mongoac C++17 Catch2 test suite integration (#2432)

---
+++ src/libmongoac/test/mongoac/test/string.hh
@@ -0,0 +1,34 @@
+// Copyright 2009-present MongoDB, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <mongoac/string.h>
+
+#include <string_view>
+
+namespace mongoac::test
+{
+
+inline std::string_view
+from_mongoac(mongoac_string_view_t v)
+{
+   if (v.ptr == nullptr) {
+      return {};
+   }
+
+   return std::string_view{v.ptr, v.len};
+}
+
+} // namespace mongoac::test
