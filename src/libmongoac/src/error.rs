a995d04ef55863913fe9fa3037ea105aa34c1130
Ezra Chung <ezra.chung@mongodb.com>
2026-09-25 12:03:17 -0500

    Add safety macro for into_raw

--- src/libmongoac/src/error.rs
+++ src/libmongoac/src/error.rs
@@ -64,12 +64,12 @@ pub extern "C" fn mongoac_error_destroy(error: *mut ErrorT) {
 
 #[unsafe(no_mangle)]
 pub extern "C" fn mongoac_error_clone(error: *const ErrorT) -> *mut ErrorT {
-    Box::into_raw(Box::new(safe_as_ref!(error).clone()))
+    safe_into_raw!(safe_as_ref!(error).clone())
 }
 
 #[unsafe(no_mangle)]
 pub extern "C" fn mongoac_error_new() -> *mut ErrorT {
-    Box::into_raw(Box::new(ErrorT::new()))
+    safe_into_raw!(ErrorT::new())
 }
 
 #[unsafe(no_mangle)]
